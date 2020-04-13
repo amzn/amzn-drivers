4eae56e14e7e4f934ec898e83b8124cfb0139938
Gal Pressman <galpress@amazon.com>
2020-04-13 14:36:02 +0300

    linux/efa: Fix missing define keyword in #ifdef statement
    gcc says:
    efa_verbs.c: In function 'efa_reg_mr':
    efa_verbs.c:2041:7: warning: "HAVE_IB_UMEM_GET_NO_DMASYNC" is not defined, evaluates to 0 [-Wundef]
     #elif HAVE_IB_UMEM_GET_NO_DMASYNC
           ^~~~~~~~~~~~~~~~~~~~~~~~~~~
    Because the #elif is missing the defined keyword, fix that.
    
    Reviewed-by: Daniel Kranzdorf <dkkranzd@amazon.com>
    Reviewed-by: Firas JahJah <firasj@amazon.com>
    Signed-off-by: Gal Pressman <galpress@amazon.com>

--- kernel/linux/efa/src/efa_verbs.c
+++ kernel/linux/efa/src/efa_verbs.c
@@ -2001,7 +2001,7 @@ struct ib_mr *efa_reg_mr(struct ib_pd *ibpd, u64 start, u64 length,
 #ifdef HAVE_IB_UMEM_GET_DEVICE_PARAM
 		mr->umem = ib_umem_get(ibpd->device, start, length,
 				       access_flags);
-#elif HAVE_IB_UMEM_GET_NO_DMASYNC
+#elif defined(HAVE_IB_UMEM_GET_NO_DMASYNC)
 		mr->umem = ib_umem_get(udata, start, length, access_flags);
 #elif defined(HAVE_IB_UMEM_GET_UDATA)
 		mr->umem = ib_umem_get(udata, start, length, access_flags, 0);
@@ -2035,7 +2035,7 @@ struct ib_mr *efa_reg_mr(struct ib_pd *ibpd, u64 start, u64 length,
 #else /* !defined(HAVE_EFA_GDR) */
 #ifdef HAVE_IB_UMEM_GET_DEVICE_PARAM
 	mr->umem = ib_umem_get(ibpd->device, start, length, access_flags);
-#elif HAVE_IB_UMEM_GET_NO_DMASYNC
+#elif defined(HAVE_IB_UMEM_GET_NO_DMASYNC)
 	mr->umem = ib_umem_get(udata, start, length, access_flags);
 #elif defined(HAVE_IB_UMEM_GET_UDATA)
 	mr->umem = ib_umem_get(udata, start, length, access_flags, 0);
