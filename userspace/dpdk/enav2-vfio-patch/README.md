8eab12c384c1466aeaec18912f0e0eb12bc470fe
Michal Krawczyk <mk@semihalf.com>
2020-10-02 15:41:51 +0200

    dpdk: update script for adding WC to the vfio
    Previous patch was hardcoded and had some issues with compatibility with
    few kernel versions.
    
    To avoid that, the patch is now kept in separate file and can be choosen
    dynamically using the kernel version.
    
    As for now, Linux v4.10 and all later are supported.
    
    Besides that, the script now prints out much more useful messages, have
    improved error reporting and error handling and bash style has been
    unified.
    
    The REAME for the script was updated as well - including list of AMIs
    supported by the script, few MD style fixes and other minor changes.
    
    Signed-off-by: Michal Krawczyk <mk@semihalf.com>

--- userspace/dpdk/enav2-vfio-patch/README.md
+++ userspace/dpdk/enav2-vfio-patch/README.md
@@ -3,58 +3,75 @@ ENA with VFIO-PCI driver
 
 Enable WC in VFIO-PCI driver
 ----------------------------
+
 VFIO-PCI driver does not support write combine.
 To activate this feature, the patch that checks if PCI BAR is prefetchable must
 be added.
+
 In such case it must be mapped with memory marked as WC:
- * `pci_iomap_wc` must be used instead `pci_iomap`
+
+- `pci_iomap_wc` must be used instead `pci_iomap`
     (for Kernel version that does not have it `ioremap_wc` could be used),
- * `pgprot_noncached` needs to be changed to `pgprot_writecombine`.
-Also, as not all AWS instances support IOMMU
-`VFIO No-IOMMU support` must be enabled in the kernel configuration.
+- `pgprot_noncached` needs to be changed to `pgprot_writecombine`.
 
-The VFIO driver can be patched-up by using `wc_enable.sh` script.
-It should be executed with root privileges.
+Also, as not all AWS instances support IOMMU, `VFIO No-IOMMU support` must be
+enabled in the kernel configuration.
 
-Script:
+The VFIO driver can be downloaded and patched-up by using `get-vfio-with-wc.sh`
+script. It should be executed with root privileges.
+
+The script performs 3 steps:
+
 1. Download appropriate kernel sources
 1. Patch and build VFIO driver
 1. Replace default VFIO module
 
-As the module was built in the default location, it can be loaded using:
-```
+As the patched module is installed in the default location, it can be loaded
+just by executing:
+
+```sh
 modprobe vfio-pci
 ```
 
 Script was tested on:
-  * Amazon Linux 2 AMI (HVM), SSD Volume Type - ami-030aae8cba933aede
-  * Amazon Linux AMI 2018.03.0 (HVM), SSD Volume Type - ami-0233214e13e500f77
-  * Red Hat Enterprise Linux 7.5 (HVM), SSD Volume Type - ami-c86c3f23
-  * Ubuntu Server 18.04 LTS (HVM), SSD Volume Type - ami-0bdf93799014acdc4
+
+- Amazon Linux 2 AMI (HVM), SSD Volume Type - ami-0bb3fad3c0286ebd5
+- Amazon Linux AMI 2018.03.0 (HVM), SSD Volume Type - ami-015232c01a82b847b
+- Red Hat Enterprise Linux 8 (HVM), SSD Volume Type - ami-08f4717d06813bf00
+- Ubuntu Server 20.04 LTS (HVM), SSD Volume Type - ami-06fd8a495a537da8b
+- Ubuntu Server 18.04 LTS (HVM), SSD Volume Type - ami-0823c236601fef765
 
 IOMMU support
 -------------
+
 Most of AWS instances do not support IOMMU.
 In such case noiommu mode in driver must be enabled:
-```
+
+```sh
 echo 1 > /sys/module/vfio/parameters/enable_unsafe_noiommu_mode
 ```
 
 Some instances (for example i3.metal) support this feature.
 It needs to be enabled, by adding parameters `iommu=1 intel_iommu=on`
 to the kernel's boot line. It could be done in `/etc/default/grub`:
-```
+
+```sh
 GRUB_CMDLINE_LINUX="console=ttyS0,115200n8 console=tty0 net.ifnames=0 crashkernel=auto iommu=1 intel_iommu=on"
 ```
+
 Then update GRUB configuration:
-```
+
+```sh
 grub2-mkconfig > /boot/grub2/grub.cfg
 ```
+
 Now, the reboot is needed.
 
 If IOMMU is active, `/sys/kernel/iommu_groups/` should not be empty.
+
 For example:
-```
+
+```sh
 ls /sys/kernel/iommu_groups/
 0    103  14  2   25  30  36  41  47  52  58  63  69  74  8   85  90  96
 1    104  15  20  26  31  37  42  48  53  59  64  7   75  80  86  91  97
@@ -66,7 +83,9 @@ ls /sys/kernel/iommu_groups/
 
 Binding
 -------
+
 NIC could be bound with DPDK tool:
-```
+
+```sh
 usertools/dpdk-devbind.py --bind=vfio-pci PCI-ID
 ```
---
+++ userspace/dpdk/enav2-vfio-patch/get-vfio-with-wc.sh
@@ -0,0 +1,196 @@
+#!/usr/bin/env bash
+# Enable WC in VFIO-PCI driver
+# Tested on:
+#  * Amazon Linux 2 AMI (HVM), SSD Volume Type - ami-0bb3fad3c0286ebd5
+#  * Amazon Linux AMI 2018.03.0 (HVM), SSD Volume Type - ami-015232c01a82b847b
+#  * Red Hat Enterprise Linux 8 (HVM), SSD Volume Type - ami-08f4717d06813bf00
+#  * Ubuntu Server 20.04 LTS (HVM), SSD Volume Type - ami-06fd8a495a537da8b
+#  * Ubuntu Server 18.04 LTS (HVM), SSD Volume Type - ami-0823c236601fef765
+
+set -e
+
+TMP_DIR="tmp"
+
+# Kernel modules location:
+P1="/usr/lib/modules/`uname -r`/kernel/drivers/vfio"
+P2="/lib/modules/`uname -r`/kernel/drivers/vfio"
+
+RED="$(tput setaf 1)"
+GREEN="$(tput setaf 2)"
+
+BOLD="$(tput bold)"
+NORMAL="$(tput sgr0)"
+
+function bold {
+	echo -e "${BOLD}${@}${NORMAL}"
+}
+
+function err {
+	bold "${RED}ERROR: ${@}"
+}
+
+function green {
+	bold "${GREEN}${@}"
+}
+
+function get_kernel_version {
+	local ver=$(uname -r | cut -f 1 -d '-')
+	local ver_major=$(echo $ver | cut -f1 -d '.')
+	local ver_minor=$(echo $ver | cut -f2 -d '.')
+	local ver_subminor=$(echo $ver | cut -f3 -d '.')
+
+	printf "%d%02d%04d" "${ver_major}" "${ver_minor}" "${ver_subminor}"
+}
+
+function download_kernel_src_yum {
+	echo "Use yum to get the kernel sources"
+
+	bold "\nInstall required applications and kernel headers"
+	yum install -y gcc "kernel-$(uname -r)" "kernel-devel-$(uname -r)" \
+	    git make elfutils-libelf-devel patch
+	green Done
+
+	# Download kernel source
+	bold "\nDownload kernel source with vfio"
+	yumdownloader --source "kernel-devel-$(uname -r)"
+	rpm2cpio kernel*.src.rpm | cpio -idmv
+	green Done
+
+	rm -f *patches.tar
+	tar xf linux-*.tar*
+	rm -f linux-*.tar* linux-*.patch
+}
+
+function download_kernel_src_apt {
+	echo "Use apt-get to get the kernel sources"
+	apt-get -q -y update
+	green Done
+
+	bold "\nInstall required applications"
+	apt-get -q -y install dpkg-dev build-essential git
+	green Done
+
+	bold "\nDownload Linux kernel source with vfio"
+	if ! apt-get -q -y source linux-image-$(uname -r); then
+		err "Cannot download Linux kernel source.\nPlease uncomment appropriate 'deb-src' line in the /etc/apt/sources.list file"
+		exit 1
+	fi
+	green Done
+
+	rm -f linux-*.dsc linux-*.gz
+}
+
+function download_kernel_src {
+	bold "[1] Downloading prerequisites..."
+	rm -rf "${TMP_DIR}"
+	mkdir -p "${TMP_DIR}"
+	cd "${TMP_DIR}"
+
+	if apt-get -v >/dev/null 2>/dev/null; then
+		download_kernel_src_apt
+	else
+		download_kernel_src_yum
+	fi
+	cd linux-*
+}
+
+function apply_wc_patch {
+	if [ "${KERNEL_VERSION}" -ge 5080000 ]; then
+		echo "Using patch for kernel version 5.8"
+		local wc_patch="${BASE_PATH}/patches/linux-5.8-vfio-wc.patch"
+	elif [ "${KERNEL_VERSION}" -ge 4100000 ]; then
+		echo "Using patch for kernel version 4.10"
+		local wc_patch="${BASE_PATH}/patches/linux-4.10-vfio-wc.patch"
+	else
+		err "Unsupported kernel version - required at least v4.10"
+		exit 1
+	fi
+
+	if ! patch --ignore-whitespace -p1 < "${wc_patch}"; then
+		err "Cannot apply patch: ${wc_patch}!"
+		exit 1
+	fi
+}
+
+function compile_vfio_driver {
+	bold "\n[2] Patch and build the vfio driver"
+	# Adjust VFIO-PCI driver
+
+	bold "Apply patch for the write combining to the vfio-pci"
+	apply_wc_patch
+	green Done
+
+	cd drivers/vfio
+	# Configure Makefile - build VFIO with support for NOIOMMU mode
+	bold "\nConfigure Makefile for standalone vfio build and noiommu mode support"
+	echo "ccflags-y := -DCONFIG_VFIO_NOIOMMU=1" >> Makefile
+	echo 'all:' >> Makefile
+	echo '	make -C /lib/modules/$(shell uname -r)/build M=$(PWD) modules' >> Makefile
+	green Done
+
+	bold "\nBuild the driver"
+	if ! make; then
+		err "Compilation error."
+		exit 1
+	fi
+	green Done
+}
+
+function get_module_location {
+	for p in ${P1} ${P2}; do
+		if find "${p}" -name "vfio.*" >/dev/null 2>/dev/null; then
+			MOD_PATH="${p}"
+			break
+		fi
+	done
+
+	if [ -z "${MOD_PATH}" ]; then
+		err "Cannot find kernel modules location..."
+		exit
+	fi
+}
+
+function get_module_compression {
+	if ls "${MOD_PATH}/vfio.ko.xz" >/dev/null 2>/dev/null; then
+		XZ=".xz"
+	else
+		XZ=""
+	fi
+}
+
+function replace_module {
+	bold "\n[3] Install module"
+	get_module_location
+	get_module_compression
+
+	for name in "pci/vfio-pci.ko" "vfio.ko"; do
+		if [ -n "${XZ}" ]; then
+			xz "${name}" -c > "${name}${XZ}"
+		fi
+		mv "${MOD_PATH}/${name}${XZ}" "${MOD_PATH}/${name}${XZ}_no_wc"
+		cp "${name}${XZ}" "${MOD_PATH}/${name}${XZ}"
+	done
+	green "Module installed at: ${MOD_PATH}/${name}${XZ}"
+}
+
+###############################################
+# Main script code
+###############################################
+
+if [ "$(id -u)" -ne 0 ]; then
+	err 'Please execute script as a root'
+	exit 1
+fi
+
+cd $(dirname ${0})
+BASE_PATH=$(pwd)
+
+KERNEL_VERSION=$(get_kernel_version)
+
+if [ "${KERNEL_VERSION}" -lt 4100000 ]; then
+	err "Kernel version: $(uname -r) is not supported by the script. Please upgrade kernel to at least v4.10."
+fi
+
+download_kernel_src
+compile_vfio_driver
+replace_module
