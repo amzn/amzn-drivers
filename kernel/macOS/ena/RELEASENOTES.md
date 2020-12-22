bdf38f0f0c39a038776b38ff0af83a3691f514fd
Guy Tzalik <gtzalik@amazon.com>
2020-12-22 18:40:29 +0200

    Revert "macOS/ena: Introduce a new ENA driver for macOS"
    This reverts commit 8971c9eab5252e89fc923fa99523ce88a5b67a8d.

--- kernel/macOS/ena/RELEASENOTES.md
+++
@@ -1,8 +0,0 @@
-# ENA macOS Kernel Extension Release Notes
-
-## Supported Kernel Versions
-* macOS Mojave   10.14.x
-* macOS Catalina 10.15.x
-
-## r1.5.0 release notes
-Initial commit of the macOS driver.
--- kernel/macOS/ena/ena_com/ena_defs/ena_gen_info.h
+++
@@ -1,4 +0,0 @@
-// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
-// SPDX-License-Identifier: Apache-2.0
-#define	ENA_GEN_DATE	"Fri Sep 18 17:09:00 IDT 2020"
-#define	ENA_GEN_COMMIT	"0f80d82"
