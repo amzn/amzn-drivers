47aaedd202d0f77c5802500a0e85500910a33bfe
Gal Pressman <galpress@amazon.com>
2021-01-27 10:35:27 +0200

    linux/efa: GDR, Use the proper variable to pass nvidia driver the kernel version
    Passing KERNEL_UNAME instead of SYSSRC to nvidia's driver is preferable
    as it will decide whether to use the /build or /source directory
    internally.
    
    Reviewed-by: Firas JahJah <firasj@amazon.com>
    Reviewed-by: Yossi Leybovich <sleybo@amazon.com>
    Signed-off-by: Gal Pressman <galpress@amazon.com>

--- kernel/linux/efa/conf/configure-dkms-gdr.sh
+++ kernel/linux/efa/conf/configure-dkms-gdr.sh
@@ -3,7 +3,6 @@
 
 kernelver=$1
 dkms_source_tree=$2
-kernel_source_dir=$3
 
 nvidia_path=$(find $dkms_source_tree -maxdepth 1 -type d -name '*nvidia-*' | sort | tail -n 1)
 if [ -z ${nvidia_path} ]; then
@@ -15,8 +14,8 @@ echo "== Using NVIDIA driver path $nvidia_path"
 
 echo "== Building Module.symvers"
 pushd $nvidia_path
-SYSSRC="${kernel_source_dir}" make clean || exit 1
-SYSSRC="${kernel_source_dir}" make -j$(nproc) || exit 1
+make KERNEL_UNAME="${kernelver}" clean || exit 1
+make KERNEL_UNAME="${kernelver}" -j$(nproc) || exit 1
 popd
 
 ./configure --with-kerneldir=/lib/modules/${kernelver} --with-gdr=${nvidia_path}
