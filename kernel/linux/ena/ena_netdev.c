3104159455a163e2d5bc31daa58006660b5fcdcc
Shahar Itzko <itzko@amazon.com>
2022-11-06 15:10:36 +0000

    linux/ena: Align ena_alloc_map_page signature
    Amend cosmetic issue.
    
    Signed-off-by: David Arinzon <darinzon@amazon.com>

--- kernel/linux/ena/ena_netdev.c
+++ kernel/linux/ena/ena_netdev.c
@@ -600,7 +600,8 @@ static void ena_free_all_io_rx_resources(struct ena_adapter *adapter)
 		ena_free_rx_resources(adapter, i);
 }
 
-struct page *ena_alloc_map_page(struct ena_ring *rx_ring, dma_addr_t *dma)
+struct page *ena_alloc_map_page(struct ena_ring *rx_ring,
+				dma_addr_t *dma)
 {
 	struct page *page;
 
