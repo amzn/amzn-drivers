amzn/amzn-drivers

commit 09da13f461923323231a9d85b02e3d88d1c5b72e

linux/ena: Add devlink support to ENA
Similar to ethtool, devlink tool [1] allows to modify some of the
underlying's device configurations.

The added support for it in ENA allows to modify the LLQ entry size
using devlink:
    sudo devlink dev param set pci/[interface pcie] name large_llq_header \
	value [true/false] cmode driverinit
    sudo devlink dev reload pci/[device pcie]

This patch also adds documentation for this tool.

[1]: https://www.kernel.org/doc/html/latest/networking/devlink/index.html

Signed-off-by: Shay Agroskin <shayagr@amazon.com>

kernel/linux/ena/ena_netdev.c
@@ -31,6 +31,8 @@
 
 #include "ena_lpc.h"
 
+#include "ena_devlink.h"
+
 static char version[] = DEVICE_NAME " v" DRV_MODULE_GENERATION "\n";
 
 MODULE_AUTHOR("Amazon.com, Inc. or its affiliates");
@@ -84,8 +86,6 @@ MODULE_DEVICE_TABLE(pci, ena_pci_tbl);
 
 static int ena_rss_init_default(struct ena_adapter *adapter);
 static void check_for_admin_com_state(struct ena_adapter *adapter);
-static void ena_destroy_device(struct ena_adapter *adapter, bool graceful);
-static int ena_restore_device(struct ena_adapter *adapter);
 static void ena_calc_io_queue_size(struct ena_adapter *adapter,
 				   struct ena_com_dev_get_features_ctx *get_feat_ctx);
 static void ena_set_dev_offloads(struct ena_com_dev_get_features_ctx *feat,
@@ -3839,12 +3839,20 @@ static void set_default_llq_configurations(struct ena_adapter *adapter,
 					   struct ena_llq_configurations *llq_config,
 					   struct ena_admin_feature_llq_desc *llq)
 {
+	struct ena_com_dev *ena_dev = adapter->ena_dev;
+
 	llq_config->llq_header_location = ENA_ADMIN_INLINE_HEADER;
 	llq_config->llq_stride_ctrl = ENA_ADMIN_MULTIPLE_DESCS_PER_ENTRY;
 	llq_config->llq_num_decs_before_header = ENA_ADMIN_LLQ_NUM_DESCS_BEFORE_HEADER_2;
 
+	adapter->large_llq_header_supported =
+		!!(ena_dev->supported_features & (1 << ENA_ADMIN_LLQ));
+	adapter->large_llq_header_supported &=
+		!!(llq->entry_size_ctrl_supported &
+			ENA_ADMIN_LIST_ENTRY_SIZE_256B);
+
 	if ((llq->entry_size_ctrl_supported & ENA_ADMIN_LIST_ENTRY_SIZE_256B) &&
-	    adapter->large_llq_header) {
+		adapter->large_llq_header_enabled) {
 		llq_config->llq_ring_entry_size = ENA_ADMIN_LIST_ENTRY_SIZE_256B;
 		llq_config->llq_ring_entry_size_value = 256;
 	} else {
@@ -3969,6 +3977,8 @@ static int ena_device_init(struct ena_adapter *adapter, struct pci_dev *pdev,
 	}
 #endif /* LINUX_VERSION_CODE >= KERNEL_VERSION(3, 13, 0) */
 
+	ena_devlink_params_get(adapter->devlink);
+
 	/* ENA admin level init */
 	rc = ena_com_admin_init(ena_dev, &aenq_handlers);
 	if (rc) {
@@ -4067,7 +4077,7 @@ err_disable_msix:
 	return rc;
 }
 
-static void ena_destroy_device(struct ena_adapter *adapter, bool graceful)
+void ena_destroy_device(struct ena_adapter *adapter, bool graceful)
 {
 	struct net_device *netdev = adapter->netdev;
 	struct ena_com_dev *ena_dev = adapter->ena_dev;
@@ -4114,7 +4124,7 @@ static void ena_destroy_device(struct ena_adapter *adapter, bool graceful)
 	clear_bit(ENA_FLAG_DEVICE_RUNNING, &adapter->flags);
 }
 
-static int ena_restore_device(struct ena_adapter *adapter)
+int ena_restore_device(struct ena_adapter *adapter)
 {
 	struct ena_com_dev_get_features_ctx get_feat_ctx;
 	struct ena_com_dev *ena_dev = adapter->ena_dev;
@@ -4758,7 +4768,7 @@ static void ena_calc_io_queue_size(struct ena_adapter *adapter,
 	 * and therefore divide the queue size by 2, leaving the amount
 	 * of memory used by the queues unchanged.
 	 */
-	if (adapter->large_llq_header) {
+	if (adapter->large_llq_header_enabled) {
 		if ((llq->entry_size_ctrl_supported & ENA_ADMIN_LIST_ENTRY_SIZE_256B) &&
 		    (ena_dev->tx_mem_queue_type == ENA_ADMIN_PLACEMENT_POLICY_DEV)) {
 			max_tx_queue_size /= 2;
@@ -4767,7 +4777,8 @@ static void ena_calc_io_queue_size(struct ena_adapter *adapter,
 		} else {
 			dev_err(&adapter->pdev->dev, "Forcing large headers failed: LLQ is disabled or device does not support large headers\n");
 
-			adapter->large_llq_header = false;
+			adapter->large_llq_header_enabled = false;
+			ena_devlink_disable_large_llq_header_param(adapter->devlink);
 		}
 	}
 
@@ -4802,6 +4813,7 @@ static int ena_probe(struct pci_dev *pdev, const struct pci_device_id *ent)
 	struct ena_adapter *adapter;
 	struct net_device *netdev;
 	static int adapters_found;
+	struct devlink *devlink;
 	u32 max_num_io_queues;
 	bool wd_state;
 	int bars, rc;
@@ -4884,12 +4896,18 @@ static int ena_probe(struct pci_dev *pdev, const struct pci_device_id *ent)
 
 	pci_set_drvdata(pdev, adapter);
 
-	adapter->large_llq_header = !!force_large_llq_header;
+	adapter->large_llq_header_enabled = !!force_large_llq_header;
+
+	devlink = ena_devlink_alloc(adapter);
+	if (!devlink) {
+		netdev_err(netdev, "ena_devlink_alloc failed\n");
+		goto err_netdev_destroy;
+	}
 
 	rc = ena_map_llq_mem_bar(pdev, ena_dev, bars);
 	if (rc) {
 		dev_err(&pdev->dev, "ENA LLQ bar mapping failed\n");
-		goto err_netdev_destroy;
+		goto err_devlink_destroy;
 	}
 
 	rc = ena_device_init(adapter, pdev, &get_feat_ctx, &wd_state);
@@ -4897,7 +4915,7 @@ static int ena_probe(struct pci_dev *pdev, const struct pci_device_id *ent)
 		dev_err(&pdev->dev, "ENA device init failed\n");
 		if (rc == -ETIME)
 			rc = -EPROBE_DEFER;
-		goto err_netdev_destroy;
+		goto err_devlink_destroy;
 	}
 
 	/* Initial TX and RX interrupt delay. Assumes 1 usec granularity.
@@ -5020,6 +5038,8 @@ static int ena_probe(struct pci_dev *pdev, const struct pci_device_id *ent)
 
 	adapters_found++;
 
+	ena_devlink_register(devlink, &pdev->dev);
+
 	return 0;
 
 err_rss:
@@ -5038,6 +5058,8 @@ err_worker_destroy:
 err_device_destroy:
 	ena_com_delete_host_info(ena_dev);
 	ena_com_admin_destroy(ena_dev);
+err_devlink_destroy:
+	ena_devlink_free(devlink);
 err_netdev_destroy:
 	free_netdev(netdev);
 err_free_region:
@@ -5064,9 +5086,14 @@ static void __ena_shutoff(struct pci_dev *pdev, bool shutdown)
 	struct ena_adapter *adapter = pci_get_drvdata(pdev);
 	struct ena_com_dev *ena_dev;
 	struct net_device *netdev;
+	struct devlink *devlink;
 
 	ena_dev = adapter->ena_dev;
 	netdev = adapter->netdev;
+
+	devlink = adapter->devlink;
+	ena_devlink_unregister(devlink);
+	ena_devlink_free(devlink);
 
 #ifdef CONFIG_RFS_ACCEL
 	if ((adapter->msix_vecs >= 1) && (netdev->rx_cpu_rmap)) {

kernel/linux/ena/Makefile
@@ -5,6 +5,7 @@ ena-objs := ena_netdev.o \
 		ena_ethtool.o \
 		ena_lpc.o	\
 		dim.o \
+		ena_devlink.o \
 		net_dim.o \
 		$(ENA_COM_PATH)/ena_com.o \
 		$(ENA_COM_PATH)/ena_eth_com.o

kernel/linux/ena/ena_devlink.c
@@ -1,0 +1,304 @@
+// SPDX-License-Identifier: GPL-2.0 OR Linux-OpenIB
+/*
+ * Copyright 2015-2021 Amazon.com, Inc. or its affiliates. All rights reserved.
+ */
+
+#include "ena_devlink.h"
+#ifdef ENA_DEVLINK_SUPPORT
+
+static int ena_devlink_llq_header_validate(struct devlink *devlink, u32 id,
+					   union devlink_param_value val,
+					   struct netlink_ext_ack *extack);
+
+enum ena_devlink_param_id {
+	ENA_DEVLINK_PARAM_ID_BASE = DEVLINK_PARAM_GENERIC_ID_MAX,
+	ENA_DEVLINK_PARAM_ID_LLQ_HEADER_SIZE,
+};
+
+static const struct devlink_param ena_devlink_params[] = {
+	DEVLINK_PARAM_DRIVER(ENA_DEVLINK_PARAM_ID_LLQ_HEADER_SIZE,
+			     "large_llq_header", DEVLINK_PARAM_TYPE_BOOL,
+			     BIT(DEVLINK_PARAM_CMODE_DRIVERINIT),
+			     NULL, NULL, ena_devlink_llq_header_validate),
+};
+
+static int ena_devlink_llq_header_validate(struct devlink *devlink, u32 id,
+					   union devlink_param_value val,
+					   struct netlink_ext_ack *extack)
+{
+	struct ena_adapter *adapter = ENA_DEVLINK_PRIV(devlink);
+	bool value = val.vbool;
+
+	if (!value)
+		return 0;
+
+	if (adapter->ena_dev->tx_mem_queue_type == ENA_ADMIN_PLACEMENT_POLICY_HOST) {
+		NL_SET_ERR_MSG_MOD(extack, "Instance doesn't support LLQ");
+		return -EOPNOTSUPP;
+	}
+
+	if (!adapter->large_llq_header_supported) {
+		NL_SET_ERR_MSG_MOD(extack, "Instance doesn't support large LLQ");
+		return -EOPNOTSUPP;
+	}
+
+	return 0;
+}
+
+#ifdef ENA_DEVLINK_CONFIGURE_AFTER_REGISTER
+/* Determines if ena_devlink_register has been called.
+ * Prefer to check if the driver enabled reloading capabilities, but fallback
+ * to check if driver configured 'dev' devlink attribute for older kernels.
+ */
+bool ena_is_devlink_params_registered(struct devlink *devlink)
+{
+#if defined(ENA_DEVLINK_RELOAD_ENABLING_REQUIRED)
+	return devlink->reload_enabled;
+#elif !defined(ENA_DEVLINK_RECEIVES_DEVICE_ON_ALLOC)
+	return devlink->dev;
+#endif
+}
+
+#endif
+void ena_devlink_params_get(struct devlink *devlink)
+{
+	struct ena_adapter *adapter = ENA_DEVLINK_PRIV(devlink);
+	union devlink_param_value val;
+	int err;
+
+#ifdef ENA_DEVLINK_CONFIGURE_AFTER_REGISTER
+	/* If devlink params aren't registered, don't access them */
+	if (!ena_is_devlink_params_registered(devlink))
+		return;
+#endif
+	err = devlink_param_driverinit_value_get(devlink,
+						 ENA_DEVLINK_PARAM_ID_LLQ_HEADER_SIZE,
+						 &val);
+	if (err) {
+		netdev_err(adapter->netdev, "Failed to query LLQ header size param\n");
+		return;
+	}
+
+	adapter->large_llq_header_enabled = val.vbool;
+}
+
+void ena_devlink_disable_large_llq_header_param(struct devlink *devlink)
+{
+	union devlink_param_value value;
+
+#ifdef ENA_DEVLINK_CONFIGURE_AFTER_REGISTER
+	/* If devlink params aren't registered, don't access them */
+	if (!ena_is_devlink_params_registered(devlink))
+		return;
+
+#endif
+	value.vbool = false;
+	devlink_param_driverinit_value_set(devlink,
+					   ENA_DEVLINK_PARAM_ID_LLQ_HEADER_SIZE,
+					   value);
+}
+
+static int ena_devlink_reload_down(struct devlink *devlink,
+#ifdef ENA_DEVLINK_RELOAD_NS_CHANGE_SUPPORT
+				   bool netns_change,
+#endif
+#ifdef ENA_DEVLINK_RELOAD_LIMIT_AND_ACTION_SUPPORT
+				   enum devlink_reload_action action,
+				   enum devlink_reload_limit limit,
+#endif
+				   struct netlink_ext_ack *extack)
+{
+	struct ena_adapter *adapter = ENA_DEVLINK_PRIV(devlink);
+
+#ifdef ENA_DEVLINK_RELOAD_NS_CHANGE_SUPPORT
+	if (netns_change) {
+		NL_SET_ERR_MSG_MOD(extack, "Namespace change is not supported");
+		return -EOPNOTSUPP;
+	}
+
+#endif
+#ifdef ENA_DEVLINK_RELOAD_LIMIT_AND_ACTION_SUPPORT
+	if (action != DEVLINK_RELOAD_ACTION_DRIVER_REINIT) {
+		NL_SET_ERR_MSG_MOD(extack, "Action is not supported");
+		return -EOPNOTSUPP;
+	}
+
+	if (limit != DEVLINK_RELOAD_LIMIT_UNSPEC) {
+		NL_SET_ERR_MSG_MOD(extack, "Driver reload doesn't support limitations");
+		return -EOPNOTSUPP;
+	}
+
+#endif
+	rtnl_lock();
+	ena_destroy_device(adapter, false);
+	rtnl_unlock();
+
+	return 0;
+}
+
+static int ena_devlink_reload_up(struct devlink *devlink,
+#ifdef ENA_DEVLINK_RELOAD_LIMIT_AND_ACTION_SUPPORT
+				 enum devlink_reload_action action,
+				 enum devlink_reload_limit limit,
+				 u32 *actions_performed,
+#endif
+				 struct netlink_ext_ack *extack)
+{
+	struct ena_adapter *adapter = ENA_DEVLINK_PRIV(devlink);
+	int err = 0;
+
+#ifdef ENA_DEVLINK_RELOAD_LIMIT_AND_ACTION_SUPPORT
+	if (action != DEVLINK_RELOAD_ACTION_DRIVER_REINIT) {
+		NL_SET_ERR_MSG_MOD(extack, "Action is not supported");
+		return -EOPNOTSUPP;
+	}
+
+	if (limit != DEVLINK_RELOAD_LIMIT_UNSPEC) {
+		NL_SET_ERR_MSG_MOD(extack, "Driver reload doesn't support limitations");
+		return -EOPNOTSUPP;
+	}
+
+#endif
+	rtnl_lock();
+	/* Check that no other routine initialized the device (e.g.
+	 * ena_fw_reset_device()). Also we're under devlink_mutex here,
+	 * so devink (and ena_adapter with it) isn't freed under our
+	 * feet.
+	 */
+	if (!test_bit(ENA_FLAG_DEVICE_RUNNING, &adapter->flags))
+		err = ena_restore_device(adapter);
+	rtnl_unlock();
+
+#ifdef ENA_DEVLINK_RELOAD_LIMIT_AND_ACTION_SUPPORT
+	if (!err)
+		*actions_performed = BIT(DEVLINK_RELOAD_ACTION_DRIVER_REINIT);
+
+#endif
+	return err;
+}
+#ifndef ENA_DEVLINK_RELOAD_UP_DOWN_SUPPORTED
+
+static int ena_devlink_reload(struct devlink *devlink, struct netlink_ext_ack *extack)
+{
+	/* This function always succeeds when called from this function */
+	ena_devlink_reload_down(devlink, extack);
+
+	return ena_devlink_reload_up(devlink, extack);
+}
+
+#endif
+
+static const struct devlink_ops ena_devlink_ops = {
+#ifdef ENA_DEVLINK_RELOAD_LIMIT_AND_ACTION_SUPPORT
+	.reload_actions = BIT(DEVLINK_RELOAD_ACTION_DRIVER_REINIT),
+#endif
+#ifdef ENA_DEVLINK_RELOAD_UP_DOWN_SUPPORTED
+	.reload_down	= ena_devlink_reload_down,
+	.reload_up	= ena_devlink_reload_up,
+#else
+	.reload		= ena_devlink_reload,
+#endif
+};
+
+static int ena_devlink_configure_params(struct devlink *devlink)
+{
+	struct ena_adapter *adapter = ENA_DEVLINK_PRIV(devlink);
+	union devlink_param_value value;
+	int rc;
+
+	rc = devlink_params_register(devlink, ena_devlink_params,
+				     ARRAY_SIZE(ena_devlink_params));
+	if (rc) {
+		netdev_err(adapter->netdev, "Failed to register devlink params\n");
+		return rc;
+	}
+
+	value.vbool = adapter->large_llq_header_enabled;
+	devlink_param_driverinit_value_set(devlink,
+					   ENA_DEVLINK_PARAM_ID_LLQ_HEADER_SIZE,
+					   value);
+
+#ifdef ENA_DEVLINK_RELOAD_SUPPORT_ADVERTISEMENT_NEEDED
+	devlink_set_features(devlink, DEVLINK_F_RELOAD);
+
+#endif
+#ifdef ENA_DEVLINK_RELOAD_ENABLING_REQUIRED
+	devlink_reload_enable(devlink);
+
+#endif
+	return 0;
+}
+
+struct devlink *ena_devlink_alloc(struct ena_adapter *adapter)
+{
+#ifdef ENA_DEVLINK_RECEIVES_DEVICE_ON_ALLOC
+	struct device *dev = &adapter->pdev->dev;
+#endif
+	struct devlink *devlink;
+
+#ifdef ENA_DEVLINK_RECEIVES_DEVICE_ON_ALLOC
+	devlink = devlink_alloc(&ena_devlink_ops, sizeof(struct ena_adapter *), dev);
+#else
+	devlink = devlink_alloc(&ena_devlink_ops, sizeof(struct ena_adapter *));
+#endif
+	if (!devlink) {
+		netdev_err(adapter->netdev, "Failed to allocate devlink struct\n");
+		return NULL;
+	}
+
+	ENA_DEVLINK_PRIV(devlink) = adapter;
+	adapter->devlink = devlink;
+
+#ifndef ENA_DEVLINK_CONFIGURE_AFTER_REGISTER
+	if (ena_devlink_configure_params(devlink))
+		goto free_devlink;
+
+	return devlink;
+free_devlink:
+	devlink_free(devlink);
+
+	return NULL;
+#else
+	return devlink;
+#endif
+}
+
+static void ena_devlink_configure_params_clean(struct devlink *devlink)
+{
+#ifdef ENA_DEVLINK_RELOAD_ENABLING_REQUIRED
+	devlink_reload_disable(devlink);
+
+#endif
+	devlink_params_unregister(devlink, ena_devlink_params,
+				  ARRAY_SIZE(ena_devlink_params));
+}
+
+void ena_devlink_free(struct devlink *devlink)
+{
+#ifndef ENA_DEVLINK_CONFIGURE_AFTER_REGISTER
+	ena_devlink_configure_params_clean(devlink);
+
+#endif
+	devlink_free(devlink);
+}
+
+void ena_devlink_register(struct devlink *devlink, struct device *dev)
+{
+#ifdef ENA_DEVLINK_RECEIVES_DEVICE_ON_ALLOC
+	devlink_register(devlink);
+#else
+	devlink_register(devlink, dev);
+#endif
+#ifdef ENA_DEVLINK_CONFIGURE_AFTER_REGISTER
+	ena_devlink_configure_params(devlink);
+#endif
+}
+
+void ena_devlink_unregister(struct devlink *devlink)
+{
+#ifdef ENA_DEVLINK_CONFIGURE_AFTER_REGISTER
+	ena_devlink_configure_params_clean(devlink);
+#endif
+	devlink_unregister(devlink);
+}
+#endif /* ENA_DEVLINK_SUPPORT */

kernel/linux/ena/ena_devlink.h
@@ -1,0 +1,45 @@
+// SPDX-License-Identifier: GPL-2.0 OR Linux-OpenIB
+/*
+ * Copyright 2015-2021 Amazon.com, Inc. or its affiliates. All rights reserved.
+ */
+
+#ifndef DEVLINK_H
+#define DEVLINK_H
+
+#include "ena_netdev.h"
+#ifndef ENA_NO_DEVLINK_HEADERS
+#include <net/devlink.h>
+#endif
+
+#ifdef ENA_DEVLINK_SUPPORT
+
+#define ENA_DEVLINK_PRIV(devlink) \
+	(*(struct ena_adapter **) devlink_priv(devlink))
+
+struct devlink *ena_devlink_alloc(struct ena_adapter *adapter);
+void ena_devlink_free(struct devlink *devlink);
+void ena_devlink_register(struct devlink *devlink, struct device *dev);
+void ena_devlink_unregister(struct devlink *devlink);
+void ena_devlink_params_get(struct devlink *devlink);
+void ena_devlink_disable_large_llq_header_param(struct devlink *devlink);
+
+#else /* ENA_DEVLINK_SUPPORT */
+
+#ifdef ENA_NO_DEVLINK_HEADERS
+struct devlink {};
+#endif
+
+/* Return a value of 1 so the caller wouldn't think the function failed (returned NULL) */
+static inline struct devlink *ena_devlink_alloc(struct ena_adapter *adapter)
+{
+	return (struct devlink *)1;
+}
+static inline void ena_devlink_free(struct devlink *devlink) { }
+static inline void ena_devlink_register(struct devlink *devlink, struct device *dev) { };
+static inline void ena_devlink_unregister(struct devlink *devlink) { }
+static inline void ena_devlink_params_get(struct devlink *devlink) { }
+static inline void ena_devlink_disable_large_llq_header_param(struct devlink *devlink) { }
+
+#endif /* ENA_DEVLINK_SUPPORT */
+
+#endif /* DEVLINK_H */

kernel/linux/ena/ena_netdev.h
@@ -364,6 +364,7 @@ struct ena_adapter {
 	struct net_device *netdev;
 	struct pci_dev *pdev;
 
+	struct devlink *devlink;
 	/* rx packets that shorter that this len will be copied to the skb
 	 * header
 	 */
@@ -391,7 +392,13 @@ struct ena_adapter {
 
 	u32 msg_enable;
 
-	bool large_llq_header;
+	/* The flag is used for two purposes:
+	 * 1. Indicates that large LLQ has been requested.
+	 * 2. Indicates whether large LLQ is set or not after device
+	 *    initialization / configuration.
+	 */
+	bool large_llq_header_enabled;
+	bool large_llq_header_supported;
 
 	u16 max_tx_sgl_size;
 	u16 max_rx_sgl_size;
@@ -571,5 +578,8 @@ static inline enum ena_xdp_errors_t ena_xdp_allowed(struct ena_adapter *adapter)
  * succeeded, or NULL
  */
 struct page *ena_alloc_map_page(struct ena_ring *rx_ring, dma_addr_t *dma);
+
+void ena_destroy_device(struct ena_adapter *adapter, bool graceful);
+int ena_restore_device(struct ena_adapter *adapter);
 
 #endif /* !(ENA_H) */

kernel/linux/ena/kcompat.h
@@ -696,6 +696,47 @@ do {									\
 #define HAVE_NDO_TX_TIMEOUT_STUCK_QUEUE_PARAMETER
 #endif
 
+#if defined(CONFIG_NET_DEVLINK) && LINUX_VERSION_CODE >= KERNEL_VERSION(4, 19, 0)
+#define ENA_DEVLINK_SUPPORT
+#endif
+
+#if !defined(CONFIG_NET_DEVLINK) && !defined(CONFIG_NET_DEVLINK_MODULE) && !defined(CONFIG_MAY_USE_DEVLINK)
+#define ENA_NO_DEVLINK_HEADERS
+#endif
+
+#if defined(CONFIG_NET_DEVLINK) &&					\
+	(LINUX_VERSION_CODE >= KERNEL_VERSION(5, 4, 0) ||		\
+	 (SUSE_VERSION && LINUX_VERSION_CODE >= KERNEL_VERSION(5, 3, 18)))
+#define ENA_DEVLINK_RELOAD_UP_DOWN_SUPPORTED
+#endif
+
+#if defined(CONFIG_NET_DEVLINK) && \
+	(KERNEL_VERSION(5, 4, 0) <= LINUX_VERSION_CODE && LINUX_VERSION_CODE < KERNEL_VERSION(5, 16, 0))
+#define ENA_DEVLINK_RELOAD_ENABLING_REQUIRED
+#endif
+
+#if defined(CONFIG_NET_DEVLINK) &&					\
+	(LINUX_VERSION_CODE >= KERNEL_VERSION(5, 5, 0) ||		\
+	 (SUSE_VERSION && LINUX_VERSION_CODE >= KERNEL_VERSION(5, 3, 18)))
+#define ENA_DEVLINK_RELOAD_NS_CHANGE_SUPPORT
+#endif
+
+#if defined(CONFIG_NET_DEVLINK) && LINUX_VERSION_CODE >= KERNEL_VERSION(5, 10, 0)
+#define ENA_DEVLINK_RELOAD_LIMIT_AND_ACTION_SUPPORT
+#endif
+
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 15, 0)
+#define ENA_DEVLINK_RECEIVES_DEVICE_ON_ALLOC
+#endif
+
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 16, 0)
+#define ENA_DEVLINK_RELOAD_SUPPORT_ADVERTISEMENT_NEEDED
+#endif
+
+#if LINUX_VERSION_CODE < KERNEL_VERSION(5, 16, 0)
+#define ENA_DEVLINK_CONFIGURE_AFTER_REGISTER
+#endif
+
 #if LINUX_VERSION_CODE < KERNEL_VERSION(3,19,0) && \
     !(RHEL_RELEASE_CODE && ((RHEL_RELEASE_CODE != RHEL_RELEASE_VERSION(7, 1)) && \
                             (RHEL_RELEASE_CODE > RHEL_RELEASE_VERSION(6, 6)))) && \

commit 1f0b0701fcbbcc91c5f4a4f1168a03fb31b2d737

linux/ena: Fix devlink large LLQ config not fully applied
When configuring enable/disable of large LLQ via devlink
and reloading the driver, the update of tx_max_header_size
(which controls the max size to be copied from the
packet headers to the LLQ entry), is not being reflected
in the netdev TX rings. This causes a misalignment between
the netdev TX rings and the ena_com_dev.

The impacts of this misalignment are:
1. Though large LLQ is enabled, the logic will still copy the
   LLQ inline header up to the regular size of 96 bytes
   (and not 224 bytes, as expected).
   This will cause packets with longer network headers
   (such as IPv6 + TCP SACK) to be dropped on device side.
2. If large LLQ is requested via module param, and then
   disabled by devlink (set to false), traffic would
   fail, as ena_com_dev would operate with standard LLQ
   inline header max size of 96 but the netdev will
   copy up 224 bytes and the LLQ inline header will exceed
   the max size in ena_com_dev.
   The impact of this is a loop of resets, which can be
   remediated only by driver reload.

This patch sets the tx_max_header_size for the TX rings
upon running ena_restore_device.

Fixes: 09da13f46192 ("linux/ena: Add devlink support to ENA")
Signed-off-by: David Arinzon <darinzon@amazon.com>

kernel/linux/ena/ena_netdev.c
@@ -3717,6 +3717,7 @@ int ena_restore_device(struct ena_adapter *adapter)
 	for (i = 0 ; i < count; i++) {
 		txr = &adapter->tx_ring[i];
 		txr->tx_mem_queue_type = ena_dev->tx_mem_queue_type;
+		txr->tx_max_header_size = ena_dev->tx_max_header_size;
 	}
 
 	rc = ena_device_validate_params(adapter, &get_feat_ctx);