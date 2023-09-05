amzn/amzn-drivers

commit 58ba634a1aa8d15eb4697c6d74a232ea88795965

linux/ena: Backport XDP features netlink advertisement
Commits [1] and [2] added advertisement of XDP features which the
driver supports. These features can be queried using netlink
generic yaml interface, e.g.
1) cd net-next/tools/net/ynl
2) ./cli.py --spec ./../../../Documentation/netlink/specs/netdev.yaml
   --dump dev-get

The output form:
    [{'ifindex': 1, 'xdp-features': set()},
     {'ifindex': 2, 'xdp-features': set()},
     {'ifindex': 6, 'xdp-features': {'redirect', 'basic'}}]

Note that ENA actually supports XDP only when it uses at most half of
the maximum available IO queues.

Also removed use of ENA_XDP_SUPPORT in ena_ethtool.c by adding a
ena_xdp_legal_queue_count() ena_xdp_present() stubs.

[1]:
https://git.kernel.org/pub/scm/linux/kernel/git/netdev/net-next.git/commit/?id=66c0e13ad236
[2]:
https://git.kernel.org/pub/scm/linux/kernel/git/netdev/net-next.git/commit/?id=7aa6dc351b92

Signed-off-by: Shay Agroskin <shayagr@amazon.com>

kernel/linux/ena/kcompat.h
@@ -1127,6 +1127,13 @@ static inline void ena_netif_napi_add(struct net_device *dev,
 #define devl_param_driverinit_value_set devlink_param_driverinit_value_set
 #endif
 
+#if LINUX_VERSION_CODE < KERNEL_VERSION(6, 3, 0)
+#define xdp_features_set_redirect_target(netdev, xdp_xmit_supported)
+#define xdp_features_clear_redirect_target(netdev)
+#define xdp_clear_features_flag(netdev)
+#define xdp_set_features_flag(netdev, features)
+#endif
+
 #if (RHEL_RELEASE_CODE && (RHEL_RELEASE_CODE <= RHEL_RELEASE_VERSION(7, 4))) || \
     (defined(UBUNTU_VERSION_CODE) && UBUNTU_VERSION_CODE < UBUNTU_VERSION(4, 5, 0, 0)) || \
     (defined(IS_UEK) && !ENA_KERNEL_VERSION_GTE(4, 1, 12, 105, 0, 0))

kernel/linux/ena/ena_ethtool.c
@@ -1262,14 +1262,20 @@ static int ena_set_channels(struct net_device *netdev,
 	struct ena_adapter *adapter = netdev_priv(netdev);
 	u32 count = channels->combined_count;
 	/* The check for max value is already done in ethtool */
-#ifdef ENA_XDP_SUPPORT
-	if (count < ENA_MIN_NUM_IO_QUEUES ||
-	    (ena_xdp_present(adapter) &&
-	    !ena_xdp_legal_queue_count(adapter, count)))
-#else
 	if (count < ENA_MIN_NUM_IO_QUEUES)
-#endif /* ENA_XDP_SUPPORT */
 		return -EINVAL;
+
+	if (!ena_xdp_legal_queue_count(adapter, count)) {
+		if (ena_xdp_present(adapter))
+			return -EINVAL;
+
+		xdp_clear_features_flag(netdev);
+	} else {
+		xdp_set_features_flag(netdev,
+				      NETDEV_XDP_ACT_BASIC |
+				      NETDEV_XDP_ACT_REDIRECT);
+	}
+
 
 	if (count > adapter->max_num_io_queues)
 		return -EINVAL;

kernel/linux/ena/ena_netdev.c
@@ -4734,6 +4734,12 @@ static int ena_probe(struct pci_dev *pdev, const struct pci_device_id *ent)
 
 	ena_config_debug_area(adapter);
 
+#ifdef ENA_XDP_NETLINK_ADVERTISEMENT
+	if (ena_xdp_legal_queue_count(adapter, adapter->num_io_queues))
+		netdev->xdp_features = NETDEV_XDP_ACT_BASIC |
+				       NETDEV_XDP_ACT_REDIRECT;
+
+#endif
 	memcpy(adapter->netdev->perm_addr, adapter->mac_addr, netdev->addr_len);
 
 	netif_carrier_off(netdev);

kernel/linux/ena/ena_xdp.c
@@ -365,7 +365,9 @@ static int ena_xdp_set(struct net_device *netdev, struct netdev_bpf *bpf)
 				if (rc)
 					return rc;
 			}
+			xdp_features_set_redirect_target(netdev, false);
 		} else if (old_bpf_prog) {
+			xdp_features_clear_redirect_target(netdev);
 			netif_dbg(adapter, drv, adapter->netdev,
 				  "Removing XDP program\n");
 

kernel/linux/ena/ena_xdp.h
@@ -209,6 +209,16 @@ static inline int ena_xdp_register_rxq_info(struct ena_ring *rx_ring)
 
 static inline void ena_xdp_unregister_rxq_info(struct ena_ring *rx_ring) {}
 
+static inline bool ena_xdp_legal_queue_count(struct ena_adapter *adapter,
+					     u32 queues)
+{
+	return false;
+}
+
+static inline bool ena_xdp_present(struct ena_adapter *adapter)
+{
+	return false;
+}
 #endif /* ENA_XDP_SUPPORT */
 #ifndef ENA_AF_XDP_SUPPORT /* stabs for AF XDP code */
 

commit 95d730bfd3b92dec0da7d503b0543db1f300c1a0

linux/ena: Define ENA_XDP_NETLINK_ADVERTISEMENT for kernel > 6.3
The else statment that defines ENA_XDP_NETLINK_ADVERTISEMENT was left
out unintentionally in the previous release.
This commit defnies ENA_XDP_NETLINK_ADVERTISEMENT.

Fixes: 58ba634a1aa8 ("linux/ena: Backport XDP features netlink advertisement")
Signed-off-by: Osama Abboud <osamaabb@amazon.com>

kernel/linux/ena/kcompat.h
@@ -1139,6 +1139,8 @@ static inline void ena_netif_napi_add(struct net_device *dev,
 #define xdp_features_clear_redirect_target(netdev)
 #define xdp_clear_features_flag(netdev)
 #define xdp_set_features_flag(netdev, features)
+#else /* LINUX_VERSION_CODE < KERNEL_VERSION(6, 3, 0) */
+#define ENA_XDP_NETLINK_ADVERTISEMENT
 #endif
 
 #if (RHEL_RELEASE_CODE && (RHEL_RELEASE_CODE <= RHEL_RELEASE_VERSION(7, 4))) || \