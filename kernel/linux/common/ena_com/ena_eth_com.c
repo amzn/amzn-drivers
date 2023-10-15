e4ee1ad9b5c4963d6df21a7bfea53a251cc9abdd
David Arinzon <darinzon@amazon.com>
2023-10-15 18:01:14 +0300

    linux/ena: RX ring submission queue optimization
    RX ring submission queue descriptors are always located in host memory.
    This optimization replaces the generic descriptor retrieval method
    with a tailored method for host memory type descriptors to avoid
    an unnecessary if statement.
    
    Signed-off-by: Evgeny Ostrovsky <evostrov@amazon.com>

--- kernel/linux/common/ena_com/ena_eth_com.c
+++ kernel/linux/common/ena_com/ena_eth_com.c
@@ -624,9 +624,8 @@ int ena_com_add_single_rx_desc(struct ena_com_io_sq *io_sq,
 	if (unlikely(!ena_com_sq_have_enough_space(io_sq, 1)))
 		return -ENOSPC;
 
-	desc = get_sq_desc(io_sq);
-	if (unlikely(!desc))
-		return -EFAULT;
+	/* virt_addr allocation success is checked before calling this function */
+	desc = get_sq_desc_regular_queue(io_sq);
 
 	memset(desc, 0x0, sizeof(struct ena_eth_io_rx_desc));
 
