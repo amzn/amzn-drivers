amzn/amzn-drivers

commit e89ffd531e902649a656eddeb8fe95620ca67a65

linux/ena: Add ethtool support for modifying large LLQ
This patch adds the capability to modify the maximum
number of bytes of a TX packet's payload the
driver writes directly to the underlying device.

The new ethtool parameter is called tx-push-buffer-len and in ENA it
can have the values 96 and 224 where the former indicates regular
entry size LLQ and the latter large LLQ header.

The upstream patchset which adds the parameter support to the kernel
can be found in [1].

The 40 magic number when formatting a string was chosen as a very
wide safe margin to the length of the string ", 224"
(which is one of the support values for tx_push_buf reported to
the user).

This commit also adds macros which define
LLQ entry's header part size in bytes.

The header part size is calculated by
    (LLQ_ENTRY_SIZE - DESC_NUMBER * DESC_LEN)

While DESC_LEN is constant (size of a struct) the other two variables
are received from the device and can theoretically change. For example
- LLQ entry size can be either 128 or 256
- number of descriptors can vary from 1 to 8, but currently
  it's set to 2

[1]:
https://lore.kernel.org/netdev/20230320132523.3203254-1-shayagr@amazon.com/

Signed-off-by: Shay Agroskin <shayagr@amazon.com>

kernel/linux/ena/ena_netdev.c
@@ -92,8 +92,6 @@ MODULE_DEVICE_TABLE(pci, ena_pci_tbl);
 
 static int ena_rss_init_default(struct ena_adapter *adapter);
 static void check_for_admin_com_state(struct ena_adapter *adapter);
-static int ena_calc_io_queue_size(struct ena_adapter *adapter,
-				   struct ena_com_dev_get_features_ctx *get_feat_ctx);
 static void ena_set_dev_offloads(struct ena_com_dev_get_features_ctx *feat,
 				 struct net_device *netdev);
 
@@ -2680,11 +2678,13 @@ int ena_set_lpc_state(struct ena_adapter *adapter, bool enabled)
 	return 0;
 }
 
-int ena_update_queue_sizes(struct ena_adapter *adapter,
-			   u32 new_tx_size,
-			   u32 new_rx_size)
+int ena_update_queue_params(struct ena_adapter *adapter,
+			    u32 new_tx_size,
+			    u32 new_rx_size,
+			    u32 new_llq_header_len)
 {
-	bool dev_was_up;
+	bool dev_was_up, large_llq_changed = false;
+	int rc = 0;
 
 	dev_was_up = test_bit(ENA_FLAG_DEV_UP, &adapter->flags);
 	ena_close(adapter->netdev);
@@ -2694,7 +2694,28 @@ int ena_update_queue_sizes(struct ena_adapter *adapter,
 			  0,
 			  adapter->xdp_num_queues +
 			  adapter->num_io_queues);
-	return dev_was_up ? ena_up(adapter) : 0;
+
+#ifdef ENA_LARGE_LLQ_ETHTOOL
+	large_llq_changed = adapter->ena_dev->tx_mem_queue_type ==
+			    ENA_ADMIN_PLACEMENT_POLICY_DEV;
+	large_llq_changed &=
+		new_llq_header_len != adapter->ena_dev->tx_max_header_size;
+
+#endif /* ENA_LARGE_LLQ_ETHTOOL */
+	/* a check that the configuration is valid is done by caller */
+	if (large_llq_changed) {
+		bool large_llq_used;
+
+		large_llq_used = (adapter->llq_policy == ENA_LLQ_HEADER_SIZE_POLICY_LARGE);
+
+		/* devlink can modify this value so let it know about the change */
+		ena_devlink_set_large_llq_param(adapter, !large_llq_used);
+
+		ena_destroy_device(adapter, false);
+		rc = ena_restore_device(adapter);
+	}
+
+	return dev_was_up && !rc ? ena_up(adapter) : rc;
 }
 
 int ena_set_rx_copybreak(struct ena_adapter *adapter, u32 rx_copybreak)
@@ -3415,6 +3436,111 @@ static const struct net_device_ops ena_netdev_ops = {
 #endif /* ENA_XDP_SUPPORT */
 };
 
+static int ena_calc_io_queue_size(struct ena_adapter *adapter,
+				  struct ena_com_dev_get_features_ctx *get_feat_ctx)
+{
+	struct ena_admin_feature_llq_desc *llq = &get_feat_ctx->llq;
+	struct ena_com_dev *ena_dev = adapter->ena_dev;
+	u32 tx_queue_size = ENA_DEFAULT_RING_SIZE;
+	u32 max_tx_queue_size;
+	u32 max_rx_queue_size;
+
+	/* If this function is called after driver load, the ring sizes have already
+	 * been configured. Take it into account when recalculating ring size.
+	 */
+	if (adapter->tx_ring->ring_size)
+		tx_queue_size = adapter->tx_ring->ring_size;
+
+	if (adapter->rx_ring->ring_size)
+		rx_queue_size = adapter->rx_ring->ring_size;
+
+	if (ena_dev->supported_features & BIT(ENA_ADMIN_MAX_QUEUES_EXT)) {
+		struct ena_admin_queue_ext_feature_fields *max_queue_ext =
+			&get_feat_ctx->max_queue_ext.max_queue_ext;
+		max_rx_queue_size = min_t(u32, max_queue_ext->max_rx_cq_depth,
+					  max_queue_ext->max_rx_sq_depth);
+		max_tx_queue_size = max_queue_ext->max_tx_cq_depth;
+
+		if (ena_dev->tx_mem_queue_type == ENA_ADMIN_PLACEMENT_POLICY_DEV)
+			max_tx_queue_size = min_t(u32, max_tx_queue_size,
+						  llq->max_llq_depth);
+		else
+			max_tx_queue_size = min_t(u32, max_tx_queue_size,
+						  max_queue_ext->max_tx_sq_depth);
+
+		adapter->max_tx_sgl_size = min_t(u16, ENA_PKT_MAX_BUFS,
+						 max_queue_ext->max_per_packet_tx_descs);
+		adapter->max_rx_sgl_size = min_t(u16, ENA_PKT_MAX_BUFS,
+						 max_queue_ext->max_per_packet_rx_descs);
+	} else {
+		struct ena_admin_queue_feature_desc *max_queues =
+			&get_feat_ctx->max_queues;
+		max_rx_queue_size = min_t(u32, max_queues->max_cq_depth,
+					  max_queues->max_sq_depth);
+		max_tx_queue_size = max_queues->max_cq_depth;
+
+		if (ena_dev->tx_mem_queue_type == ENA_ADMIN_PLACEMENT_POLICY_DEV)
+			max_tx_queue_size = min_t(u32, max_tx_queue_size,
+						  llq->max_llq_depth);
+		else
+			max_tx_queue_size = min_t(u32, max_tx_queue_size,
+						  max_queues->max_sq_depth);
+
+		adapter->max_tx_sgl_size = min_t(u16, ENA_PKT_MAX_BUFS,
+						 max_queues->max_packet_tx_descs);
+		adapter->max_rx_sgl_size = min_t(u16, ENA_PKT_MAX_BUFS,
+						 max_queues->max_packet_rx_descs);
+	}
+
+	max_tx_queue_size = rounddown_pow_of_two(max_tx_queue_size);
+	max_rx_queue_size = rounddown_pow_of_two(max_rx_queue_size);
+
+	if (max_tx_queue_size < ENA_MIN_RING_SIZE) {
+		netdev_err(adapter->netdev, "Device max TX queue size: %d < minimum: %d\n",
+			   max_tx_queue_size, ENA_MIN_RING_SIZE);
+		return -EFAULT;
+	}
+
+	if (max_rx_queue_size < ENA_MIN_RING_SIZE) {
+		netdev_err(adapter->netdev, "Device max RX queue size: %d < minimum: %d\n",
+			   max_rx_queue_size, ENA_MIN_RING_SIZE);
+		return -EFAULT;
+	}
+
+	/* When forcing large headers, we multiply the entry size by 2, and therefore divide
+	 * the queue size by 2, leaving the amount of memory used by the queues unchanged.
+	 */
+	if (adapter->llq_policy == ENA_LLQ_HEADER_SIZE_POLICY_LARGE) {
+		if (ena_dev->tx_mem_queue_type == ENA_ADMIN_PLACEMENT_POLICY_DEV) {
+			max_tx_queue_size /= 2;
+			dev_info(&adapter->pdev->dev,
+				 "Forcing large headers and decreasing maximum TX queue size to %d\n",
+				 max_tx_queue_size);
+		} else {
+			dev_err(&adapter->pdev->dev,
+				"Forcing large headers failed: LLQ is disabled or device does not support large headers\n");
+
+			adapter->llq_policy = ENA_LLQ_HEADER_SIZE_POLICY_NORMAL;
+			ena_devlink_disable_large_llq_header_param(adapter->devlink);
+		}
+	}
+
+	tx_queue_size = clamp_val(tx_queue_size, ENA_MIN_RING_SIZE,
+				  max_tx_queue_size);
+	rx_queue_size = clamp_val(rx_queue_size, ENA_MIN_RING_SIZE,
+				  max_rx_queue_size);
+
+	tx_queue_size = rounddown_pow_of_two(tx_queue_size);
+	rx_queue_size = rounddown_pow_of_two(rx_queue_size);
+
+	adapter->max_tx_ring_size  = max_tx_queue_size;
+	adapter->max_rx_ring_size = max_rx_queue_size;
+	adapter->requested_tx_ring_size = tx_queue_size;
+	adapter->requested_rx_ring_size = rx_queue_size;
+
+	return 0;
+}
+
 static int ena_device_validate_params(struct ena_adapter *adapter,
 				      struct ena_com_dev_get_features_ctx *get_feat_ctx)
 {
@@ -3462,7 +3588,7 @@ static int ena_set_llq_configurations(struct ena_adapter *adapter,
 	llq_config->llq_num_decs_before_header = ENA_ADMIN_LLQ_NUM_DESCS_BEFORE_HEADER_2;
 
 	adapter->large_llq_header_supported =
-		!!(ena_dev->supported_features & (1 << ENA_ADMIN_LLQ));
+		!!(ena_dev->supported_features & BIT(ENA_ADMIN_LLQ));
 	adapter->large_llq_header_supported &=
 		!!(llq->entry_size_ctrl_supported &
 			ENA_ADMIN_LIST_ENTRY_SIZE_256B);
@@ -4384,114 +4510,6 @@ static void ena_release_bars(struct ena_com_dev *ena_dev, struct pci_dev *pdev)
 	int release_bars = pci_select_bars(pdev, IORESOURCE_MEM) & ENA_BAR_MASK;
 
 	pci_release_selected_regions(pdev, release_bars);
-}
-
-
-static int ena_calc_io_queue_size(struct ena_adapter *adapter,
-				   struct ena_com_dev_get_features_ctx *get_feat_ctx)
-{
-	struct ena_admin_feature_llq_desc *llq = &get_feat_ctx->llq;
-	struct ena_com_dev *ena_dev = adapter->ena_dev;
-	u32 tx_queue_size = ENA_DEFAULT_RING_SIZE;
-	bool tx_configured, rx_configured;
-	u32 max_tx_queue_size;
-	u32 max_rx_queue_size;
-
-	/* If this function is called after driver load, the ring sizes have
-	 * already been configured. Take it into account when recalculating ring
-	 * size.
-	 */
-	tx_configured = !!adapter->tx_ring[0].ring_size;
-	rx_configured = !!adapter->rx_ring[0].ring_size;
-	tx_queue_size = tx_configured ? adapter->tx_ring[0].ring_size : tx_queue_size;
-	rx_queue_size = rx_configured ? adapter->rx_ring[0].ring_size : rx_queue_size;
-
-	if (ena_dev->supported_features & BIT(ENA_ADMIN_MAX_QUEUES_EXT)) {
-		struct ena_admin_queue_ext_feature_fields *max_queue_ext =
-			&get_feat_ctx->max_queue_ext.max_queue_ext;
-		max_rx_queue_size = min_t(u32, max_queue_ext->max_rx_cq_depth,
-					  max_queue_ext->max_rx_sq_depth);
-		max_tx_queue_size = max_queue_ext->max_tx_cq_depth;
-
-		if (ena_dev->tx_mem_queue_type == ENA_ADMIN_PLACEMENT_POLICY_DEV)
-			max_tx_queue_size = min_t(u32, max_tx_queue_size,
-						  llq->max_llq_depth);
-		else
-			max_tx_queue_size = min_t(u32, max_tx_queue_size,
-						  max_queue_ext->max_tx_sq_depth);
-
-		adapter->max_tx_sgl_size = min_t(u16, ENA_PKT_MAX_BUFS,
-						 max_queue_ext->max_per_packet_tx_descs);
-		adapter->max_rx_sgl_size = min_t(u16, ENA_PKT_MAX_BUFS,
-						 max_queue_ext->max_per_packet_rx_descs);
-	} else {
-		struct ena_admin_queue_feature_desc *max_queues =
-			&get_feat_ctx->max_queues;
-		max_rx_queue_size = min_t(u32, max_queues->max_cq_depth,
-					  max_queues->max_sq_depth);
-		max_tx_queue_size = max_queues->max_cq_depth;
-
-		if (ena_dev->tx_mem_queue_type == ENA_ADMIN_PLACEMENT_POLICY_DEV)
-			max_tx_queue_size = min_t(u32, max_tx_queue_size,
-						  llq->max_llq_depth);
-		else
-			max_tx_queue_size = min_t(u32, max_tx_queue_size,
-						  max_queues->max_sq_depth);
-
-		adapter->max_tx_sgl_size = min_t(u16, ENA_PKT_MAX_BUFS,
-						 max_queues->max_packet_tx_descs);
-		adapter->max_rx_sgl_size = min_t(u16, ENA_PKT_MAX_BUFS,
-						 max_queues->max_packet_rx_descs);
-	}
-
-	max_tx_queue_size = rounddown_pow_of_two(max_tx_queue_size);
-	max_rx_queue_size = rounddown_pow_of_two(max_rx_queue_size);
-
-	if (max_tx_queue_size < ENA_MIN_RING_SIZE) {
-		netdev_err(adapter->netdev, "Device max TX queue size: %d < minimum: %d\n",
-			   max_tx_queue_size, ENA_MIN_RING_SIZE);
-		return -EFAULT;
-	}
-
-	if (max_rx_queue_size < ENA_MIN_RING_SIZE) {
-		netdev_err(adapter->netdev, "Device max RX queue size: %d < minimum: %d\n",
-			   max_rx_queue_size, ENA_MIN_RING_SIZE);
-		return -EFAULT;
-	}
-
-	/* When forcing large headers, we multiply the entry size by 2,
-	 * and therefore divide the queue size by 2, leaving the amount
-	 * of memory used by the queues unchanged.
-	 */
-	if (adapter->llq_policy == ENA_LLQ_HEADER_SIZE_POLICY_LARGE) {
-		if (ena_dev->tx_mem_queue_type == ENA_ADMIN_PLACEMENT_POLICY_DEV) {
-			max_tx_queue_size /= 2;
-			dev_info(&adapter->pdev->dev,
-				 "Forcing large headers and decreasing maximum TX queue size to %d\n",
-				 max_tx_queue_size);
-		} else {
-			dev_err(&adapter->pdev->dev,
-				"Forcing large headers failed: LLQ is disabled or device does not support large headers\n");
-
-			adapter->llq_policy = ENA_LLQ_HEADER_SIZE_POLICY_NORMAL;
-			ena_devlink_disable_large_llq_header_param(adapter->devlink);
-		}
-	}
-
-	tx_queue_size = clamp_val(tx_queue_size, ENA_MIN_RING_SIZE,
-				  max_tx_queue_size);
-	rx_queue_size = clamp_val(rx_queue_size, ENA_MIN_RING_SIZE,
-				  max_rx_queue_size);
-
-	tx_queue_size = rounddown_pow_of_two(tx_queue_size);
-	rx_queue_size = rounddown_pow_of_two(rx_queue_size);
-
-	adapter->max_tx_ring_size  = max_tx_queue_size;
-	adapter->max_rx_ring_size = max_rx_queue_size;
-	adapter->requested_tx_ring_size = tx_queue_size;
-	adapter->requested_rx_ring_size = rx_queue_size;
-
-	return 0;
 }
 
 /* ena_probe - Device Initialization Routine

kernel/linux/common/ena_com/ena_eth_com.h
@@ -8,6 +8,11 @@
 
 #include "ena_com.h"
 
+/* we allow 2 DMA descriptors per LLQ entry */
+#define ENA_LLQ_ENTRY_DESC_CHUNK_SIZE	(2 * sizeof(struct ena_eth_io_tx_desc))
+#define ENA_LLQ_HEADER		(128UL - ENA_LLQ_ENTRY_DESC_CHUNK_SIZE)
+#define ENA_LLQ_LARGE_HEADER	(256UL - ENA_LLQ_ENTRY_DESC_CHUNK_SIZE)
+
 struct ena_com_tx_ctx {
 	struct ena_com_tx_meta ena_meta;
 	struct ena_com_buf *ena_bufs;

kernel/linux/ena/ena_devlink.c
@@ -264,6 +264,16 @@ static const struct devlink_ops ena_devlink_ops = {
 #endif
 };
 
+void ena_devlink_set_large_llq_param(struct ena_adapter *adapter, bool enable)
+{
+	union devlink_param_value value;
+
+	value.vbool = enable;
+	devlink_param_driverinit_value_set(adapter->devlink,
+					   ENA_DEVLINK_PARAM_ID_LLQ_HEADER_SIZE,
+					   value);
+}
+
 static int ena_devlink_configure_params(struct devlink *devlink)
 {
 	struct ena_adapter *adapter = ENA_DEVLINK_PRIV(devlink);

kernel/linux/ena/ena_devlink.h
@@ -22,6 +22,7 @@ void ena_devlink_unregister(struct devlink *devlink);
 void ena_devlink_params_get(struct devlink *devlink);
 void ena_devlink_disable_large_llq_header_param(struct devlink *devlink);
 void ena_devlink_disable_phc_param(struct devlink *devlink);
+void ena_devlink_set_large_llq_param(struct ena_adapter *adapter, bool enable);
 
 #else /* ENA_DEVLINK_SUPPORT */
 #ifdef ENA_NO_DEVLINK_HEADERS
@@ -39,6 +40,7 @@ static inline void ena_devlink_unregister(struct devlink *devlink) { }
 static inline void ena_devlink_params_get(struct devlink *devlink) { }
 static inline void ena_devlink_disable_large_llq_header_param(struct devlink *devlink) { }
 static inline void ena_devlink_disable_phc_param(struct devlink *devlink) { }
+static inline void ena_devlink_set_large_llq_param(struct ena_adapter *adapter, bool enable) { }
 
 #endif /* ENA_DEVLINK_SUPPORT */
 #endif /* DEVLINK_H */

kernel/linux/ena/ena_ethtool.c
@@ -749,6 +749,21 @@ static void ena_get_ringparam(struct net_device *netdev,
 
 	ring->tx_max_pending = adapter->max_tx_ring_size;
 	ring->rx_max_pending = adapter->max_rx_ring_size;
+#ifdef ENA_LARGE_LLQ_ETHTOOL
+	if (adapter->ena_dev->tx_mem_queue_type == ENA_ADMIN_PLACEMENT_POLICY_DEV) {
+		bool large_llq_supported = adapter->large_llq_header_supported;
+
+		kernel_ring->tx_push_buf_len = adapter->ena_dev->tx_max_header_size;
+		if (large_llq_supported)
+			kernel_ring->tx_push_buf_max_len = ENA_LLQ_LARGE_HEADER;
+		else
+			kernel_ring->tx_push_buf_max_len = ENA_LLQ_HEADER;
+	} else {
+		kernel_ring->tx_push_buf_max_len = 0;
+		kernel_ring->tx_push_buf_len = 0;
+	}
+
+#endif
 	ring->tx_pending = adapter->tx_ring[0].ring_size;
 	ring->rx_pending = adapter->rx_ring[0].ring_size;
 }
@@ -763,7 +778,8 @@ static int ena_set_ringparam(struct net_device *netdev,
 #endif
 {
 	struct ena_adapter *adapter = netdev_priv(netdev);
-	u32 new_tx_size, new_rx_size;
+	u32 new_tx_size, new_rx_size, new_tx_push_buf_len;
+	bool changed = false;
 
 	if (ring->rx_mini_pending || ring->rx_jumbo_pending)
 		return -EINVAL;
@@ -776,11 +792,47 @@ static int ena_set_ringparam(struct net_device *netdev,
 				adapter->max_rx_ring_size);
 	new_rx_size = rounddown_pow_of_two(new_rx_size);
 
-	if (new_tx_size == adapter->requested_tx_ring_size &&
-	    new_rx_size == adapter->requested_rx_ring_size)
+	changed |= new_tx_size != adapter->requested_tx_ring_size ||
+		   new_rx_size != adapter->requested_rx_ring_size;
+
+	/* This value is ignored if LLQ is not supported */
+	new_tx_push_buf_len = adapter->ena_dev->tx_max_header_size;
+#ifdef ENA_LARGE_LLQ_ETHTOOL
+
+	/* Validate that the push buffer is supported on the underlying device */
+	if (kernel_ring->tx_push_buf_len) {
+		enum ena_admin_placement_policy_type placement;
+
+		new_tx_push_buf_len = kernel_ring->tx_push_buf_len;
+
+		placement = adapter->ena_dev->tx_mem_queue_type;
+		if (placement == ENA_ADMIN_PLACEMENT_POLICY_HOST)
+			return -EOPNOTSUPP;
+
+		if (new_tx_push_buf_len != ENA_LLQ_HEADER &&
+		    new_tx_push_buf_len != ENA_LLQ_LARGE_HEADER) {
+			bool large_llq_sup = adapter->large_llq_header_supported;
+			char large_llq_size_str[40];
+
+			snprintf(large_llq_size_str, 40, ", %lu", ENA_LLQ_LARGE_HEADER);
+
+			NL_SET_ERR_MSG_FMT_MOD(extack,
+					       "Supported tx push buff values: [%lu%s]",
+					       ENA_LLQ_HEADER,
+					       large_llq_sup ? large_llq_size_str : "");
+
+			return -EINVAL;
+		}
+
+		changed |= new_tx_push_buf_len != adapter->ena_dev->tx_max_header_size;
+	}
+
+#endif
+	if (!changed)
 		return 0;
 
-	return ena_update_queue_sizes(adapter, new_tx_size, new_rx_size);
+	return ena_update_queue_params(adapter, new_tx_size, new_rx_size,
+				       new_tx_push_buf_len);
 }
 
 #ifdef ETHTOOL_GRXRINGS
@@ -1288,6 +1340,9 @@ static const struct ethtool_ops ena_ethtool_ops = {
 #if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 7, 0)
 	.supported_coalesce_params = ETHTOOL_COALESCE_USECS |
 				     ETHTOOL_COALESCE_USE_ADAPTIVE_RX,
+#endif
+#ifdef ENA_LARGE_LLQ_ETHTOOL
+	.supported_ring_params	= ETHTOOL_RING_USE_TX_PUSH_BUF_LEN,
 #endif
 #if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 6, 0)
 	.get_link_ksettings	= ena_get_link_ksettings,

kernel/linux/ena/ena_netdev.h
@@ -493,9 +493,10 @@ void ena_dump_stats_to_buf(struct ena_adapter *adapter, u8 *buf);
 
 int ena_set_lpc_state(struct ena_adapter *adapter, bool enabled);
 
-int ena_update_queue_sizes(struct ena_adapter *adapter,
-			   u32 new_tx_size,
-			   u32 new_rx_size);
+int ena_update_queue_params(struct ena_adapter *adapter,
+			    u32 new_tx_size,
+			    u32 new_rx_size,
+			    u32 new_llq_header_len);
 
 int ena_update_queue_count(struct ena_adapter *adapter, u32 new_channel_count);
 

kernel/linux/ena/kcompat.h
@@ -1118,6 +1118,10 @@ static inline void ena_netif_napi_add(struct net_device *dev,
 #endif /* LINUX_VERSION_CODE < KERNEL_VERSION(6, 1, 0) */
 }
 
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(6, 4, 0)
+#define ENA_LARGE_LLQ_ETHTOOL
+#endif
+
 #if defined(ENA_DEVLINK_SUPPORT) && LINUX_VERSION_CODE < KERNEL_VERSION(6, 3, 0)
 #define devl_param_driverinit_value_get devlink_param_driverinit_value_get
 #define devl_param_driverinit_value_set devlink_param_driverinit_value_set

commit e4e9c87893db1820b50bfde029de1acca880647a

linux/ena: Fix large LLQ in ethtool when devlink is disabled
Due to a bad rebase on top of large LLQ recommendation patch [1] the
value of 'adapter->llq_policy' wasn't changed to reflect changes in LLQ
entry size when modified using ethtool [2]. This can result in leaving
LLQ entry size the same.

The bug is mitigated in case devlink is configured
(i.e. has CONFIG_DEVLINK set in its kernel and the kernel version is
newer than 4.19).

[1]: commit 5366abefe205("linux/ena: Support LLQ entry size recommendation from device")
[2]: See Fixes

Fixes: e89ffd531e90 ("linux/ena: Add ethtool support for modifying large LLQ")
Signed-off-by: Shay Agroskin <shayagr@amazon.com>

kernel/linux/ena/ena_netdev.c
@@ -2724,12 +2724,11 @@ int ena_update_queue_params(struct ena_adapter *adapter,
 #endif /* ENA_LARGE_LLQ_ETHTOOL */
 	/* a check that the configuration is valid is done by caller */
 	if (large_llq_changed) {
-		bool large_llq_used;
+		bool large_llq_requested = new_llq_header_len == ENA_LLQ_LARGE_HEADER;
 
-		large_llq_used = (adapter->llq_policy == ENA_LLQ_HEADER_SIZE_POLICY_LARGE);
-
-		/* devlink can modify this value so let it know about the change */
-		ena_devlink_set_large_llq_param(adapter, !large_llq_used);
+		adapter->llq_policy = large_llq_requested ?
+					ENA_LLQ_HEADER_SIZE_POLICY_LARGE :
+					ENA_LLQ_HEADER_SIZE_POLICY_NORMAL;
 
 		ena_destroy_device(adapter, false);
 		rc = ena_restore_device(adapter);