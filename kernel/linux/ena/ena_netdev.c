amzn/amzn-drivers

commit 5366abefe205e8786cd8b994299494c3d6dec08b

linux/ena: Support LLQ entry size recommendation from device
ENA driver will use LLQ entry size according to device's
recommended size which is being published via ENA_ADMIN_GET_FEATURE
command (for ENA_ADMIN_LLQ feature).

User selection of LLQ entry size is still the highest priority and
will override any device recommendation.

new definitions:
(1) `entry_size_recommended` field was introduced in the ENA_ADMIN_LLQ
    feature struct and is being used to set LLQ size in case user did
    not specify otherwise.
(2) enum llq_feature_version was introduced to allow struct changes.

From now on, when querying ENA_ADMIN_LLQ feature, we use current version
(ENA_ADMIN_LLQ_FEATURE_VERSION_1) instead of hard-coded 0.

Signed-off-by: Gal Inbar <ginbar@amazon.com>

kernel/linux/ena/ena_netdev.c
@@ -61,7 +61,8 @@ static int rx_queue_size = ENA_DEFAULT_RING_SIZE;
 module_param(rx_queue_size, int, 0444);
 MODULE_PARM_DESC(rx_queue_size, "Rx queue size. The size should be a power of 2. Depending on instance type, max value can be up to 16K\n");
 
-static int force_large_llq_header = 0;
+#define FORCE_LARGE_LLQ_HEADER_UNINIT_VALUE 0xFFFF
+static int force_large_llq_header = FORCE_LARGE_LLQ_HEADER_UNINIT_VALUE;
 module_param(force_large_llq_header, int, 0444);
 MODULE_PARM_DESC(force_large_llq_header, "Increases maximum supported header size in LLQ mode to 224 bytes, while reducing the maximum TX queue size by half.\n");
 
@@ -3437,11 +3438,24 @@ static int ena_device_validate_params(struct ena_adapter *adapter,
 	return 0;
 }
 
-static void set_default_llq_configurations(struct ena_adapter *adapter,
-					   struct ena_llq_configurations *llq_config,
-					   struct ena_admin_feature_llq_desc *llq)
+static void ena_set_forced_llq_size_policy(struct ena_adapter *adapter)
+{
+	/* policy will be set according to device recommendation unless user
+	 * forced either large/normal size
+	 */
+	if (force_large_llq_header != FORCE_LARGE_LLQ_HEADER_UNINIT_VALUE) {
+		/* user selection is prioritized on top of device recommendation */
+		adapter->llq_policy = force_large_llq_header ? ENA_LLQ_HEADER_SIZE_POLICY_LARGE :
+							       ENA_LLQ_HEADER_SIZE_POLICY_NORMAL;
+	}
+}
+
+static int ena_set_llq_configurations(struct ena_adapter *adapter,
+				      struct ena_llq_configurations *llq_config,
+				      struct ena_admin_feature_llq_desc *llq)
 {
 	struct ena_com_dev *ena_dev = adapter->ena_dev;
+	bool use_large_llq;
 
 	llq_config->llq_header_location = ENA_ADMIN_INLINE_HEADER;
 	llq_config->llq_stride_ctrl = ENA_ADMIN_MULTIPLE_DESCS_PER_ENTRY;
@@ -3453,14 +3467,23 @@ static void set_default_llq_configurations(struct ena_adapter *adapter,
 		!!(llq->entry_size_ctrl_supported &
 			ENA_ADMIN_LIST_ENTRY_SIZE_256B);
 
-	if ((llq->entry_size_ctrl_supported & ENA_ADMIN_LIST_ENTRY_SIZE_256B) &&
-	    adapter->large_llq_header_enabled) {
-		llq_config->llq_ring_entry_size = ENA_ADMIN_LIST_ENTRY_SIZE_256B;
-		llq_config->llq_ring_entry_size_value = 256;
-	} else {
+	use_large_llq = adapter->llq_policy != ENA_LLQ_HEADER_SIZE_POLICY_NORMAL;
+	use_large_llq &= adapter->large_llq_header_supported;
+
+	if (adapter->llq_policy == ENA_LLQ_HEADER_SIZE_POLICY_UNSPECIFIED)
+		use_large_llq &= (llq->entry_size_recommended == ENA_ADMIN_LIST_ENTRY_SIZE_256B);
+
+	if (!use_large_llq) {
 		llq_config->llq_ring_entry_size = ENA_ADMIN_LIST_ENTRY_SIZE_128B;
 		llq_config->llq_ring_entry_size_value = 128;
+		adapter->llq_policy = ENA_LLQ_HEADER_SIZE_POLICY_NORMAL;
+	} else {
+		llq_config->llq_ring_entry_size = ENA_ADMIN_LIST_ENTRY_SIZE_256B;
+		llq_config->llq_ring_entry_size_value = 256;
+		adapter->llq_policy = ENA_LLQ_HEADER_SIZE_POLICY_LARGE;
 	}
+
+	return 0;
 }
 
 static int ena_set_queues_placement_policy(struct pci_dev *pdev,
@@ -3622,7 +3645,11 @@ static int ena_device_init(struct ena_adapter *adapter, struct pci_dev *pdev,
 
 	*wd_state = !!(aenq_groups & BIT(ENA_ADMIN_KEEP_ALIVE));
 
-	set_default_llq_configurations(adapter, &llq_config, &get_feat_ctx->llq);
+	rc = ena_set_llq_configurations(adapter, &llq_config, &get_feat_ctx->llq);
+	if (rc) {
+		netdev_err(netdev, "Cannot set llq configuration rc= %d\n", rc);
+		goto err_admin_init;
+	}
 
 	rc = ena_set_queues_placement_policy(pdev, ena_dev, &get_feat_ctx->llq,
 					     &llq_config);
@@ -4426,9 +4453,8 @@ static int ena_calc_io_queue_size(struct ena_adapter *adapter,
 	 * and therefore divide the queue size by 2, leaving the amount
 	 * of memory used by the queues unchanged.
 	 */
-	if (adapter->large_llq_header_enabled) {
-		if ((llq->entry_size_ctrl_supported & ENA_ADMIN_LIST_ENTRY_SIZE_256B) &&
-		    (ena_dev->tx_mem_queue_type == ENA_ADMIN_PLACEMENT_POLICY_DEV)) {
+	if (adapter->llq_policy == ENA_LLQ_HEADER_SIZE_POLICY_LARGE) {
+		if (ena_dev->tx_mem_queue_type == ENA_ADMIN_PLACEMENT_POLICY_DEV) {
 			max_tx_queue_size /= 2;
 			dev_info(&adapter->pdev->dev,
 				 "Forcing large headers and decreasing maximum TX queue size to %d\n",
@@ -4437,7 +4463,7 @@ static int ena_calc_io_queue_size(struct ena_adapter *adapter,
 			dev_err(&adapter->pdev->dev,
 				"Forcing large headers failed: LLQ is disabled or device does not support large headers\n");
 
-			adapter->large_llq_header_enabled = false;
+			adapter->llq_policy = ENA_LLQ_HEADER_SIZE_POLICY_NORMAL;
 			ena_devlink_disable_large_llq_header_param(adapter->devlink);
 		}
 	}
@@ -4564,7 +4590,7 @@ static int ena_probe(struct pci_dev *pdev, const struct pci_device_id *ent)
 		goto err_netdev_destroy;
 	}
 
-	adapter->large_llq_header_enabled = !!force_large_llq_header;
+	ena_set_forced_llq_size_policy(adapter);
 
 #ifdef ENA_PHC_SUPPORT
 	ena_phc_enable(adapter, !!phc_enable);

kernel/linux/common/ena_com/ena_admin_defs.h
@@ -69,6 +69,14 @@ enum ena_admin_aq_feature_id {
 	ENA_ADMIN_FEATURES_OPCODE_NUM               = 32,
 };
 
+/* feature version for the set/get ENA_ADMIN_LLQ feature admin commands */
+enum ena_admin_llq_feature_version {
+	/* legacy base version in older drivers */
+	ENA_ADMIN_LLQ_FEATURE_VERSION_0_LEGACY      = 0,
+	/* support entry_size recommendation by device */
+	ENA_ADMIN_LLQ_FEATURE_VERSION_1             = 1,
+};
+
 /* device capabilities */
 enum ena_admin_aq_caps_id {
 	ENA_ADMIN_ENI_STATS                         = 0,
@@ -671,8 +679,17 @@ struct ena_admin_feature_llq_desc {
 	/* the stride control the driver selected to use */
 	u16 descriptors_stride_ctrl_enabled;
 
+	/* feature version of device resp to either GET/SET commands. */
+	u8 feature_version;
+
+	/* llq entry size recommended by the device,
+	 * values correlated to enum ena_admin_llq_ring_entry_size.
+	 * used only for GET command.
+	 */
+	u8 entry_size_recommended;
+
 	/* reserved */
-	u32 reserved1;
+	u8 reserved1[2];
 
 	/* accelerated low latency queues requirement. driver needs to
 	 * support those requirements in order to use accelerated llq

kernel/linux/common/ena_com/ena_com.c
@@ -2303,7 +2303,8 @@ int ena_com_get_dev_attr_feat(struct ena_com_dev *ena_dev,
 	else
 		return rc;
 
-	rc = ena_com_get_feature(ena_dev, &get_resp, ENA_ADMIN_LLQ, 0);
+	rc = ena_com_get_feature(ena_dev, &get_resp,
+				 ENA_ADMIN_LLQ, ENA_ADMIN_LLQ_FEATURE_VERSION_1);
 	if (!rc)
 		memcpy(&get_feat_ctx->llq, &get_resp.u.llq,
 		       sizeof(get_resp.u.llq));

kernel/linux/ena/ena_devlink.c
@@ -116,7 +116,8 @@ void ena_devlink_params_get(struct devlink *devlink)
 		return;
 	}
 
-	adapter->large_llq_header_enabled = val.vbool;
+	adapter->llq_policy = val.vbool ? ENA_LLQ_HEADER_SIZE_POLICY_LARGE :
+					  ENA_LLQ_HEADER_SIZE_POLICY_NORMAL;
 #ifdef ENA_PHC_SUPPORT
 
 	err = devl_param_driverinit_value_get(devlink, ENA_DEVLINK_PARAM_ID_PHC_ENABLE, &val);
@@ -276,7 +277,7 @@ static int ena_devlink_configure_params(struct devlink *devlink)
 		return rc;
 	}
 
-	value.vbool = adapter->large_llq_header_enabled;
+	value.vbool = (adapter->llq_policy == ENA_LLQ_HEADER_SIZE_POLICY_LARGE);
 	devl_param_driverinit_value_set(devlink,
 					ENA_DEVLINK_PARAM_ID_LLQ_HEADER_SIZE,
 					value);

kernel/linux/ena/ena_netdev.h
@@ -374,6 +374,17 @@ enum ena_flags_t {
 	ENA_FLAG_ONGOING_RESET
 };
 
+enum ena_llq_header_size_policy_t {
+	/* Intermediate policy until llq configuration is initialized
+	 * to either NORMAL or LARGE
+	 */
+	ENA_LLQ_HEADER_SIZE_POLICY_UNSPECIFIED = 0,
+	/* Policy for Normal size LLQ entry (128B) */
+	ENA_LLQ_HEADER_SIZE_POLICY_NORMAL,
+	/* Policy for Large size LLQ entry (256B) */
+	ENA_LLQ_HEADER_SIZE_POLICY_LARGE
+};
+
 /* adapter specific private data structure */
 struct ena_adapter {
 	struct ena_com_dev *ena_dev;
@@ -412,12 +423,12 @@ struct ena_adapter {
 
 	u32 msg_enable;
 
-	/* The flag is used for two purposes:
-	 * 1. Indicates that large LLQ has been requested.
+	/* The policy is used for two purposes:
+	 * 1. Indicates who decided on LLQ entry size (user / device)
 	 * 2. Indicates whether large LLQ is set or not after device
 	 *    initialization / configuration.
 	 */
-	bool large_llq_header_enabled;
+	enum ena_llq_header_size_policy_t llq_policy;
 	bool large_llq_header_supported;
 
 	u16 max_tx_sgl_size;

commit aef1003c45732ab02053a461b2373331e8d64a4b

linux/ena: Initialize llq_policy field for all cases
Commit [1] added the field llq_policy to struct ena_adapter,
which is being accessed in the code, and initialized to 0,
but the driver relies on one of the enum values to be 0,
which is not granted.

In case there is no such value in the enum, the field will be
initialized correctly.
There are two cases:
- force_large_llq_header is set: initialization in [2]
- force_large_llq_header is not set: wrong initialization to 0

This commit adds an explicit initialization to the field
for every case.

[1]: See Fixes
[2]: ena_set_forced_llq_size_policy() in ena_netdev.c

Fixes: 5366abefe205 ("linux/ena: Support LLQ entry size recommendation from device")
Signed-off-by: Shahar Itzko <itzko@amazon.com>

kernel/linux/ena/ena_netdev.c
@@ -4658,6 +4658,7 @@ static int ena_probe(struct pci_dev *pdev, const struct pci_device_id *ent)
 		goto err_netdev_destroy;
 	}
 
+	adapter->llq_policy = ENA_LLQ_HEADER_SIZE_POLICY_UNSPECIFIED;
 	ena_set_forced_llq_size_policy(adapter);
 
 #ifdef ENA_PHC_SUPPORT