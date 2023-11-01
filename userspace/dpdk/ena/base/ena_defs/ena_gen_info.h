8734667df536a4281105bf8592310a22af9a3e6d
Shai Brandes <shaibran@amazon.com>
2023-11-01 11:33:12 +0200

    dpdk/ena: add ena driver v2.8.0 folder

---
+++ userspace/dpdk/ena/base/ena_defs/ena_gen_info.h
@@ -0,0 +1,6 @@
+/* SPDX-License-Identifier: BSD-3-Clause
+ * Copyright (c) 2015-2020 Amazon.com, Inc. or its affiliates.
+ * All rights reserved.
+ */
+#define	ENA_GEN_DATE	"Thu 14 Apr 2022 12:50:30 PM IDT"
+#define	ENA_GEN_COMMIT	"35388392"
---
+++ userspace/dpdk/ena/base/ena_plat.h
@@ -0,0 +1,11 @@
+/* SPDX-License-Identifier: BSD-3-Clause
+ * Copyright (c) 2015-2019 Amazon.com, Inc. or its affiliates.
+ * All rights reserved.
+ */
+
+#ifndef ENA_PLAT_H_
+#define ENA_PLAT_H_
+
+#include <ena_plat_dpdk.h>
+
+#endif /* ENA_PLAT_H_ */
