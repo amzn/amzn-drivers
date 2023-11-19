amzn/amzn-drivers

commit ff05a31909bddf08e6c611073899b0ced98976d2

linux/ena: Count number of unique tx packets missing completion
Currently, we're reporting the number of missing tx packets including
duplicates in check_missing_comp_in_tx_queue() since we increment the
missing_tx counter before we check if this packet has been encountered
in previous calls.

Modify the counter to be incremented only in case this packet is not
seen before (tx_buf->print_once is not set).

Fixes: 6733e9e9cea8 ("linux/ena: Add more information on TX timeouts")
Signed-off-by: Osama Abboud <osamaabb@amazon.com>

kernel/linux/ena/ena_netdev.c
@@ -3915,8 +3915,6 @@ static int check_missing_comp_in_tx_queue(struct ena_adapter *adapter,
 				reset_reason = ENA_REGS_RESET_SUSPECTED_POLL_STARVATION;
 			}
 
-			missed_tx++;
-
 			if (tx_buf->print_once)
 				continue;
 
@@ -3924,6 +3922,7 @@ static int check_missing_comp_in_tx_queue(struct ena_adapter *adapter,
 				     "TX hasn't completed, qid %d, index %d. %u usecs from last napi execution, napi scheduled: %d\n",
 				     tx_ring->qid, i, time_since_last_napi, napi_scheduled);
 
+			missed_tx++;
 			tx_buf->print_once = 1;
 		}
 	}

commit b245e7367cde7927e88c33a72f54f840b1886efe

linux/ena: Count all currently missing TX completions in check
The change introduced in [1] caused only new missing TX
completions to be taken into account when checking whether
the threshold has been reached. In case there are a few new
missing TX completions in each check, the number will never
pass the threshold though the number of missing TX completions
accumulates.

This patch makes two changes:
1. Brings back the local counter that counts all existing
   missing TX completions in the queue and compare it to
   the threshold.
2. Adds a new local counter for new missing TX completions,
   to be used for updating the `missed_tx` stat.

[1]: See Fixes tag

Fixes: ff05a31909bd ("linux/ena: Count number of unique tx packets missing completion")

Signed-off-by: David Arinzon <darinzon@amazon.com>

kernel/linux/ena/ena_netdev.c
@@ -4070,11 +4070,11 @@ static int check_missing_comp_in_tx_queue(struct ena_adapter *adapter, struct en
 	struct net_device *netdev = adapter->netdev;
 	unsigned long jiffies_since_last_napi;
 	unsigned long jiffies_since_last_intr;
+	u32 missed_tx = 0, new_missed_tx = 0;
 	unsigned long graceful_timeout;
 	struct ena_tx_buffer *tx_buf;
 	unsigned long timeout;
 	int napi_scheduled;
-	u32 missed_tx = 0;
 	bool is_expired;
 	int i, rc = 0;
 
@@ -4117,20 +4117,24 @@ static int check_missing_comp_in_tx_queue(struct ena_adapter *adapter, struct en
 				reset_reason = ENA_REGS_RESET_SUSPECTED_POLL_STARVATION;
 			}
 
+			missed_tx++;
+
 			if (tx_buf->print_once)
 				continue;
+
+			/* Add new TX completions which are missed */
+			new_missed_tx++;
 
 			netif_notice(adapter, tx_err, netdev,
 				     "TX hasn't completed, qid %d, index %d. %u msecs since last interrupt, %u msecs since last napi execution, napi scheduled: %d\n",
 				     tx_ring->qid, i, jiffies_to_msecs(jiffies_since_last_intr),
 				     jiffies_to_msecs(jiffies_since_last_napi), napi_scheduled);
 
-			missed_tx++;
 			tx_buf->print_once = 1;
 		}
 	}
 
-	/* Checking if this TX ring got to max missing TX completes */
+	/* Checking if this TX ring missing TX completions have passed the threshold */
 	if (unlikely(missed_tx > missed_tx_thresh)) {
 		jiffies_since_last_intr = jiffies - READ_ONCE(ena_napi->last_intr_jiffies);
 		jiffies_since_last_napi = jiffies - READ_ONCE(tx_ring->tx_stats.last_napi_jiffies);
@@ -4156,7 +4160,8 @@ static int check_missing_comp_in_tx_queue(struct ena_adapter *adapter, struct en
 		rc = -EIO;
 	}
 
-	ena_increase_stat(&tx_ring->tx_stats.missed_tx, missed_tx, &tx_ring->syncp);
+	/* Add the newly discovered missing TX completions */
+	ena_increase_stat(&tx_ring->tx_stats.missed_tx, new_missed_tx, &tx_ring->syncp);
 
 	return rc;
 }