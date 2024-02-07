amzn/amzn-drivers

commit 2e9a716dbbb696c16755a90cace6820eea24d130

linux/ena: Move PHC info struct allocation
This struct is allocated upon PHC init flow, which
is too late for module param and devlink init params load.
This commit adds PHC alloc and free APIs. The alloc API
must be called upon ena_probe() before ena_devlink_params_get()
occurs, in order to enable flags to be ready for devlink
param update.

Signed-off-by: Amit Bernstein <amitbern@amazon.com>

kernel/linux/ena/ena_netdev.c
@@ -4516,12 +4516,18 @@ static int ena_probe(struct pci_dev *pdev, const struct pci_device_id *ent)
 
 	pci_set_drvdata(pdev, adapter);
 
+	rc = ena_phc_alloc(adapter);
+	if (rc) {
+		netdev_err(netdev, "ena_phc_alloc failed\n");
+		goto err_netdev_destroy;
+	}
+
 	adapter->large_llq_header_enabled = !!force_large_llq_header;
 
 	rc = ena_com_allocate_customer_metrics_buffer(ena_dev);
 	if (rc) {
 		netdev_err(netdev, "ena_com_allocate_customer_metrics_buffer failed\n");
-		goto err_netdev_destroy;
+		goto err_free_phc;
 	}
 
 	devlink = ena_devlink_alloc(adapter);
@@ -4690,6 +4696,8 @@ err_netdev_destroy:
 	free_netdev(netdev);
 err_metrics_destroy:
 	ena_com_delete_customer_metrics_buffer(ena_dev);
+err_free_phc:
+	ena_phc_free(adapter);
 err_free_region:
 	ena_release_bars(ena_dev, pdev);
 err_free_ena_dev:
@@ -4757,6 +4765,8 @@ static void __ena_shutoff(struct pci_dev *pdev, bool shutdown)
 	ena_com_delete_host_info(ena_dev);
 
 	ena_com_delete_customer_metrics_buffer(ena_dev);
+
+	ena_phc_free(adapter);
 
 	ena_release_bars(ena_dev, pdev);
 

kernel/linux/ena/ena_netdev.h
@@ -125,8 +125,10 @@
 
 struct ena_page_cache;
 
+#ifdef ENA_PHC_SUPPORT
 struct ena_phc_info;
 
+#endif
 struct ena_irq {
 	irq_handler_t handler;
 	void *data;
@@ -421,6 +423,10 @@ struct ena_adapter {
 	unsigned long missing_tx_completion_to;
 
 	char name[ENA_NAME_MAX_LEN];
+#ifdef ENA_PHC_SUPPORT
+
+	struct ena_phc_info *phc_info;
+#endif
 
 	unsigned long flags;
 	/* TX */
@@ -459,8 +465,6 @@ struct ena_adapter {
 #endif
 	u32 xdp_first_ring;
 	u32 xdp_num_queues;
-
-	struct ena_phc_info *phc_info;
 };
 
 void ena_set_ethtool_ops(struct net_device *netdev);

kernel/linux/ena/ena_phc.c
@@ -156,17 +156,37 @@ static int ena_phc_register(struct ena_adapter *adapter)
 
 bool ena_phc_enabled(struct ena_adapter *adapter)
 {
-	struct ena_phc_info *phc_info = adapter->phc_info;
-
-	return (phc_info && phc_info->clock);
+	return adapter->phc_info.clock;
 }
 
 static void ena_phc_unregister(struct ena_adapter *adapter)
 {
 	struct ena_phc_info *phc_info = adapter->phc_info;
 
-	if (ena_phc_enabled(adapter))
+	if (ena_phc_enabled(adapter)) {
 		ptp_clock_unregister(phc_info->clock);
+		phc_info->clock = NULL;
+	}
+}
+
+int ena_phc_alloc(struct ena_adapter *adapter)
+{
+	/* Allocate driver specific PHC info */
+	adapter->phc_info = vzalloc(sizeof(*adapter->phc_info));
+	if (unlikely(!adapter->phc_info)) {
+		netdev_err(adapter->netdev, "Failed to alloc phc_info\n");
+		return -ENOMEM;
+	}
+
+	return 0;
+}
+
+void ena_phc_free(struct ena_adapter *adapter)
+{
+	if (adapter->phc_info) {
+		vfree(adapter->phc_info);
+		adapter->phc_info = NULL;
+	}
 }
 
 int ena_phc_init(struct ena_adapter *adapter)
@@ -181,7 +201,7 @@ int ena_phc_init(struct ena_adapter *adapter)
 		goto err_ena_com_phc_init;
 	}
 
-	/* Allocate and initialize device specific PHC info */
+	/* Initialize device specific PHC info */
 	rc = ena_com_phc_init(ena_dev);
 	if (unlikely(rc)) {
 		netdev_err(netdev, "Failed to init phc, error: %d\n", rc);
@@ -195,26 +215,15 @@ int ena_phc_init(struct ena_adapter *adapter)
 		goto err_ena_com_phc_config;
 	}
 
-	/* Allocate and initialize driver specific PHC info */
-	adapter->phc_info = vzalloc(sizeof(*adapter->phc_info));
-	if (unlikely(!adapter->phc_info)) {
-		rc = -ENOMEM;
-		netdev_err(netdev, "Failed to alloc phc_info, error: %d\n", rc);
-		goto err_ena_com_phc_config;
-	}
-
 	/* Register to PTP class driver */
 	rc = ena_phc_register(adapter);
 	if (unlikely(rc)) {
 		netdev_err(netdev, "Failed to register phc, error: %d\n", rc);
-		goto err_ena_phc_register;
+		goto err_ena_com_phc_config;
 	}
 
 	return 0;
 
-err_ena_phc_register:
-	vfree(adapter->phc_info);
-	adapter->phc_info = NULL;
 err_ena_com_phc_config:
 	ena_com_phc_destroy(ena_dev);
 err_ena_com_phc_init:
@@ -224,21 +233,13 @@ err_ena_com_phc_init:
 void ena_phc_destroy(struct ena_adapter *adapter)
 {
 	ena_phc_unregister(adapter);
-
-	if (likely(adapter->phc_info)) {
-		vfree(adapter->phc_info);
-		adapter->phc_info = NULL;
-	}
-
 	ena_com_phc_destroy(adapter->ena_dev);
 }
 
 int ena_phc_get_index(struct ena_adapter *adapter)
 {
-	struct ena_phc_info *phc_info = adapter->phc_info;
-
 	if (ena_phc_enabled(adapter))
-		return ptp_clock_index(phc_info->clock);
+		return ptp_clock_index(adapter->phc_info->clock);
 
 	return -1;
 }

kernel/linux/ena/ena_phc.h
@@ -6,8 +6,6 @@
 #ifndef ENA_PHC_H
 #define ENA_PHC_H
 
-#include "ena_netdev.h"
-
 #ifdef ENA_PHC_SUPPORT
 
 #include <linux/ptp_clock_kernel.h>
@@ -30,14 +28,16 @@ bool ena_phc_enabled(struct ena_adapter *adapter);
 int ena_phc_get_index(struct ena_adapter *adapter);
 int ena_phc_init(struct ena_adapter *adapter);
 void ena_phc_destroy(struct ena_adapter *adapter);
-
+int ena_phc_alloc(struct ena_adapter *adapter);
+void ena_phc_free(struct ena_adapter *adapter);
 #else /* ENA_PHC_SUPPORT */
 
 static inline bool ena_phc_enabled(struct ena_adapter *adapter) {return false; }
 static inline int ena_phc_get_index(struct ena_adapter *adapter) { return -1; }
 static inline int ena_phc_init(struct ena_adapter *adapter) { return 0; }
 static inline void ena_phc_destroy(struct ena_adapter *adapter) { }
-
+static inline int ena_phc_alloc(struct ena_adapter *adapter) { return 0; }
+static inline void ena_phc_free(struct ena_adapter *adapter) { }
 #endif /* ENA_PHC_SUPPORT */
 
 #endif /* ENA_PHC_H */

commit f391b6c5957fec01381a8ff2ea8b4a86891d9140

linux/ena: Fix PHC info release timing
As PHC info is a part of the adapter struct and since the adapter
struct is also a part of the netdev private area [1],
PHC info must be released before releasing netdev.

[1]: https://elixir.bootlin.com/linux/v6.7/source/net/core/dev.c#L10778

Fixes: 2e9a716dbbb6 ("linux/ena: Move PHC info struct allocation")
Signed-off-by: Amit Bernstein <amitbern@amazon.com>

kernel/linux/ena/ena_netdev.c
@@ -4914,6 +4914,8 @@ static void __ena_shutoff(struct pci_dev *pdev, bool shutdown)
 	adapter->reset_reason = ENA_REGS_RESET_SHUTDOWN;
 	ena_destroy_device(adapter, true);
 
+	ena_phc_free(adapter);
+
 	if (shutdown) {
 		netif_device_detach(netdev);
 		dev_close(netdev);
@@ -4931,8 +4933,6 @@ static void __ena_shutoff(struct pci_dev *pdev, bool shutdown)
 	ena_com_delete_host_info(ena_dev);
 
 	ena_com_delete_customer_metrics_buffer(ena_dev);
-
-	ena_phc_free(adapter);
 
 	ena_release_bars(ena_dev, pdev);
 