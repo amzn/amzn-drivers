amzn/amzn-drivers

commit fe29659fb13b891e50d72831d05bea9f8d329c8a

linux/ena update ENA linux driver to version 2.2.5
**New Features**
* Enable dynamic interrupt moderation on ARM64

Signed-off-by: Shay Agroskin <shayagr@amazon.com>
Signed-off-by: Sameeh Jubran <sameehj@amazon.com>

kernel/linux/ena/ena_netdev.c
@@ -2770,6 +2770,14 @@ static int ena_up(struct ena_adapter *adapter)
 	 */
 	ena_init_napi_in_range(adapter, 0, io_queue_count);
 
+#ifdef CONFIG_ARM64
+	/* enable DIM by default on ARM machines, also needs to happen
+	 * before enabling IRQs since DIM is ran from napi routine
+	 */
+	if (ena_com_interrupt_moderation_supported(adapter->ena_dev))
+		ena_com_enable_adaptive_moderation(adapter->ena_dev);
+#endif
+
 	rc = ena_request_io_irq(adapter);
 	if (rc)
 		goto err_req_irq;

kernel/linux/ena/ena_netdev.h
@@ -53,7 +53,7 @@
 
 #define DRV_MODULE_GEN_MAJOR	2
 #define DRV_MODULE_GEN_MINOR	2
-#define DRV_MODULE_GEN_SUBMINOR 4
+#define DRV_MODULE_GEN_SUBMINOR 5
 
 #define DRV_MODULE_NAME		"ena"
 #ifndef DRV_MODULE_GENERATION

kernel/linux/rpm/Makefile
@@ -3,7 +3,7 @@
 # Author: Cristian Gafton <gafton@amazon.com>
 
 NAME	= ena
-VERSION	= 2.2.4
+VERSION	= 2.2.5
 
 TOPDIR	:= $(shell git rev-parse --show-toplevel)
 

kernel/linux/rpm/ena.spec
@@ -1,5 +1,5 @@
 %define kmod_name		ena
-%define kmod_driver_version	2.2.4
+%define kmod_driver_version	2.2.5
 %define kmod_rpm_release	1
 %define kmod_git_hash		3ac3e0bf079b2c0468f759f2213541e214a6dd77
 %define kmod_kbuild_dir		kernel/linux/ena
@@ -22,7 +22,7 @@ Source7:	preamble
 
 Name:		%{kmod_name}
 Version:	%{kmod_driver_version}
-Release:	%{kmod_rpm_release}%{?dist}.20
+Release:	%{kmod_rpm_release}%{?dist}.21
 Summary:	%{kmod_name} kernel module
 
 Group:		System/Kernel
@@ -99,6 +99,9 @@ install -m 644 -D source/%{kmod_kbuild_dir}/RELEASENOTES.md $RPM_BUILD_ROOT/usr/
 rm -rf $RPM_BUILD_ROOT
 
 %changelog
+* Thu Mar 19 2020 Sameeh Jubran sameehj@amazon.com - 2.2.5-1.21
+- Update ENA driver to version 2.2.5
+
 * Thu Mar 19 2020 Sameeh Jubran sameehj@amazon.com - 2.2.4-1.20
 - Update ENA driver to version 2.2.4
 

commit 806da4f06514b9e4d7fff394097c0de6c0ed6c90

linux/ena: Prevent adaptive moderation override
Currently, adaptive interrupt moderation is enabled in `ena_up()`
in case the device supports interrupt moderation.
`ena_up()` is called from multiple flows, including configuration
changes and resets. The current logic causes the existing
configuration to be overidden and adaptive moderation to be
enabled regardless of the previous configuration.

This patch makes two changes
1. Moves the enablement of adaptive moderation based on
   device support to `ena_probe()` and after the interrupt
   moderation logic is initialized in the com layer.
2. Keeps the check and disables adaptive moderation if the
   device stops supporting interrupt moderation in `ena_up()`.

Fixes: fe29659fb13b ("linux/ena update ENA linux driver to version 2.2.5")
Signed-off-by: David Arinzon <darinzon@amazon.com>

kernel/linux/ena/ena_netdev.c
@@ -2516,11 +2516,11 @@ int ena_up(struct ena_adapter *adapter)
 	 */
 	ena_init_napi_in_range(adapter, 0, io_queue_count);
 
-	/* Enabling DIM needs to happen before enabling IRQs since DIM
-	 * is run from napi routine
+	/* If the device stopped supporting interrupt moderation, need
+	 * to disable adaptive interrupt moderation.
 	 */
-	if (ena_com_interrupt_moderation_supported(adapter->ena_dev))
-		ena_com_enable_adaptive_moderation(adapter->ena_dev);
+	if (!ena_com_interrupt_moderation_supported(adapter->ena_dev))
+		ena_com_disable_adaptive_moderation(adapter->ena_dev);
 
 	rc = ena_request_io_irq(adapter);
 	if (rc)
@@ -4752,6 +4752,12 @@ static int ena_probe(struct pci_dev *pdev, const struct pci_device_id *ent)
 			"Failed to query interrupt moderation feature\n");
 		goto err_device_destroy;
 	}
+
+	/* Enabling DIM needs to happen before enabling IRQs since DIM
+	 * is run from napi routine.
+	 */
+	if (ena_com_interrupt_moderation_supported(adapter->ena_dev))
+		ena_com_enable_adaptive_moderation(adapter->ena_dev);
 
 	ena_init_io_rings(adapter,
 			  0,