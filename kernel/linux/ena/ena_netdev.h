amzn/amzn-drivers

commit 39c8eee94982982b2e9c805345bee6d0831169a8

linux/ena: Support polling for XSK zero-copy xmit completions
To avoid code duplication, AF-XDP zero-copy TX completions polling
function is shared with the XDP Redirect/TX one.
Most of the xmit completion handling is the same except the following
things:
- For XDP Redirect/TX, the packet is associated with an xdp_frame which
  needs to be unmapped, and freed. Since AF XDP zc receives a
  buffer from a DMA mapped user memory buffer, it isn't unmapped/freed
  and is simply acked.

- For AF-XDP zc, make the application know that it can now trigger
  another TX transmission (which would be implemented in a future
  patch) using xsk_set_tx_need_wakeup();

This patch also changes XDP napi's return code. This value is mostly
relevant for RX, unless the 'budget' value is returned in which case the
napi is called again. Given this, the completion polling function return
value is changed to boolean to indicate whether another napi cycle is
needed or not.

Signed-off-by: Shay Agroskin <shayagr@amazon.com>

kernel/linux/ena/ena_xdp.c
@@ -472,10 +472,13 @@ int ena_xdp(struct net_device *netdev, struct netdev_bpf *bpf)
 	return 0;
 }
 
-static int ena_clean_xdp_irq(struct ena_ring *tx_ring, u32 budget)
+static bool ena_clean_xdp_irq(struct ena_ring *tx_ring, u32 budget)
 {
+
+	bool is_zc_q = ENA_IS_XSK_RING(tx_ring);
 	u32 total_done = 0;
 	u16 next_to_clean;
+	bool needs_wakeup;
 	u32 tx_bytes = 0;
 	int tx_pkts = 0;
 	u16 req_id;
@@ -504,21 +507,22 @@ static int ena_clean_xdp_irq(struct ena_ring *tx_ring, u32 budget)
 			break;
 
 		tx_info = &tx_ring->tx_buffer_info[req_id];
-		xdpf = tx_info->xdpf;
 
-		tx_info->xdpf = NULL;
 		tx_info->last_jiffies = 0;
-		ena_unmap_tx_buff(tx_ring, tx_info);
+
+		if (!is_zc_q) {
+			xdpf = tx_info->xdpf;
+			tx_info->xdpf = NULL;
+			ena_unmap_tx_buff(tx_ring, tx_info);
+			xdp_return_frame(xdpf);
+		}
 
 		netif_dbg(tx_ring->adapter, tx_done, tx_ring->netdev,
-			  "tx_poll: q %d skb %p completed\n", tx_ring->qid,
-			  xdpf);
+			  "tx_poll: q %d pkt #%d\n", tx_ring->qid, tx_pkts);
 
 		tx_bytes += tx_info->total_tx_size;
 		tx_pkts++;
 		total_done += tx_info->tx_descs;
-
-		xdp_return_frame(xdpf);
 
 		tx_info->total_tx_size = 0;
 
@@ -535,7 +539,24 @@ static int ena_clean_xdp_irq(struct ena_ring *tx_ring, u32 budget)
 		  "tx_poll: q %d done. total pkts: %d\n",
 		  tx_ring->qid, tx_pkts);
 
-	return tx_pkts;
+	needs_wakeup = tx_pkts < budget;
+#ifdef ENA_AF_XDP_SUPPORT
+	if (is_zc_q) {
+		struct xsk_buff_pool *xsk_pool = tx_ring->xsk_pool;
+
+		if (tx_pkts)
+			xsk_tx_completed(xsk_pool, tx_pkts);
+
+		if (xsk_uses_need_wakeup(xsk_pool)) {
+			if (needs_wakeup)
+				xsk_set_tx_need_wakeup(xsk_pool);
+			else
+				xsk_clear_tx_need_wakeup(xsk_pool);
+		}
+	}
+#endif /* ENA_AF_XDP_SUPPORT */
+
+	return needs_wakeup;
 }
 
 /* This is the XDP napi callback. XDP queues use a separate napi callback
@@ -544,14 +565,12 @@ static int ena_clean_xdp_irq(struct ena_ring *tx_ring, u32 budget)
 int ena_xdp_io_poll(struct napi_struct *napi, int budget)
 {
 	struct ena_napi *ena_napi = container_of(napi, struct ena_napi, napi);
-	u32 xdp_work_done, xdp_budget;
 	struct ena_ring *tx_ring;
-	int napi_comp_call = 0;
+	bool needs_wakeup = true;
+	u32 rx_work_done = 0;
 	int ret;
 
 	tx_ring = ena_napi->tx_ring;
-
-	xdp_budget = budget;
 
 	if (!test_bit(ENA_FLAG_DEV_UP, &tx_ring->adapter->flags) ||
 	    test_bit(ENA_FLAG_TRIGGER_RESET, &tx_ring->adapter->flags)) {
@@ -559,7 +578,7 @@ int ena_xdp_io_poll(struct napi_struct *napi, int budget)
 		return 0;
 	}
 
-	xdp_work_done = ena_clean_xdp_irq(tx_ring, xdp_budget);
+	needs_wakeup &= ena_clean_xdp_irq(tx_ring, budget);
 
 	/* If the device is about to reset or down, avoid unmask
 	 * the interrupt and return 0 so NAPI won't reschedule
@@ -567,18 +586,19 @@ int ena_xdp_io_poll(struct napi_struct *napi, int budget)
 	if (unlikely(!test_bit(ENA_FLAG_DEV_UP, &tx_ring->adapter->flags))) {
 		napi_complete_done(napi, 0);
 		ret = 0;
-	} else if (xdp_budget > xdp_work_done) {
-		napi_comp_call = 1;
-		if (napi_complete_done(napi, xdp_work_done))
+	} else if (needs_wakeup) {
+		ena_increase_stat(&tx_ring->tx_stats.napi_comp, 1,
+				  &tx_ring->syncp);
+		if (napi_complete_done(napi, rx_work_done))
 			ena_unmask_interrupt(tx_ring, NULL);
+
 		ena_update_ring_numa_node(tx_ring, NULL);
-		ret = xdp_work_done;
+		ret = rx_work_done;
 	} else {
-		ret = xdp_budget;
+		ret = budget;
 	}
 
 	u64_stats_update_begin(&tx_ring->syncp);
-	tx_ring->tx_stats.napi_comp += napi_comp_call;
 	tx_ring->tx_stats.tx_poll++;
 	u64_stats_update_end(&tx_ring->syncp);
 	tx_ring->tx_stats.last_napi_jiffies = jiffies;

commit 4cd2e2868d3e96b185decb88fc51aaecc8e863e7

linux/ena: Force ordering on XSK TX queues
AF XDP has no mechanism to report specific TX completions to the
application, but rather only pass the total TX completions that have
been polled. This property requires the driver to ensure that the
transmitted packets are completed in order.

This patch adds logic to the TX completion polling which ensures that
only in-order TX completions are reported.

To help the ordering mechanism, this patch adds an additional flag to
ena_tx_buffer struct. The added field fits inside an existing memory
hole in the struct and doesn't change its overall size or cache
layout.

Fixes: 39c8eee94982 ("linux/ena: Support polling for XSK zero-copy xmit completions")
Signed-off-by: Shay Agroskin <shayagr@amazon.com>

kernel/linux/ena/ena_netdev.h
@@ -177,6 +177,12 @@ struct ena_tx_buffer {
 
 	/* Used for detect missing tx packets to limit the number of prints */
 	u8 print_once;
+#ifdef ENA_AF_XDP_SUPPORT
+
+	/* used for ordering TX completions when needed (e.g. AF_XDP) */
+	u8 acked;
+
+#endif
 	/* Save the last jiffies to detect missing tx packets
 	 *
 	 * sets to non zero value on ena_start_xmit and set to zero on

kernel/linux/ena/ena_xdp.c
@@ -579,18 +579,15 @@ static int ena_clean_xdp_irq(struct ena_ring *tx_ring, u32 budget)
  */
 static int ena_xdp_clean_tx_zc(struct ena_ring *tx_ring, u32 budget)
 {
-	int rc, tx_pkts, skb_pkts, zc_pkts;
-	u16 next_to_clean, req_id;
+	struct xsk_buff_pool *xsk_pool = tx_ring->xsk_pool;
+	int rc, cleaned_pkts, zc_pkts, acked_pkts;
+	struct ena_tx_buffer *tx_info;
 	u32 total_done;
+	u16 req_id;
 
-	next_to_clean = tx_ring->next_to_clean;
-	total_done = 0;
-	tx_pkts = skb_pkts = 0;
+	acked_pkts = 0;
 
-	while (tx_pkts < budget) {
-		struct ena_tx_buffer *tx_info;
-		bool is_zc_packet;
-
+	while (acked_pkts < budget) {
 		rc = ena_com_tx_comp_req_id_get(tx_ring->ena_com_io_cq,
 						&req_id);
 		if (rc) {
@@ -607,8 +604,36 @@ static int ena_xdp_clean_tx_zc(struct ena_ring *tx_ring, u32 budget)
 
 		tx_info->tx_sent_jiffies = 0;
 
-		is_zc_packet = !!tx_info->skb;
-		if (!is_zc_packet) {
+		tx_info->acked = 1;
+
+		acked_pkts++;
+	}
+
+	/* AF XDP expects the completions to be ordered but HW doesn't guarantee
+	 * this. Force ordering.
+	 */
+	total_done = 0;
+	cleaned_pkts = zc_pkts = 0;
+	req_id = tx_ring->next_to_clean;
+	while (true) {
+		bool is_zc_pkt;
+
+		tx_info = &tx_ring->tx_buffer_info[req_id];
+		if (!tx_info->acked)
+			break;
+
+		/* Used as a sanity check to signify that the packet is
+		 * in-flight.
+		 */
+		tx_info->total_tx_size = 0;
+
+		is_zc_pkt = !tx_info->skb;
+
+		cleaned_pkts++;
+		zc_pkts += is_zc_pkt;
+		total_done += tx_info->tx_descs;
+
+		if (!is_zc_pkt) {
 			struct sk_buff *skb = tx_info->skb;
 
 			ena_unmap_tx_buff(tx_ring, tx_info);
@@ -617,43 +642,34 @@ static int ena_xdp_clean_tx_zc(struct ena_ring *tx_ring, u32 budget)
 			prefetch(&skb->end);
 			dev_kfree_skb(skb);
 			tx_info->skb = NULL;
-
-			skb_pkts++;
 		}
 
-		tx_pkts++;
-		total_done += tx_info->tx_descs;
-		tx_info->total_tx_size = 0;
-
-		tx_ring->free_ids[next_to_clean] = req_id;
-		next_to_clean = ENA_TX_RING_IDX_NEXT(next_to_clean,
-						     tx_ring->ring_size);
+		/* This ensures that this loop will stop */
+		tx_info->acked = 0;
+		req_id = ENA_TX_RING_IDX_NEXT(req_id, tx_ring->ring_size);
 
 		netif_dbg(tx_ring->adapter, tx_done, tx_ring->netdev,
 			  "ZC tx_poll: q %d pkt #%d req_id %d, ZC packet: %d\n",
-			  tx_ring->qid, tx_pkts, req_id, is_zc_packet);
+			  tx_ring->qid, cleaned_pkts, req_id, is_zc_pkt);
 	}
 
-	tx_ring->next_to_clean = next_to_clean;
+	tx_ring->next_to_clean = req_id;
+
 	ena_com_comp_ack(tx_ring->ena_com_io_sq, total_done);
 
-	zc_pkts = tx_pkts - skb_pkts;
-	if (zc_pkts) {
-		struct xsk_buff_pool *xsk_pool = tx_ring->xsk_pool;
+	if (zc_pkts)
+		xsk_tx_completed(xsk_pool, zc_pkts);
 
-		if (tx_pkts)
-			xsk_tx_completed(xsk_pool, tx_pkts);
+	if (xsk_uses_need_wakeup(xsk_pool)) {
+		bool needs_wakeup = zc_pkts < budget;
 
-		if (xsk_uses_need_wakeup(xsk_pool)) {
-			bool needs_wakeup = tx_pkts < budget;
-			if (needs_wakeup)
-				xsk_set_tx_need_wakeup(xsk_pool);
-			else
-				xsk_clear_tx_need_wakeup(xsk_pool);
-		}
+		if (needs_wakeup)
+			xsk_set_tx_need_wakeup(xsk_pool);
+		else
+			xsk_clear_tx_need_wakeup(xsk_pool);
 	}
 
-	return tx_pkts;
+	return acked_pkts;
 }
 
 static bool ena_xdp_xmit_irq_zc(struct ena_ring *tx_ring,