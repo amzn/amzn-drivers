amzn/amzn-drivers

commit 99770f9af7dc2cfb841c04f42651ff57473dc421

linux/ena: Improve XDP_TX throughput by using batching
Current code issues a doorbell for every XDP_TX packet added
to the xdp(_tx) queue.
This is wasteful in case there is a burst of XDP_TX packets that
needs handling.

This commit makes sure that a doorbell is issued only at the end
of a napi call.

This commit also removes the flags parameter to ena_xdp_xmit_frame()
and the code that uses it in the function, as of the time
this commit was introduced only flags=0 are passed to the
function, making it useless.

Signed-off-by: Arthur Kiyanovski <akiyano@amazon.com>

kernel/linux/ena/ena_netdev.c
@@ -1551,6 +1551,8 @@ static int ena_clean_rx_irq(struct ena_ring *rx_ring, struct napi_struct *napi,
 #ifdef ENA_XDP_SUPPORT
 	if (xdp_flags & ENA_XDP_REDIRECT)
 		xdp_do_flush_map();
+	if (xdp_flags & ENA_XDP_TX)
+		ena_ring_tx_doorbell(rx_ring->xdp_ring);
 #endif
 
 	return work_done;

kernel/linux/ena/ena_xdp.c
@@ -75,8 +75,7 @@ error_report_dma_error:
 
 int ena_xdp_xmit_frame(struct ena_ring *tx_ring,
 		       struct ena_adapter *adapter,
-		       struct xdp_frame *xdpf,
-		       int flags)
+		       struct xdp_frame *xdpf)
 {
 	struct ena_com_tx_ctx ena_tx_ctx = {};
 	struct ena_tx_buffer *tx_info;
@@ -102,12 +101,6 @@ int ena_xdp_xmit_frame(struct ena_ring *tx_ring,
 			     xdpf->len);
 	if (rc)
 		goto error_unmap_dma;
-
-	/* trigger the dma engine. ena_ring_tx_doorbell()
-	 * calls a memory barrier inside it.
-	 */
-	if (flags & XDP_XMIT_FLUSH)
-		ena_ring_tx_doorbell(tx_ring);
 
 	return rc;
 
@@ -142,7 +135,7 @@ int ena_xdp_xmit(struct net_device *dev, int n,
 	spin_lock(&tx_ring->xdp_tx_lock);
 
 	for (i = 0; i < n; i++) {
-		if (ena_xdp_xmit_frame(tx_ring, adapter, frames[i], 0))
+		if (ena_xdp_xmit_frame(tx_ring, adapter, frames[i]))
 			break;
 		nxmit++;
 	}

kernel/linux/ena/ena_xdp.h
@@ -50,8 +50,7 @@ void ena_xdp_exchange_program_rx_in_range(struct ena_adapter *adapter,
 int ena_xdp_io_poll(struct napi_struct *napi, int budget);
 int ena_xdp_xmit_frame(struct ena_ring *tx_ring,
 		       struct ena_adapter *adapter,
-		       struct xdp_frame *xdpf,
-		       int flags);
+		       struct xdp_frame *xdpf);
 int ena_xdp_xmit(struct net_device *dev, int n,
 		 struct xdp_frame **frames, u32 flags);
 int ena_xdp(struct net_device *netdev, struct netdev_bpf *bpf);
@@ -147,8 +146,7 @@ static inline int ena_xdp_execute(struct ena_ring *rx_ring, struct xdp_buff *xdp
 		/* The XDP queues are shared between XDP_TX and XDP_REDIRECT */
 		spin_lock(&xdp_ring->xdp_tx_lock);
 
-		if (ena_xdp_xmit_frame(xdp_ring, rx_ring->adapter, xdpf,
-				       XDP_XMIT_FLUSH))
+		if (ena_xdp_xmit_frame(xdp_ring, rx_ring->adapter, xdpf))
 			xdp_return_frame(xdpf);
 
 		spin_unlock(&xdp_ring->xdp_tx_lock);

commit 201fdbba7caed8cc043ac8b34b938158846236f9

linux/ena: Add a doorbell in XDP error path for ENA_XDP_TX
In case ena_com_rx_pkt() fails while running in the context of
ena_clean_rx_irq(), and at least one packet was queued to TX ring, due
to xdp_verdict which is XDP_TX, we end up having packets in the send
queue without a doorbell being triggered.

This commit fixes this use-case by adding a doorbell if ENA_XDP_TX
verdict is set in the error path.

Fixes: 99770f9af7dc ("linux/ena: Improve XDP_TX throughput by using batching")
Signed-off-by: Evgeny Ostrovsky <evostrov@amazon.com>

kernel/linux/ena/ena_netdev.c
@@ -1581,6 +1581,8 @@ error:
 #ifdef ENA_XDP_SUPPORT
 	if (xdp_flags & ENA_XDP_REDIRECT)
 		xdp_do_flush();
+	if (xdp_flags & ENA_XDP_TX)
+		ena_ring_tx_doorbell(rx_ring->xdp_ring);
 
 #endif
 	adapter = netdev_priv(rx_ring->netdev);