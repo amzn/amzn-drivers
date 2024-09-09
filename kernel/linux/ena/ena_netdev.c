amzn/amzn-drivers

commit fd80522361cb199288ec234c676107e3ea4c739e

linux/ena: Add AF XDP zero-copy xmit function
The AF XDP ZC packets are transmitted as part of the napi routine. Each
packet is pointed by an xdp_desc from which the virtual and DMA address
can be extracted.

The transmission function is pretty much the same as for XDP TX/Redirect
(ena_xdp_xmit_frame()) and non-XDP case (ena_start_xmit()) but since it
uses its own data structures, much of the code has been re-written.

Also, since the napi routine for AF XDP zc will be shared with the napi
routine for XDP TX/Redirect queues in future patch, add a check for
which one is used for this specific queue (only AF XDP zc queues hold an
xsk_pool pointer).

Signed-off-by: Shay Agroskin <shayagr@amazon.com>

kernel/linux/ena/ena_netdev.c
@@ -798,6 +798,10 @@ static void ena_free_all_tx_bufs(struct ena_adapter *adapter)
 
 	for (i = 0; i < adapter->num_io_queues + adapter->xdp_num_queues; i++) {
 		tx_ring = &adapter->tx_ring[i];
+		if (ENA_IS_XSK_RING(tx_ring)) {
+			ena_xdp_free_tx_bufs_zc(tx_ring);
+			continue;
+		}
 		ena_free_tx_bufs(tx_ring);
 	}
 }

kernel/linux/ena/ena_xdp.c
@@ -232,6 +232,26 @@ err:
 	return rc;
 }
 
+#ifdef ENA_AF_XDP_SUPPORT
+void ena_xdp_free_tx_bufs_zc(struct ena_ring *tx_ring)
+{
+	struct xsk_buff_pool *xsk_pool = tx_ring->xsk_pool;
+	int i, xsk_frames = 0;
+
+	for (i = 0; i < tx_ring->ring_size; i++) {
+		struct ena_tx_buffer *tx_info = &tx_ring->tx_buffer_info[i];
+
+		if (tx_info->last_jiffies)
+			xsk_frames++;
+
+		tx_info->last_jiffies = 0;
+	}
+
+	if (xsk_frames)
+		xsk_tx_completed(xsk_pool, xsk_frames);
+}
+
+#endif /* ENA_AF_XDP_SUPPORT */
 static void ena_xdp_unregister_rxq_info(struct ena_ring *rx_ring)
 {
 	xdp_rxq_info_unreg_mem_model(&rx_ring->xdp_rxq);
@@ -559,6 +579,90 @@ static bool ena_clean_xdp_irq(struct ena_ring *tx_ring, u32 budget)
 	return needs_wakeup;
 }
 
+#ifdef ENA_AF_XDP_SUPPORT
+static bool ena_xdp_xmit_irq_zc(struct ena_ring *tx_ring,
+				struct napi_struct *napi,
+				int budget)
+{
+	struct xsk_buff_pool *xsk_pool = tx_ring->xsk_pool;
+	int size, rc, push_len = 0, work_done = 0;
+	struct ena_tx_buffer *tx_info;
+	struct ena_com_buf *ena_buf;
+	u16 next_to_use, req_id;
+	bool need_wakeup = true;
+	struct xdp_desc desc;
+	dma_addr_t dma;
+
+	while (likely(work_done < budget)) {
+		struct ena_com_tx_ctx ena_tx_ctx = {};
+
+		/* We assume the maximum number of descriptors, which is two
+		 * (meta data included)
+		 */
+		if (unlikely(!ena_com_sq_have_enough_space(tx_ring->ena_com_io_sq, 2)))
+			break;
+
+		if (!xsk_tx_peek_desc(xsk_pool, &desc))
+			break;
+
+		next_to_use = tx_ring->next_to_use;
+		req_id = tx_ring->free_ids[next_to_use];
+		tx_info = &tx_ring->tx_buffer_info[req_id];
+
+		size = desc.len;
+
+		if (tx_ring->tx_mem_queue_type == ENA_ADMIN_PLACEMENT_POLICY_DEV) {
+			/* Designate part of the packet for LLQ */
+			push_len = min_t(u32, size, tx_ring->tx_max_header_size);
+			ena_tx_ctx.push_header = xsk_buff_raw_get_data(xsk_pool, desc.addr);
+			ena_tx_ctx.header_len = push_len;
+
+			size -= push_len;
+			if (!size)
+				goto xmit_desc;
+		}
+
+		/* Pass the rest of the descriptor as a DMA address. Assuming
+		 * single page descriptor.
+		 */
+		dma  = xsk_buff_raw_get_dma(xsk_pool, desc.addr);
+		ena_buf = tx_info->bufs;
+		ena_buf->paddr = dma + push_len;
+		ena_buf->len = size;
+
+		ena_tx_ctx.ena_bufs = ena_buf;
+		ena_tx_ctx.num_bufs = 1;
+
+xmit_desc:
+		ena_tx_ctx.req_id = req_id;
+
+		rc = ena_xmit_common(tx_ring->adapter,
+				     tx_ring,
+				     tx_info,
+				     &ena_tx_ctx,
+				     next_to_use,
+				     desc.len);
+		if (rc)
+			break;
+
+		work_done++;
+	}
+
+	if (work_done) {
+		xsk_tx_release(xsk_pool);
+		ena_ring_tx_doorbell(tx_ring);
+	}
+
+	if (work_done == budget) {
+		need_wakeup = false;
+		if (xsk_uses_need_wakeup(xsk_pool))
+			xsk_clear_tx_need_wakeup(xsk_pool);
+	}
+
+	return need_wakeup;
+}
+
+#endif /* ENA_AF_XDP_SUPPORT */
 /* This is the XDP napi callback. XDP queues use a separate napi callback
  * than Rx/Tx queues.
  */
@@ -580,6 +684,14 @@ int ena_xdp_io_poll(struct napi_struct *napi, int budget)
 
 	needs_wakeup &= ena_clean_xdp_irq(tx_ring, budget);
 
+#ifdef ENA_AF_XDP_SUPPORT
+	if (!ENA_IS_XSK_RING(tx_ring))
+		goto polling_done;
+
+	needs_wakeup &= ena_xdp_xmit_irq_zc(tx_ring, napi, budget);
+
+polling_done:
+#endif /* ENA_AF_XDP_SUPPORT */
 	/* If the device is about to reset or down, avoid unmask
 	 * the interrupt and return 0 so NAPI won't reschedule
 	 */

kernel/linux/ena/ena_xdp.h
@@ -51,6 +51,10 @@ int ena_xdp_xmit_frame(struct ena_ring *tx_ring,
 int ena_xdp_xmit(struct net_device *dev, int n,
 		 struct xdp_frame **frames, u32 flags);
 int ena_xdp(struct net_device *netdev, struct netdev_bpf *bpf);
+#ifdef ENA_AF_XDP_SUPPORT
+void ena_xdp_free_tx_bufs_zc(struct ena_ring *tx_ring);
+#endif
+
 
 enum ena_xdp_errors_t {
 	ENA_XDP_ALLOWED = 0,
@@ -170,6 +174,8 @@ static inline bool ena_xdp_present_ring(struct ena_ring *ring)
 
 #endif /* ENA_XDP_SUPPORT */
 #ifndef ENA_AF_XDP_SUPPORT /* stabs for AF XDP code */
+static inline void ena_xdp_free_tx_bufs_zc(struct ena_ring *tx_ring) {}
+
 #define ENA_IS_XSK_RING(ring) false
 #endif /* ENA_AF_XDP_SUPPORT */
 #endif /* ENA_XDP_H */

commit 6ea8174590ab90cf341325c3f2bfbeefa9a0b80d

linux/ena: Exit start_xmit early if queue is full
The non-XDP TX flow (packets arriving from the network stack) runs
ena_start_xmit() with the assumption that there is enough room in the
TX SQ for the packet it wants to transmit.

When AF XDP isn't used this assumption is governed by the
queue-closing mechanism which blocks further transmission after the
driver advertises that it cannot process more packets.

However, when the queue is shared with AF-XDP, the mechanism is
disabled, and the kernel keeps passing packets for transmissions even
when no more entries are available in the SQ.

This commit ensures that TX packets are dropped at the earliest stage
possible if the queue doesn't have enough room. This change ensures
that ena_start_xmit() accesses the TX info data structures only when
they represent an unused packet.

Fixes: fd80522361cb ("linux/ena: Add AF XDP zero-copy xmit function")
Signed-off-by: Shay Agroskin <shayagr@amazon.com>

kernel/linux/ena/ena_netdev.c
@@ -3025,8 +3025,13 @@ static netdev_tx_t ena_start_xmit(struct sk_buff *skb, struct net_device *dev)
 #endif
 
 #ifdef ENA_AF_XDP_SUPPORT
-	if (unlikely(ENA_IS_XSK_RING(tx_ring)))
+	if (unlikely(ENA_IS_XSK_RING(tx_ring))) {
 		spin_lock(&tx_ring->xdp_tx_lock);
+
+		if (unlikely(!ena_com_sq_have_enough_space(tx_ring->ena_com_io_sq,
+							   tx_ring->sgl_size + 2)))
+			goto error_drop_packet;
+	}
 
 #endif
 	next_to_use = tx_ring->next_to_use;

kernel/linux/ena/ena_xdp.c
@@ -723,10 +723,11 @@ static bool ena_xdp_xmit_irq_zc(struct ena_ring *tx_ring,
 	while (likely(work_done < budget)) {
 		struct ena_com_tx_ctx ena_tx_ctx = {};
 
-		/* We assume the maximum number of descriptors, which is two
-		 * (meta data included)
+		/* To align with the network stack path (.ndo_start_xmit) we
+		 * leave the same amount of empty space in the SQ.
 		 */
-		if (unlikely(!ena_com_sq_have_enough_space(tx_ring->ena_com_io_sq, 2)))
+		if (unlikely(!ena_com_sq_have_enough_space(
+			    tx_ring->ena_com_io_sq, tx_ring->sgl_size + 2)))
 			break;
 
 		if (!xsk_tx_peek_desc(xsk_pool, &desc))