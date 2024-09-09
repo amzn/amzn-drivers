amzn/amzn-drivers

commit 162950f820aaf9f7eebbd69528dfebfab0ca9dfc

linux/ena: Allow setting XSK pools
The XSK pool is a contiguous memory region which a process allocates and
passes to the driver. When working with AF XDP zero-copy both TX and RX
buffers are allocated from this memory region.
Although the HW can probably deal with smaller buffers than a whole
page, for now only allow these settings.

Signed-off-by: Shay Agroskin <shayagr@amazon.com>

kernel/linux/ena/ena_xdp.c
@@ -5,6 +5,9 @@
 
 #include "ena_xdp.h"
 #ifdef ENA_XDP_SUPPORT
+#ifdef ENA_AF_XDP_SUPPORT
+#include <net/xdp_sock_drv.h>
+#endif /* ENA_AF_XDP_SUPPORT */
 
 static int validate_xdp_req_id(struct ena_ring *tx_ring, u16 req_id)
 {
@@ -351,18 +354,112 @@ static int ena_xdp_set(struct net_device *netdev, struct netdev_bpf *bpf)
 	return 0;
 }
 
+#ifdef ENA_AF_XDP_SUPPORT
+static bool ena_is_xsk_pool_params_allowed(struct xsk_buff_pool *pool)
+{
+	return xsk_pool_get_headroom(pool) == 0 &&
+	       xsk_pool_get_chunk_size(pool) == ENA_PAGE_SIZE;
+}
+
+static int ena_xsk_pool_enable(struct ena_adapter *adapter,
+			       struct xsk_buff_pool *pool,
+			       u16 qid)
+{
+	struct ena_ring *rx_ring, *tx_ring;
+	bool dev_was_up = false;
+	int err;
+
+	if (!ena_xdp_legal_queue_count(adapter, qid)) {
+		netdev_err(adapter->netdev,
+			   "Max qid for XSK pool is %d (received %d)\n",
+			   adapter->max_num_io_queues >> 1, qid);
+		return -EINVAL;
+	}
+
+	if (ena_is_xsk_pool_params_allowed(pool))
+		return -EINVAL;
+
+	rx_ring = &adapter->rx_ring[qid];
+	tx_ring = &adapter->tx_ring[qid];
+
+	err = xsk_pool_dma_map(pool, adapter->ena_dev->dmadev, 0);
+	if (err) {
+		ena_increase_stat(&rx_ring->rx_stats.dma_mapping_err, 1,
+				  &rx_ring->syncp);
+		netif_err(adapter, drv, adapter->netdev,
+			  "Failed to DMA map XSK pool for qid %d\n", qid);
+		return err;
+	}
+
+	if (test_bit(ENA_FLAG_DEV_UP, &adapter->flags)) {
+		dev_was_up = true;
+		ena_down(adapter);
+	}
+
+	rx_ring->xsk_pool = tx_ring->xsk_pool = pool;
+
+	netif_dbg(adapter, drv, adapter->netdev,
+		  "Setting XSK pool for queue %d\n", qid);
+
+	return dev_was_up ? ena_up(adapter) : 0;
+}
+
+static int ena_xsk_pool_disable(struct ena_adapter *adapter,
+				u16 qid)
+{
+	struct ena_ring *rx_ring, *tx_ring;
+	bool dev_was_up = false;
+
+	if (qid >= adapter->num_io_queues)
+		return -EINVAL;
+
+	rx_ring = &adapter->rx_ring[qid];
+	tx_ring = &adapter->tx_ring[qid];
+
+	/* XSK pool isn't attached to this ring */
+	if (!rx_ring->xsk_pool)
+		return 0;
+
+	if (test_bit(ENA_FLAG_DEV_UP, &adapter->flags)) {
+		dev_was_up = true;
+		ena_down(adapter);
+	}
+
+	xsk_pool_dma_unmap(rx_ring->xsk_pool, 0);
+
+	rx_ring->xsk_pool = tx_ring->xsk_pool = NULL;
+
+	netif_dbg(adapter, drv, adapter->netdev,
+		  "Removing XSK pool for queue %d\n", qid);
+
+	return dev_was_up ? ena_up(adapter) : 0;
+}
+
+static int ena_xsk_pool_setup(struct ena_adapter *adapter,
+			      struct xsk_buff_pool *pool,
+			      u16 qid)
+{
+	return pool ? ena_xsk_pool_enable(adapter, pool, qid) :
+		      ena_xsk_pool_disable(adapter, qid);
+}
+
+#endif /* ENA_AF_XDP_SUPPORT */
 /* This is the main xdp callback, it's used by the kernel to set/unset the xdp
  * program as well as to query the current xdp program id.
  */
 int ena_xdp(struct net_device *netdev, struct netdev_bpf *bpf)
 {
-#ifndef ENA_XDP_QUERY_IN_KERNEL
+#if !defined(ENA_XDP_QUERY_IN_KERNEL) || defined(ENA_AF_XDP_SUPPORT)
 	struct ena_adapter *adapter = netdev_priv(netdev);
 
-#endif /* ENA_XDP_QUERY_IN_KERNEL */
+#endif /* ENA_XDP_QUERY_IN_KERNEL || ENA_AF_XDP_SUPPORT */
 	switch (bpf->command) {
 	case XDP_SETUP_PROG:
 		return ena_xdp_set(netdev, bpf);
+#ifdef ENA_AF_XDP_SUPPORT
+	case XDP_SETUP_XSK_POOL:
+		return ena_xsk_pool_setup(adapter, bpf->xsk.pool, bpf->xsk.queue_id);
+#endif /* ENA_AF_XDP_SUPPORT */
 #ifndef ENA_XDP_QUERY_IN_KERNEL
 	case XDP_QUERY_PROG:
 		bpf->prog_id = adapter->xdp_bpf_prog ?

kernel/linux/ena/ena_netdev.h
@@ -269,7 +269,10 @@ struct ena_ring {
 	 * which traffic should be redirected from this rx ring.
 	 */
 	struct ena_ring *xdp_ring;
-#endif
+#ifdef ENA_AF_XDP_SUPPORT
+	struct xsk_buff_pool *xsk_pool;
+#endif /* ENA_AF_XDP_SUPPORT */
+#endif /* ENA_XDP_SUPPORT */
 
 	u16 next_to_use;
 	u16 next_to_clean;

kernel/linux/ena/kcompat.h
@@ -900,4 +900,8 @@ static inline void eth_hw_addr_set(struct net_device *dev, const u8 *addr)
 #if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 17, 0)
 #define ENA_ETHTOOL_RX_BUFF_SIZE_CHANGE
 #endif
+
+#if defined(ENA_XDP_SUPPORT) && LINUX_VERSION_CODE >= KERNEL_VERSION(5, 10, 0)
+#define ENA_AF_XDP_SUPPORT
+#endif
 #endif /* _KCOMPAT_H_ */

commit 19b911b44a5732f2a9f2cdea55d6f09e825a500f

linux/ena: Account XDP_PACKET_HEADROOM in UMEM headroom
When asking the driver to assign a UMEM to a queue the size of each
frame's headroom in the UMEM is specified as well.
The requested value is retrieved using the kernel's xsk_pool_get_headroom()
function which returns a *minimum* of XDP_PACKET_HEADROOM [1].

This patch makes the driver to only block headrooms larger than this
minimum (instead of 0).

[1]:
https://github.com/torvalds/linux/blob/e8f897f4afef/include/net/xdp_sock_drv.h#L37

Fixes: 162950f820aa ("linux/ena: Allow setting XSK pools")
Signed-off-by: Shay Agroskin <shayagr@amazon.com>

kernel/linux/ena/ena_xdp.c
@@ -401,8 +401,9 @@ static bool ena_can_queue_have_xsk_pool(struct ena_adapter *adapter, u16 qid)
 static bool ena_is_xsk_pool_params_allowed(struct ena_adapter *adapter,
 					   struct xsk_buff_pool *pool)
 {
-	if (xsk_pool_get_headroom(pool)) {
-		netdev_err(adapter->netdev, "Only a headroom of 0 is supported");
+	if (xsk_pool_get_headroom(pool) > XDP_PACKET_HEADROOM) {
+		netdev_err(adapter->netdev,
+			   "Adding additional headroom to pool is not supported");
 
 		return false;
 	}