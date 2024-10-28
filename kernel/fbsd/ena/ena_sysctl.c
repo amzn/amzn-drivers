35a4b29a4a3c044b012119e04f101f464dfa8a96
osamaabb <osamaabb@amazon.com>
2024-10-28 19:08:54 +0000

    fbsd/ena: Automated cleanup of cdefs and other formatting
    Backport upstream commit [1] into the ena driver.
    
    This backport was introduced to kernel versions 1500004 and above.
    
    [1] commit fdafd315ad0d ("sys: Automated cleanup of cdefs and other formatting")
    
    Signed-off-by: Osama Abboud <osamaabb@amazon.com>

--- kernel/fbsd/ena/ena_sysctl.c
+++ kernel/fbsd/ena/ena_sysctl.c
@@ -27,7 +27,9 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
+#if __FreeBSD_version < 1500004
 #include <sys/cdefs.h>
+#endif
 #include <sys/param.h>
 #if __FreeBSD_version <= 1302507
 __FBSDID("$FreeBSD$");
