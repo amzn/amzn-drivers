amzn/amzn-drivers

commit 3575da07847f81d761361738bc41d2faa0bbb78e

linux/ena: Introduce ZC queue TX completion polling
The TX queue is shared between traffic transmitted through the network
stack and packets sent by an AF XDP application (from an XSK socket).

Given the different nature of zero-copy queues, this patch introduces
a new TX completion function for the network-stack/AF-XDP shared queue.

Signed-off-by: Shay Agroskin <shayagr@amazon.com>

kernel/linux/ena/ena_xdp.c
@@ -521,9 +521,6 @@ int ena_xdp_xsk_wakeup(struct net_device *netdev, u32 qid, u32 flags)
 #endif /* ENA_AF_XDP_SUPPORT */
 static int ena_clean_xdp_irq(struct ena_ring *tx_ring, u32 budget)
 {
-#ifdef ENA_AF_XDP_SUPPORT
-	bool is_zc_q = ENA_IS_XSK_RING(tx_ring);
-#endif /* ENA_AF_XDP_SUPPORT */
 	u16 next_to_clean, req_id;
 	int rc, tx_pkts = 0;
 	u32 total_done = 0;
@@ -549,20 +546,12 @@ static int ena_clean_xdp_irq(struct ena_ring *tx_ring, u32 budget)
 		tx_info = &tx_ring->tx_buffer_info[req_id];
 
 		tx_info->tx_sent_jiffies = 0;
-#ifdef ENA_AF_XDP_SUPPORT
-
-		if (is_zc_q)
-			goto log_xdp_packet;
-#endif /* ENA_AF_XDP_SUPPORT */
 
 		xdpf = tx_info->xdpf;
 		tx_info->xdpf = NULL;
 		ena_unmap_tx_buff(tx_ring, tx_info);
 		xdp_return_frame(xdpf);
 
-#ifdef ENA_AF_XDP_SUPPORT
-log_xdp_packet:
-#endif /* ENA_AF_XDP_SUPPORT */
 		tx_pkts++;
 		total_done += tx_info->tx_descs;
 		tx_info->total_tx_size = 0;
@@ -581,8 +570,75 @@ log_xdp_packet:
 		  "tx_poll: q %d done. total pkts: %d\n",
 		  tx_ring->qid, tx_pkts);
 
+	return tx_pkts;
+}
+
 #ifdef ENA_AF_XDP_SUPPORT
-	if (is_zc_q) {
+/* Poll TX completions. Completions can be either for TX packets sent by
+ * an AF XDP application or the network stack (through .ndo_start_xmit)
+ */
+static int ena_xdp_clean_tx_zc(struct ena_ring *tx_ring, u32 budget)
+{
+	int rc, tx_pkts, skb_pkts, zc_pkts;
+	u16 next_to_clean, req_id;
+	u32 total_done;
+
+	next_to_clean = tx_ring->next_to_clean;
+	total_done = 0;
+	tx_pkts = skb_pkts = 0;
+
+	while (tx_pkts < budget) {
+		struct ena_tx_buffer *tx_info;
+		bool is_zc_packet;
+
+		rc = ena_com_tx_comp_req_id_get(tx_ring->ena_com_io_cq,
+						&req_id);
+		if (rc) {
+			handle_tx_comp_poll_error(tx_ring, req_id, rc);
+			break;
+		}
+
+		/* validate that the request id points to a valid packet */
+		rc = validate_tx_req_id(tx_ring, req_id);
+		if (unlikely(rc))
+			break;
+
+		tx_info = &tx_ring->tx_buffer_info[req_id];
+
+		tx_info->tx_sent_jiffies = 0;
+
+		is_zc_packet = !!tx_info->skb;
+		if (!is_zc_packet) {
+			struct sk_buff *skb = tx_info->skb;
+
+			ena_unmap_tx_buff(tx_ring, tx_info);
+
+			/* prefetch skb_end_pointer() to speedup skb_shinfo(skb) */
+			prefetch(&skb->end);
+			dev_kfree_skb(skb);
+			tx_info->skb = NULL;
+
+			skb_pkts++;
+		}
+
+		tx_pkts++;
+		total_done += tx_info->tx_descs;
+		tx_info->total_tx_size = 0;
+
+		tx_ring->free_ids[next_to_clean] = req_id;
+		next_to_clean = ENA_TX_RING_IDX_NEXT(next_to_clean,
+						     tx_ring->ring_size);
+
+		netif_dbg(tx_ring->adapter, tx_done, tx_ring->netdev,
+			  "ZC tx_poll: q %d pkt #%d req_id %d, ZC packet: %d\n",
+			  tx_ring->qid, tx_pkts, req_id, is_zc_packet);
+	}
+
+	tx_ring->next_to_clean = next_to_clean;
+	ena_com_comp_ack(tx_ring->ena_com_io_sq, total_done);
+
+	zc_pkts = tx_pkts - skb_pkts;
+	if (zc_pkts) {
 		struct xsk_buff_pool *xsk_pool = tx_ring->xsk_pool;
 
 		if (tx_pkts)
@@ -597,11 +653,9 @@ log_xdp_packet:
 		}
 	}
 
-#endif /* ENA_AF_XDP_SUPPORT */
 	return tx_pkts;
 }
 
-#ifdef ENA_AF_XDP_SUPPORT
 static bool ena_xdp_xmit_irq_zc(struct ena_ring *tx_ring,
 				struct napi_struct *napi,
 				int budget)
@@ -897,14 +951,20 @@ int ena_xdp_io_poll(struct napi_struct *napi, int budget)
 		return 0;
 	}
 
+#ifndef ENA_AF_XDP_SUPPORT
 	work_done = ena_clean_xdp_irq(tx_ring, budget);
 
-#ifdef ENA_AF_XDP_SUPPORT
-	/* Take XDP work into account */
-	needs_wakeup &= work_done < budget;
+#else
+	if (!ENA_IS_XSK_RING(tx_ring)) {
+		work_done = ena_clean_xdp_irq(tx_ring, budget);
 
-	if (!ENA_IS_XSK_RING(tx_ring))
 		goto polling_done;
+	}
+
+	work_done = ena_xdp_clean_tx_zc(tx_ring, budget);
+
+	/* Take TX completions polling into account */
+	needs_wakeup &= work_done < budget;
 
 	rx_ring = ena_napi->rx_ring;
 

commit f15f30afd383743b8efa37d168226d8084464192

linux/ena: Don't complete napi in XDP if budget is consumed
In case the budget is consumed, it means that there are
probably more packets to be processed, therefore, the
budget value is sent back to the napi poll caller,
indicating that another scheduling is required.

In the current code, if `ENA_AF_XDP_SUPPORT` is enabled,
in `ena_xdp_io_poll()`, the work done by the XDP
routine `ena_clean_xdp_irq()` is not taken into account
and `needs_wakeup` is set to a default true, causing
napi to be marked as complete and interrupts unmasked.
After all of this, the napi caller function may
receive the `budget` value as a return, indicating the
mentioned above (re-scheduling).

This scenario creates a case where napi is marked as
completed, while the `budget` return indicates that
napi schedule is required. In parallel to this,
an interrupt is triggered and schedules napi as well
with `napi_schedule_irqoff()`, causing a double add
of the same napi to the napi list maintained by the
kernel core code.

The above scenario leads to a kernel panic like the below

[ 3716.003483]  ? show_trace_log_lvl+0x1c4/0x2d2
[ 3716.003854]  ? show_trace_log_lvl+0x1c4/0x2d2
[ 3716.004219]  ? __napi_schedule_irqoff+0x58/0x80
[ 3716.004598]  ? __die_body.cold+0x8/0xd
[ 3716.004907]  ? die+0x2b/0x50
[ 3716.005160]  ? do_trap+0xce/0x120
[ 3716.005447]  ? __list_add_valid.cold+0x23/0x5b
[ 3716.005822]  ? do_error_trap+0x65/0x80
[ 3716.006137]  ? __list_add_valid.cold+0x23/0x5b
[ 3716.006507]  ? exc_invalid_op+0x4e/0x70
[ 3716.006826]  ? __list_add_valid.cold+0x23/0x5b
[ 3716.007202]  ? asm_exc_invalid_op+0x16/0x20
[ 3716.007540]  ? __list_add_valid.cold+0x23/0x5b
[ 3716.007913]  ? __list_add_valid.cold+0x23/0x5b
[ 3716.008285]  __napi_schedule_irqoff+0x58/0x80
[ 3716.008650]  ena_intr_msix_io+0x37/0x50 [ena]
[ 3716.009021]  __handle_irq_event_percpu+0x43/0x180
[ 3716.009416]  handle_irq_event+0x34/0x70
[ 3716.009740]  handle_edge_irq+0x87/0x220
[ 3716.010067]  __common_interrupt+0x3b/0xa0
[ 3716.010408]  common_interrupt+0x7b/0xa0

This patch takes the work done by the XDP function
`ena_clean_xdp_irq()` into account when checking whether
wakeup is required (which means that napi is truly done, and
the budget is not fully consumed).

Fixes: 3575da07847f ("linux/ena: Introduce ZC queue TX completion polling")

kernel/linux/ena/ena_xdp.c
@@ -1014,6 +1014,7 @@ int ena_xdp_io_poll(struct napi_struct *napi, int budget)
 #else
 	if (!ENA_IS_XSK_RING(tx_ring)) {
 		work_done = ena_clean_xdp_irq(tx_ring, budget);
+		needs_wakeup &= work_done < budget;
 	} else {
 		needs_wakeup &= ena_xdp_clean_tx_zc(tx_ring, budget);
 