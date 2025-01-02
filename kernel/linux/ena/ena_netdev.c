amzn/amzn-drivers

commit 0d5aca93bcfa4162dae4a752e8e1cfeeab354e17

linux/ena: Introduce support for flow steering
Flow steering is the ability to give the user the option to steer
RX traffic to specific queues, by noting specific fields and values
in the packets headers.

This commit contains the initial support for this feature.
This includes the resource allocation and communication with the device
to retrieve its support capabilities, and subsequently conveying this
information to the OS.

Rules are re-configured from driver memory back to the device in
case of an internal error that caused a reset.

The patch adds support to ethtool interface to add, remove, and fetch
the rules configured.

Signed-off-by: Sun Amar <sunamar@amazon.com>

kernel/linux/ena/ena_netdev.c
@@ -92,7 +92,7 @@ MODULE_DEVICE_TABLE(pci, ena_pci_tbl);
 static int ena_rss_init_default(struct ena_adapter *adapter);
 static void check_for_admin_com_state(struct ena_adapter *adapter);
 static void ena_set_dev_offloads(struct ena_com_dev_get_features_ctx *feat,
-				 struct net_device *netdev);
+				 struct ena_adapter *adapter);
 
 static void ena_tx_timeout(struct net_device *dev, unsigned int txqueue)
 {
@@ -2218,6 +2218,28 @@ static int ena_rss_configure(struct ena_adapter *adapter)
 	return 0;
 }
 
+/* Configure the Rx Flow Steering rules */
+static int ena_flow_steering_restore(struct ena_adapter *adapter, u16 flow_steering_max_entries)
+{
+	struct ena_com_dev *ena_dev = adapter->ena_dev;
+	int rc = 0;
+
+	/* In case the flow steering table wasn't initialized by probe */
+	if (!ena_dev->flow_steering.tbl_size) {
+		rc = ena_com_flow_steering_init(ena_dev, flow_steering_max_entries);
+		if (rc && (rc != -EOPNOTSUPP)) {
+			netif_err(adapter, ifup, adapter->netdev,
+				  "Failed to init Flow steering rules rc: %d\n", rc);
+			return rc;
+		}
+
+		/* No need to go further to rules restore if table just initialized */
+		return 0;
+	}
+
+	return ena_com_flow_steering_restore_device_rules(ena_dev);
+}
+
 static int ena_up_complete(struct ena_adapter *adapter)
 {
 	int rc;
@@ -3843,7 +3865,7 @@ static int ena_device_init(struct ena_adapter *adapter, struct pci_dev *pdev,
 
 	/* Turned on features shouldn't change due to reset. */
 	prev_netdev_features = adapter->netdev->features;
-	ena_set_dev_offloads(get_feat_ctx, adapter->netdev);
+	ena_set_dev_offloads(get_feat_ctx, adapter);
 	adapter->netdev->features = prev_netdev_features;
 
 	rc = ena_phc_init(adapter);
@@ -3982,6 +4004,12 @@ int ena_restore_device(struct ena_adapter *adapter)
 	if (rc) {
 		dev_err(&pdev->dev, "Enable MSI-X failed\n");
 		goto err_device_destroy;
+	}
+
+	rc = ena_flow_steering_restore(adapter, get_feat_ctx.dev_attr.flow_steering_max_entries);
+	if (rc && (rc != -EOPNOTSUPP)) {
+		dev_err(&pdev->dev, "Failed to restore flow steering rules\n");
+		goto err_disable_msix;
 	}
 
 	/* If the interface was up before the reset bring it up */
@@ -4479,8 +4507,9 @@ static u32 ena_calc_max_io_queue_num(struct pci_dev *pdev,
 }
 
 static void ena_set_dev_offloads(struct ena_com_dev_get_features_ctx *feat,
-				 struct net_device *netdev)
+				 struct ena_adapter *adapter)
 {
+	struct net_device *netdev = adapter->netdev;
 	netdev_features_t dev_features = 0;
 
 	/* Set offload features */
@@ -4517,6 +4546,9 @@ static void ena_set_dev_offloads(struct ena_com_dev_get_features_ctx *feat,
 #endif /* NETIF_F_RXHASH */
 		NETIF_F_HIGHDMA;
 
+	if (adapter->ena_dev->supported_features & BIT(ENA_ADMIN_FLOW_STEERING_CONFIG))
+		netdev->features |= NETIF_F_NTUPLE;
+
 #ifdef HAVE_RHEL6_NET_DEVICE_OPS_EXT
 	do {
 		u32 hw_features = get_netdev_hw_features(netdev);
@@ -4544,7 +4576,7 @@ static void ena_set_conf_feat_params(struct ena_adapter *adapter,
 	}
 
 	/* Set offload features */
-	ena_set_dev_offloads(feat, netdev);
+	ena_set_dev_offloads(feat, adapter);
 
 	adapter->max_mtu = feat->dev_attr.max_mtu;
 #ifdef HAVE_MTU_MIN_MAX_IN_NET_DEVICE
@@ -4835,6 +4867,12 @@ static int ena_probe(struct pci_dev *pdev, const struct pci_device_id *ent)
 
 	ena_config_debug_area(adapter);
 
+	rc = ena_com_flow_steering_init(ena_dev, get_feat_ctx.dev_attr.flow_steering_max_entries);
+	if (rc && (rc != -EOPNOTSUPP)) {
+		dev_err(&pdev->dev, "Cannot init Flow steering rules rc: %d\n", rc);
+		goto err_rss;
+	}
+
 #ifdef ENA_XDP_NETLINK_ADVERTISEMENT
 	if (ena_xdp_legal_queue_count(adapter, adapter->num_io_queues))
 		netdev->xdp_features = ENA_XDP_FEATURES;
@@ -4847,7 +4885,7 @@ static int ena_probe(struct pci_dev *pdev, const struct pci_device_id *ent)
 	rc = register_netdev(netdev);
 	if (rc) {
 		dev_err(&pdev->dev, "Cannot register net device\n");
-		goto err_rss;
+		goto err_flow_steering;
 	}
 
 	INIT_WORK(&adapter->reset_task, ena_fw_reset_device);
@@ -4878,6 +4916,8 @@ static int ena_probe(struct pci_dev *pdev, const struct pci_device_id *ent)
 
 	return 0;
 
+err_flow_steering:
+	ena_com_flow_steering_destroy(ena_dev);
 err_rss:
 	ena_com_delete_debug_area(ena_dev);
 	ena_com_rss_destroy(ena_dev);
@@ -4959,6 +4999,8 @@ static void __ena_shutoff(struct pci_dev *pdev, bool shutdown)
 	}
 
 	ena_com_rss_destroy(ena_dev);
+
+	ena_com_flow_steering_destroy(ena_dev);
 
 	ena_com_delete_debug_area(ena_dev);
 

kernel/linux/common/ena_com/ena_admin_defs.h
@@ -9,6 +9,8 @@
 #define ENA_ADMIN_EXTRA_PROPERTIES_STRING_LEN 32
 #define ENA_ADMIN_EXTRA_PROPERTIES_COUNT     32
 
+#define ENA_ADMIN_FLOW_STEERING_DEVICE_CHOOSE_LOCATION 0xFFFF
+
 #define ENA_ADMIN_RSS_KEY_PARTS              10
 
 #define ENA_ADMIN_CUSTOMER_METRICS_SUPPORT_MASK 0x3F
@@ -67,6 +69,7 @@ enum ena_admin_aq_feature_id {
 	ENA_ADMIN_LINK_CONFIG                       = 27,
 	ENA_ADMIN_HOST_ATTR_CONFIG                  = 28,
 	ENA_ADMIN_PHC_CONFIG                        = 29,
+	ENA_ADMIN_FLOW_STEERING_CONFIG              = 30,
 	ENA_ADMIN_FEATURES_OPCODE_NUM               = 32,
 };
 
@@ -577,7 +580,7 @@ struct ena_admin_device_attr_feature_desc {
 	/* unicast MAC address (in Network byte order) */
 	u8 mac_addr[6];
 
-	u8 reserved7[2];
+	u16 flow_steering_max_entries;
 
 	u32 max_mtu;
 };
@@ -703,6 +706,78 @@ struct ena_admin_feature_llq_desc {
 	 * support those requirements in order to use accelerated llq
 	 */
 	struct ena_admin_accel_mode_req accel_mode;
+};
+
+struct ena_admin_flow_steering_rule_params {
+	u8 dst_ip[16];
+
+	u8 dst_ip_mask[16];
+
+	u8 src_ip[16];
+
+	u8 src_ip_mask[16];
+
+	u16 dst_port;
+
+	u16 dst_port_mask;
+
+	u16 src_port;
+
+	u16 src_port_mask;
+
+	u8 tos;
+
+	u8 tos_mask;
+
+	/* Reserved */
+	u8 reserved1[54];
+};
+
+/* add new rule or remove existing one */
+enum ena_admin_flow_steering_action {
+	ENA_ADMIN_FLOW_STEERING_ADD_RULE            = 1,
+	ENA_ADMIN_FLOW_STEERING_REMOVE_RULE         = 2,
+	ENA_ADMIN_FLOW_STEERING_REMOVE_ALL_RULES    = 3,
+};
+
+/* type of traffic the rule applied on */
+enum ena_admin_flow_steering_type {
+	ENA_ADMIN_FLOW_INVALID                      = 0,
+	ENA_ADMIN_FLOW_IPV4                         = 1,
+	ENA_ADMIN_FLOW_IPV6                         = 2,
+	ENA_ADMIN_FLOW_IPV4_TCP                     = 3,
+	ENA_ADMIN_FLOW_IPV6_TCP                     = 4,
+	ENA_ADMIN_FLOW_IPV4_UDP                     = 5,
+	ENA_ADMIN_FLOW_IPV6_UDP                     = 6,
+	ENA_ADMIN_FLOW_LAST                         = 7,
+};
+
+struct ena_admin_set_feature_flow_steering_desc_req {
+	/* specific command action as defined in enum
+	 * ena_admin_flow_steering_action
+	 */
+	u8 action;
+
+	/* specific flow type as defined in enum ena_admin_flow_steering_type */
+	u8 flow_type;
+
+	u16 rx_q_idx;
+
+	/* if value is ENA_ADMIN_FLOW_STEERING_DEVICE_CHOOSE_LOCATION it means
+	 * the device should find unsued location for this rule
+	 */
+	u16 rule_location;
+
+	u16 reserved;
+
+	/* 31:0 : reserved */
+	u32 flags;
+};
+
+struct ena_admin_set_feature_flow_steering_desc_resp {
+	u16 rule_location;
+
+	u16 reserved;
 };
 
 struct ena_admin_queue_ext_feature_fields {
@@ -1176,6 +1251,9 @@ struct ena_admin_set_feat_cmd {
 
 		/* PHC configuration */
 		struct ena_admin_feature_phc_desc phc;
+
+		/* Flow steering configuration */
+		struct ena_admin_set_feature_flow_steering_desc_req flow_steering;
 	} u;
 };
 
@@ -1184,6 +1262,9 @@ struct ena_admin_set_feat_resp {
 
 	union {
 		u32 raw[14];
+
+		/* Flow steering configuration */
+		struct ena_admin_set_feature_flow_steering_desc_resp flow_steering;
 	} u;
 };
 

kernel/linux/common/ena_com/ena_com.c
@@ -3074,6 +3074,59 @@ void ena_com_rss_destroy(struct ena_com_dev *ena_dev)
 	memset(&ena_dev->rss, 0x0, sizeof(ena_dev->rss));
 }
 
+int ena_com_flow_steering_init(struct ena_com_dev *ena_dev, u16 flow_steering_entries)
+{
+	u32 tbl_size_in_bytes =
+		flow_steering_entries * sizeof(struct ena_com_flow_steering_table_entry);
+	struct ena_com_flow_steering *flow_steering = &ena_dev->flow_steering;
+
+	memset(flow_steering, 0x0, sizeof(*flow_steering));
+
+	if (!(ena_dev->supported_features & BIT(ENA_ADMIN_FLOW_STEERING_CONFIG)))
+		return -EOPNOTSUPP;
+
+	flow_steering->tbl_size = flow_steering_entries;
+
+	flow_steering->flow_steering_tbl =
+		devm_kzalloc(ena_dev->dmadev, tbl_size_in_bytes, GFP_KERNEL);
+	if (unlikely(!flow_steering->flow_steering_tbl)) {
+		netdev_err(ena_dev->net_device, "Flow steering table memory allocation failed\n");
+		return -ENOMEM;
+	}
+
+	flow_steering->requested_rule =
+		dma_zalloc_coherent(ena_dev->dmadev,
+				    sizeof(struct ena_admin_flow_steering_rule_params),
+				    &flow_steering->requested_rule_dma_addr, GFP_KERNEL);
+	if (unlikely(!flow_steering->requested_rule)) {
+		netdev_err(ena_dev->net_device,
+			   "Flow steering dma-able params memory allocation failed\n");
+		goto err;
+	}
+
+	return 0;
+err:
+	ena_com_flow_steering_destroy(ena_dev);
+
+	return -ENOMEM;
+}
+
+void ena_com_flow_steering_destroy(struct ena_com_dev *ena_dev)
+{
+	struct ena_com_flow_steering *flow_steering = &ena_dev->flow_steering;
+
+	if (flow_steering->requested_rule) {
+		dma_free_coherent(ena_dev->dmadev,
+				  sizeof(struct ena_admin_flow_steering_rule_params),
+				  flow_steering->requested_rule,
+				  flow_steering->requested_rule_dma_addr);
+	}
+
+	if (flow_steering->flow_steering_tbl) {
+		devm_kfree(ena_dev->dmadev, flow_steering->flow_steering_tbl);
+	}
+}
+
 int ena_com_allocate_host_info(struct ena_com_dev *ena_dev)
 {
 	struct ena_host_attribute *host_attr = &ena_dev->host_attr;
@@ -3318,6 +3371,259 @@ int ena_com_config_dev_mode(struct ena_com_dev *ena_dev,
 	}
 
 	ena_dev->tx_mem_queue_type = ENA_ADMIN_PLACEMENT_POLICY_DEV;
+
+	return 0;
+}
+
+int ena_com_flow_steering_add_rule(struct ena_com_dev *ena_dev,
+				   struct ena_com_flow_steering_rule_params *configure_params,
+				   u16 *rule_idx)
+{
+	struct ena_com_flow_steering *flow_steering = &ena_dev->flow_steering;
+	struct ena_com_admin_queue *admin_queue;
+	struct ena_admin_set_feat_resp resp;
+	struct ena_admin_set_feat_cmd cmd;
+	int ret;
+
+	if (!(ena_dev->supported_features & BIT(ENA_ADMIN_FLOW_STEERING_CONFIG))) {
+		netdev_err(ena_dev->net_device,
+			   "Flow steering rules are not supported by this device\n");
+		return -EOPNOTSUPP;
+	}
+
+	if ((*rule_idx >= flow_steering->tbl_size) &&
+	    (*rule_idx != ENA_ADMIN_FLOW_STEERING_DEVICE_CHOOSE_LOCATION)) {
+		netdev_err(ena_dev->net_device,
+			   "Failed to add a flow steering rule, index %u out of bounds\n",
+			   *rule_idx);
+		return -EINVAL;
+	}
+
+	if ((*rule_idx != ENA_ADMIN_FLOW_STEERING_DEVICE_CHOOSE_LOCATION) &&
+	    (flow_steering->flow_steering_tbl[*rule_idx].in_use)) {
+		netdev_err(ena_dev->net_device,
+			   "Failed to configure Flow steering rule to index %u with currently active rule in it\n",
+			   *rule_idx);
+		return -EINVAL;
+	}
+
+	memset(&cmd, 0x0, sizeof(cmd));
+	admin_queue = &ena_dev->admin_queue;
+
+	cmd.aq_common_descriptor.opcode = ENA_ADMIN_SET_FEATURE;
+	cmd.aq_common_descriptor.flags =
+			ENA_ADMIN_AQ_COMMON_DESC_CTRL_DATA_INDIRECT_MASK;
+	cmd.feat_common.feature_id = ENA_ADMIN_FLOW_STEERING_CONFIG;
+	cmd.u.flow_steering.action = ENA_ADMIN_FLOW_STEERING_ADD_RULE;
+	cmd.u.flow_steering.flow_type = configure_params->flow_type;
+	cmd.u.flow_steering.rx_q_idx = configure_params->qid;
+	cmd.u.flow_steering.rule_location = *rule_idx;
+	cmd.u.flow_steering.flags = 0;
+
+	memcpy(flow_steering->requested_rule,
+	       &configure_params->flow_params,
+	       sizeof(struct ena_admin_flow_steering_rule_params));
+
+	ret = ena_com_mem_addr_set(ena_dev,
+				   &cmd.control_buffer.address,
+				   flow_steering->requested_rule_dma_addr);
+	if (unlikely(ret)) {
+		netdev_err(ena_dev->net_device, "Memory address set failed\n");
+		return ret;
+	}
+
+	cmd.control_buffer.length = sizeof(struct ena_admin_flow_steering_rule_params);
+
+	ret = ena_com_execute_admin_command(admin_queue,
+					    (struct ena_admin_aq_entry *)&cmd,
+					    sizeof(cmd),
+					    (struct ena_admin_acq_entry *)&resp,
+					    sizeof(resp));
+	if (unlikely(ret)) {
+		netdev_err(ena_dev->net_device, "Failed to add a new flow steering rule: %d\n", ret);
+		return ret;
+	}
+
+	/* If the rule index needs to be chosen by the device,
+	 * set it to the rule_idx from the response
+	 */
+	if (*rule_idx == ENA_ADMIN_FLOW_STEERING_DEVICE_CHOOSE_LOCATION) {
+		*rule_idx = resp.u.flow_steering.rule_location;
+		if (*rule_idx >= flow_steering->tbl_size) {
+			netdev_err(ena_dev->net_device,
+				   "Flow steering rule configured to invalid index: %d\n",
+				   *rule_idx);
+			return -EFAULT;
+		}
+	}
+
+	flow_steering->flow_steering_tbl[*rule_idx].rule_params = *configure_params;
+	flow_steering->flow_steering_tbl[*rule_idx].in_use = true;
+
+	flow_steering->active_rules_cnt++;
+
+	return 0;
+}
+
+int ena_com_flow_steering_remove_rule(struct ena_com_dev *ena_dev, u16 rule_idx)
+{
+	struct ena_com_flow_steering *flow_steering = &ena_dev->flow_steering;
+	struct ena_admin_set_feat_resp resp;
+	struct ena_admin_set_feat_cmd cmd;
+	int ret;
+
+	if (!(ena_dev->supported_features & BIT(ENA_ADMIN_FLOW_STEERING_CONFIG))) {
+		netdev_err(ena_dev->net_device,
+			   "Flow steering rules are not supported by this device\n");
+		return -EOPNOTSUPP;
+	}
+
+	if (rule_idx >= flow_steering->tbl_size) {
+		netdev_err(ena_dev->net_device,
+			   "Failed to remove a flow steering rule, index %u out of bounds\n",
+			   rule_idx);
+		return -EINVAL;
+	}
+
+	if (!flow_steering->flow_steering_tbl[rule_idx].in_use) {
+		netdev_err(ena_dev->net_device,
+			   "Failed to remove a flow steering rule, no rule configured in index %u\n",
+			   rule_idx);
+		return -EINVAL;
+	}
+
+	memset(&cmd, 0x0, sizeof(cmd));
+
+	cmd.aq_common_descriptor.opcode = ENA_ADMIN_SET_FEATURE;
+	cmd.feat_common.feature_id = ENA_ADMIN_FLOW_STEERING_CONFIG;
+	cmd.u.flow_steering.action = ENA_ADMIN_FLOW_STEERING_REMOVE_RULE;
+	cmd.u.flow_steering.rule_location = rule_idx;
+
+	ret = ena_com_execute_admin_command(&ena_dev->admin_queue,
+					    (struct ena_admin_aq_entry *)&cmd,
+					    sizeof(cmd),
+					    (struct ena_admin_acq_entry *)&resp,
+					    sizeof(resp));
+	if (unlikely(ret)) {
+		netdev_err(ena_dev->net_device, "Failed to remove a flow steering rule: %d\n", ret);
+		return ret;
+	}
+
+	memset(&flow_steering->flow_steering_tbl[rule_idx].rule_params, 0,
+	       sizeof(struct ena_admin_flow_steering_rule_params));
+	flow_steering->flow_steering_tbl[rule_idx].in_use = false;
+
+	flow_steering->active_rules_cnt--;
+
+	return 0;
+}
+
+int ena_com_flow_steering_remove_all_rules(struct ena_com_dev *ena_dev)
+{
+	struct ena_com_flow_steering *flow_steering = &ena_dev->flow_steering;
+	struct ena_admin_set_feat_resp resp;
+	struct ena_admin_set_feat_cmd cmd;
+	int ret;
+
+	if (!(ena_dev->supported_features & BIT(ENA_ADMIN_FLOW_STEERING_CONFIG))) {
+		netdev_err(ena_dev->net_device,
+			   "Flow steering rules are not supported by this device\n");
+		return -EOPNOTSUPP;
+	}
+
+	memset(&cmd, 0x0, sizeof(cmd));
+
+	cmd.aq_common_descriptor.opcode = ENA_ADMIN_SET_FEATURE;
+	cmd.feat_common.feature_id = ENA_ADMIN_FLOW_STEERING_CONFIG;
+	cmd.u.flow_steering.action = ENA_ADMIN_FLOW_STEERING_REMOVE_ALL_RULES;
+
+	ret = ena_com_execute_admin_command(&ena_dev->admin_queue,
+					    (struct ena_admin_aq_entry *)&cmd,
+					    sizeof(cmd),
+					    (struct ena_admin_acq_entry *)&resp,
+					    sizeof(resp));
+	if (unlikely(ret)) {
+		netdev_err(ena_dev->net_device, "Failed to remove all flow steering rules: %d\n",
+			   ret);
+		return ret;
+	}
+
+	memset(flow_steering->flow_steering_tbl, 0,
+	       flow_steering->tbl_size * sizeof(struct ena_com_flow_steering_table_entry));
+	flow_steering->active_rules_cnt = 0;
+
+	return 0;
+}
+
+int ena_com_flow_steering_get_rule(struct ena_com_dev *ena_dev,
+				   struct ena_com_flow_steering_rule_params *configure_params,
+				   u16 rule_idx)
+{
+	struct ena_com_flow_steering *flow_steering = &ena_dev->flow_steering;
+	struct ena_com_flow_steering_table_entry *entry;
+
+	if (!(ena_dev->supported_features & BIT(ENA_ADMIN_FLOW_STEERING_CONFIG))) {
+		netdev_err(ena_dev->net_device,
+			   "Flow steering rules are not supported by this device\n");
+		return -EOPNOTSUPP;
+	}
+
+	if (rule_idx >= flow_steering->tbl_size) {
+		netdev_err(ena_dev->net_device,
+			   "Failed to get a flow steering rule, index %u out bounds\n", rule_idx);
+		return -EINVAL;
+	}
+
+	entry = &flow_steering->flow_steering_tbl[rule_idx];
+
+	if (!entry->in_use) {
+		netdev_err(ena_dev->net_device,
+			   "Failed to get a flow steering rule in index %u, entry not in use\n",
+			   rule_idx);
+		return -EINVAL;
+	}
+
+	*configure_params = entry->rule_params;
+
+	return 0;
+}
+
+int ena_com_flow_steering_restore_device_rules(struct ena_com_dev *ena_dev)
+{
+	struct ena_com_flow_steering *flow_steering = &ena_dev->flow_steering;
+	struct ena_com_flow_steering_table_entry *rule_entry;
+	u16 rule_idx;
+	int ret;
+
+	if (!(ena_dev->supported_features & BIT(ENA_ADMIN_FLOW_STEERING_CONFIG)))
+		return -EOPNOTSUPP;
+
+	/* no rules to restore */
+	if (flow_steering->active_rules_cnt == 0)
+		return 0;
+
+	/* set the amount of active rules to zero, will count them again while restoring */
+	flow_steering->active_rules_cnt = 0;
+
+	for (rule_idx = 0; rule_idx < flow_steering->tbl_size; rule_idx++) {
+		rule_entry = &flow_steering->flow_steering_tbl[rule_idx];
+
+		if (rule_entry->in_use) {
+			/* mark the entry as not in use before attempt to reconfigure it
+			 * so it will be counted as new rule
+			 */
+			rule_entry->in_use = false;
+
+			ret = ena_com_flow_steering_add_rule(ena_dev, &rule_entry->rule_params,
+							     &rule_idx);
+			if (unlikely(ret)) {
+				netdev_err(ena_dev->net_device,
+					   "Failed to restore flow steering rule in index %d\n",
+					   rule_idx);
+				return -EFAULT;
+			}
+		}
+	}
 
 	return 0;
 }

kernel/linux/common/ena_com/ena_com.h
@@ -366,6 +366,27 @@ struct ena_host_attribute {
 	dma_addr_t host_info_dma_addr;
 };
 
+struct ena_com_flow_steering_rule_params {
+	struct ena_admin_flow_steering_rule_params flow_params;
+	u16 qid;
+	/* Specific flow type as defined in enum ena_admin_flow_steering_type */
+	u8 flow_type;
+};
+
+struct ena_com_flow_steering_table_entry {
+	struct ena_com_flow_steering_rule_params rule_params;
+	bool in_use;
+};
+
+struct ena_com_flow_steering {
+	struct ena_com_flow_steering_table_entry *flow_steering_tbl;
+	u16 tbl_size;
+	u16 active_rules_cnt;
+
+	struct ena_admin_flow_steering_rule_params *requested_rule;
+	dma_addr_t requested_rule_dma_addr;
+};
+
 /* Each ena_dev is a PCI function. */
 struct ena_com_dev {
 	struct ena_com_admin_queue admin_queue;
@@ -408,6 +429,8 @@ struct ena_com_dev {
 	struct ena_com_llq_info llq_info;
 
 	struct ena_customer_metrics customer_metrics;
+
+	struct ena_com_flow_steering flow_steering;
 };
 
 struct ena_com_dev_get_features_ctx {
@@ -793,6 +816,23 @@ int ena_com_rss_init(struct ena_com_dev *ena_dev, u16 log_size);
  */
 void ena_com_rss_destroy(struct ena_com_dev *ena_dev);
 
+/* ena_com_flow_steering_init - Init Flow steering
+ * @ena_dev: ENA communication layer struct
+ * @flow_steering_entries: Number of Flow steering entries to use.
+ *
+ * Allocate the table to hold the steering rules context.
+ *
+ * @return: 0 on success and negative value otherwise.
+ */
+int ena_com_flow_steering_init(struct ena_com_dev *ena_dev, u16 flow_steering_entries);
+
+/* ena_com_flow_steering_destroy - Destroy flow steering
+ * @ena_dev: ENA communication layer struct
+ *
+ * Free the flow steering allocated resources.
+ */
+void ena_com_flow_steering_destroy(struct ena_com_dev *ena_dev);
+
 /* ena_com_get_current_hash_function - Get RSS hash function
  * @ena_dev: ENA communication layer struct
  *
@@ -1113,6 +1153,50 @@ static inline bool ena_com_get_missing_admin_interrupt(struct ena_com_dev *ena_d
 {
 	return ena_dev->admin_queue.is_missing_admin_interrupt;
 }
+
+/* ena_com_flow_steering_add_rule - configure new Rx flow steering rule
+ * @ena_dev: ENA communication layer struct
+ * @configure_params: Steering rule params, including queue and flow type.
+ * @rule_idx: Index in rules table to configure the rule into, on return
+ * it will hold the actual index of the configured rule.
+ *
+ * @return - 0 on success, negative value on failure.
+ */
+int ena_com_flow_steering_add_rule(struct ena_com_dev *ena_dev,
+				   struct ena_com_flow_steering_rule_params *configure_params,
+				   u16 *rule_idx);
+
+/* ena_com_flow_steering_remove_rule - Remove an existing RX flow steering rule
+ * @ena_dev: ENA communication layer struct
+ * @rule_idx: Rule table index to delete
+ *
+ * @return - 0 on success, negative value on failure.
+ */
+int ena_com_flow_steering_remove_rule(struct ena_com_dev *ena_dev, u16 rule_idx);
+
+/* ena_com_flow_steering_remove_all_rules - Remove all flow steering rules
+ * @ena_dev: ENA communication layer struct
+ *
+ * @return - 0 on success, negative value on failure.
+ */
+int ena_com_flow_steering_remove_all_rules(struct ena_com_dev *ena_dev);
+
+/* ena_com_flow_steering_get_rule - retrieve info about specific steering rule
+ * @ena_dev: ENA communication layer struct
+ * @configure_params: pointer to be filled with the steering rule parmeters.
+ * @rule_idx: Rule index to get info about.
+ *
+ * @return - 0 on success, negative value on failure.
+ */
+int ena_com_flow_steering_get_rule(struct ena_com_dev *ena_dev,
+				   struct ena_com_flow_steering_rule_params *configure_params,
+				   u16 rule_idx);
+
+/* ena_com_flow_steering_restore_device_rules - reconfigure the existing rules to the device
+ * after resets caused by errors
+ * @ena_dev: ENA communication layer struct
+ */
+int ena_com_flow_steering_restore_device_rules(struct ena_com_dev *ena_dev);
 
 /* ena_com_io_sq_to_ena_dev - Extract ena_com_dev using contained field io_sq.
  * @io_sq: IO submit queue struct

kernel/linux/ena/config/test_defs.sh
@@ -87,3 +87,9 @@ try_compile_async "#include <net/xdp.h>"                           \
                   "ENA_HAVE_NETDEV_XDP_ACT_XSK_ZEROCOPY"           \
                   ""                                               \
                   "6.3 <= LINUX_VERSION_CODE"
+
+try_compile_async "#include <linux/ethtool.h> "               \
+                  "struct ethtool_tcpip6_spec tcp_ip6;"       \
+                  "ENA_ETHTOOL_NFC_IPV6_SUPPORTED"            \
+                  ""                                          \
+                  "4.6.0 <= LINUX_VERSION_CODE"

commit b1c6fc5b13d8594a369dd5f039baa4a6655f382e

linux/ena: Add ndo_rx_flow_steer hook
With the introduction of n-tuple support in [1], `NETIF_F_NTUPLE`
is advertised on supported devices.
The kernel assumes that if `NETIF_F_NTUPLE` is set, then
`ndo_rx_flow_steer` is implemented (See the code in [2]).
As ENA doesn't support `ndo_rx_flow_steer`, add the hook
with an unsupported return value in order to avoid a kernel panic
with NULL pointer dereference. This may happen when
the user configures RPS and tries to run network traffic.

[1]: See Fixes tag
[2]: https://github.com/torvalds/linux/blob/master/net/core/dev.c#L4556

Fixes: 0d5aca93bcfa ("linux/ena: Introduce support for flow steering")
Signed-off-by: David Arinzon <darinzon@amazon.com>

kernel/linux/ena/ena_netdev.c
@@ -3510,6 +3510,16 @@ static int ena_busy_poll(struct napi_struct *napi)
 }
 #endif
 
+#ifdef CONFIG_RFS_ACCEL
+static int ena_rx_flow_steer(struct net_device *dev,
+			     const struct sk_buff *skb,
+			     u16 rxq_index,
+			     u32 flow_id)
+{
+	return -EOPNOTSUPP;
+}
+
+#endif /* CONFIG_RFS_ACCEL */
 static const struct net_device_ops ena_netdev_ops = {
 	.ndo_open		= ena_open,
 	.ndo_stop		= ena_close,
@@ -3539,6 +3549,9 @@ static const struct net_device_ops ena_netdev_ops = {
 	.ndo_xsk_wakeup         = ena_xdp_xsk_wakeup,
 #endif /* ENA_AF_XDP_SUPPORT */
 #endif /* ENA_XDP_SUPPORT */
+#ifdef CONFIG_RFS_ACCEL
+	.ndo_rx_flow_steer	= ena_rx_flow_steer,
+#endif /* CONFIG_RFS_ACCEL */
 };
 
 static int ena_calc_io_queue_size(struct ena_adapter *adapter,