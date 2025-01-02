amzn/amzn-drivers

commit 0f361baa628668c553b53d58310bd5c702baf0b9

linux/ena: Add AF XDP zero-copy RX polling function
The function processes descriptors which were written to the UMEM by the
device.

While the function could be used without an XDP program being loaded, it
makes little sense to do it and so the function drops any packets which
consist of several descriptors (which cannot be processed by an XDP
program).

If a packet is forwarded (either with XDP TX or Redirect) the kernel
takes care to free its descriptors. If the packet is passed (XDP_PASS)
or dropped (XDP_DROP) its memory is reused in the next descriptors passed
to the device (same as copybreak packets are "recycled").

Since the kernel doesn't allow to pass UMEM memory to the network stack,
if a packet is passed, its memory is copied to a newly allocated SKB. All
further processing of the packet is identical to the non XDP RX flow.

Similar for the TX side, the driver tells the application that it
doesn't need it to trigger another napi cycle if it's polled 'budget'
packets, or requests such a trigger otherwise. This is done using
xsk_set_rx_need_wakeup() and xsk_clear_rx_need_wakeup() functions.

Signed-off-by: Shay Agroskin <shayagr@amazon.com>

kernel/linux/ena/ena_netdev.c
@@ -651,7 +651,7 @@ static void ena_free_rx_page(struct ena_ring *rx_ring,
 	rx_info->page = NULL;
 }
 
-static int ena_refill_rx_bufs(struct ena_ring *rx_ring, u32 num)
+int ena_refill_rx_bufs(struct ena_ring *rx_ring, u32 num)
 {
 	u16 next_to_use, req_id;
 	u32 i;
@@ -1186,9 +1186,9 @@ static struct sk_buff *ena_rx_skb(struct ena_ring *rx_ring,
  * @ena_rx_ctx: received packet context/metadata
  * @skb: skb currently being received and modified
  */
-static void ena_rx_checksum(struct ena_ring *rx_ring,
-				   struct ena_com_rx_ctx *ena_rx_ctx,
-				   struct sk_buff *skb)
+void ena_rx_checksum(struct ena_ring *rx_ring,
+		     struct ena_com_rx_ctx *ena_rx_ctx,
+		     struct sk_buff *skb)
 {
 	/* Rx csum disabled */
 	if (unlikely(!(rx_ring->netdev->features & NETIF_F_RXCSUM))) {
@@ -1243,9 +1243,9 @@ static void ena_rx_checksum(struct ena_ring *rx_ring,
 
 }
 
-static void ena_set_rx_hash(struct ena_ring *rx_ring,
-			    struct ena_com_rx_ctx *ena_rx_ctx,
-			    struct sk_buff *skb)
+void ena_set_rx_hash(struct ena_ring *rx_ring,
+		     struct ena_com_rx_ctx *ena_rx_ctx,
+		     struct sk_buff *skb)
 {
 #ifdef NETIF_F_RXHASH
 	enum pkt_hash_types hash_type;

kernel/linux/ena/ena_xdp.c
@@ -203,7 +203,8 @@ static int ena_xdp_register_rxq_info(struct ena_ring *rx_ring)
 	int rc;
 
 #ifdef AF_XDP_BUSY_POLL_SUPPORTED
-	rc = xdp_rxq_info_reg(&rx_ring->xdp_rxq, rx_ring->netdev, rx_ring->qid, 0);
+	rc = xdp_rxq_info_reg(&rx_ring->xdp_rxq, rx_ring->netdev, rx_ring->qid,
+			      rx_ring->napi->napi_id < 0);
 #else
 	rc = xdp_rxq_info_reg(&rx_ring->xdp_rxq, rx_ring->netdev, rx_ring->qid);
 #endif
@@ -679,6 +680,188 @@ xmit_desc:
 	return need_wakeup;
 }
 
+static struct sk_buff *ena_xdp_rx_skb_zc(struct ena_ring *rx_ring, struct xdp_buff *xdp)
+{
+	u32 headroom, data_len;
+	struct sk_buff *skb;
+	void *data_addr;
+
+	/* Assuming single-page packets for XDP */
+	headroom  = xdp->data - xdp->data_hard_start;
+	data_len  = xdp->data_end - xdp->data;
+	data_addr = xdp->data;
+
+	/* allocate a skb to store the frags */
+	skb = __napi_alloc_skb(rx_ring->napi,
+			       headroom + data_len,
+			       GFP_ATOMIC | __GFP_NOWARN);
+	if (unlikely(!skb)) {
+		ena_increase_stat(&rx_ring->rx_stats.skb_alloc_fail, 1,
+				  &rx_ring->syncp);
+		netif_err(rx_ring->adapter, rx_err, rx_ring->netdev,
+			  "Failed to allocate skb in zc queue %d\n", rx_ring->qid);
+		return NULL;
+	}
+
+	skb_reserve(skb, headroom);
+	memcpy(__skb_put(skb, data_len), data_addr, data_len);
+
+	skb->protocol = eth_type_trans(skb, rx_ring->netdev);
+
+	return skb;
+}
+
+static int ena_xdp_clean_rx_irq_zc(struct ena_ring *rx_ring,
+				   struct napi_struct *napi,
+				   int budget)
+{
+	int i, refill_required, work_done, refill_threshold;
+	u16 next_to_clean = rx_ring->next_to_clean;
+	int xdp_verdict, req_id, rc, total_len;
+	struct ena_com_rx_ctx ena_rx_ctx;
+	struct ena_rx_buffer *rx_info;
+	bool xdp_prog_present;
+	struct xdp_buff *xdp;
+	struct sk_buff *skb;
+	u32 xdp_flags = 0;
+
+	netif_dbg(rx_ring->adapter, rx_status, rx_ring->netdev,
+		  "%s qid %d\n", __func__, rx_ring->qid);
+
+	ena_rx_ctx.ena_bufs = rx_ring->ena_bufs;
+	ena_rx_ctx.max_bufs = rx_ring->sgl_size;
+
+	xdp_prog_present = ena_xdp_present_ring(rx_ring);
+
+	work_done = 0;
+	total_len = 0;
+
+	do {
+		xdp_verdict = ENA_XDP_PASS;
+
+		/* Poll a packet from HW */
+		rc = ena_com_rx_pkt(rx_ring->ena_com_io_cq,
+				    rx_ring->ena_com_io_sq,
+				    &ena_rx_ctx);
+		if (unlikely(rc))
+			break;
+
+		/* Polled all RX packets */
+		if (unlikely(ena_rx_ctx.descs == 0))
+			break;
+
+		netif_dbg(rx_ring->adapter, rx_status, rx_ring->netdev,
+			  "rx_poll: q %d got packet from ena. descs #: %d l3 proto %d l4 proto %d hash: %x\n",
+			  rx_ring->qid, ena_rx_ctx.descs, ena_rx_ctx.l3_proto,
+			  ena_rx_ctx.l4_proto, ena_rx_ctx.hash);
+
+		/* First descriptor might have an offset set by the device */
+		rx_info = &rx_ring->rx_buffer_info[ena_rx_ctx.ena_bufs[0].req_id];
+		xdp = rx_info->xdp;
+		xdp->data += ena_rx_ctx.pkt_offset;
+		xdp->data_end = xdp->data + ena_rx_ctx.ena_bufs[0].len;
+		xsk_buff_dma_sync_for_cpu(xdp, rx_ring->xsk_pool);
+
+		/* Don't process several descriptors, not blocked by HW
+		 * (regardless of MTU)
+		 */
+		if (unlikely(ena_rx_ctx.descs > 1)) {
+			ena_increase_stat(&rx_ring->rx_stats.xdp_drop, 1, &rx_ring->syncp);
+			xdp_verdict = ENA_XDP_DROP;
+			goto skip_xdp_prog;
+		}
+
+		if (likely(xdp_prog_present))
+			xdp_verdict = ena_xdp_execute(rx_ring, xdp);
+
+skip_xdp_prog:
+		/* Note that there can be several descriptors, since device
+		 * might not honor MTU
+		 */
+		for (i = 0; i < ena_rx_ctx.descs; i++) {
+			req_id = rx_ring->ena_bufs[i].req_id;
+			rx_ring->free_ids[next_to_clean] = req_id;
+			next_to_clean =
+				ENA_RX_RING_IDX_NEXT(next_to_clean,
+						     rx_ring->ring_size);
+		}
+
+		if (likely(xdp_verdict)) {
+			work_done++;
+			total_len += ena_rx_ctx.ena_bufs[0].len;
+			xdp_flags |= xdp_verdict;
+
+			/* Mark buffer as consumed when it is redirected */
+			if (likely(xdp_verdict & ENA_XDP_FORWARDED))
+				rx_info->xdp = NULL;
+
+			continue;
+		}
+
+		/* XDP PASS */
+		skb = ena_xdp_rx_skb_zc(rx_ring, xdp);
+		if (unlikely(!skb)) {
+			rc = -ENOMEM;
+			break;
+		}
+
+		work_done++;
+		total_len += ena_rx_ctx.ena_bufs[0].len;
+		ena_rx_checksum(rx_ring, &ena_rx_ctx, skb);
+		ena_set_rx_hash(rx_ring, &ena_rx_ctx, skb);
+		skb_record_rx_queue(skb, rx_ring->qid);
+		napi_gro_receive(napi, skb);
+
+	} while (likely(work_done <= budget));
+
+	rx_ring->per_napi_packets += work_done;
+	u64_stats_update_begin(&rx_ring->syncp);
+	rx_ring->rx_stats.bytes += total_len;
+	rx_ring->rx_stats.cnt += work_done;
+	u64_stats_update_end(&rx_ring->syncp);
+
+	rx_ring->next_to_clean = next_to_clean;
+
+	if (xdp_flags & ENA_XDP_REDIRECT)
+		xdp_do_flush_map();
+
+	refill_required = ena_com_free_q_entries(rx_ring->ena_com_io_sq);
+	refill_threshold =
+		min_t(int, rx_ring->ring_size / ENA_RX_REFILL_THRESH_DIVIDER,
+		      ENA_RX_REFILL_THRESH_PACKET);
+	/* Optimization, try to batch new rx buffers */
+	if (refill_required > refill_threshold) {
+		ena_com_update_dev_comp_head(rx_ring->ena_com_io_cq);
+		ena_refill_rx_bufs(rx_ring, refill_required);
+	}
+
+	if (xsk_uses_need_wakeup(rx_ring->xsk_pool)) {
+		if (likely(rc || work_done < budget))
+			xsk_set_rx_need_wakeup(rx_ring->xsk_pool);
+		else
+			xsk_clear_rx_need_wakeup(rx_ring->xsk_pool);
+	}
+
+	if (unlikely(rc)) {
+		struct ena_adapter *adapter = netdev_priv(rx_ring->netdev);
+
+		if (rc == -ENOSPC) {
+			ena_increase_stat(&rx_ring->rx_stats.bad_desc_num, 1,
+					  &rx_ring->syncp);
+			ena_reset_device(adapter,
+					 ENA_REGS_RESET_TOO_MANY_RX_DESCS);
+		} else if (rc == -EIO) {
+			ena_increase_stat(&rx_ring->rx_stats.bad_req_id, 1,
+					  &rx_ring->syncp);
+			ena_reset_device(adapter, ENA_REGS_RESET_INV_RX_REQ_ID);
+		}
+
+		return 0;
+	}
+
+	return work_done;
+}
+
 #endif /* ENA_AF_XDP_SUPPORT */
 /* This is the XDP napi callback. XDP queues use a separate napi callback
  * than Rx/Tx queues.
@@ -686,11 +869,12 @@ xmit_desc:
 int ena_xdp_io_poll(struct napi_struct *napi, int budget)
 {
 	struct ena_napi *ena_napi = container_of(napi, struct ena_napi, napi);
-	struct ena_ring *tx_ring;
+	struct ena_ring *rx_ring, *tx_ring;
 	bool needs_wakeup = true;
 	u32 rx_work_done = 0;
 	int ret;
 
+	rx_ring = ena_napi->rx_ring;
 	tx_ring = ena_napi->tx_ring;
 
 	if (!test_bit(ENA_FLAG_DEV_UP, &tx_ring->adapter->flags) ||
@@ -706,6 +890,9 @@ int ena_xdp_io_poll(struct napi_struct *napi, int budget)
 		goto polling_done;
 
 	needs_wakeup &= ena_xdp_xmit_irq_zc(tx_ring, napi, budget);
+
+	rx_work_done = ena_xdp_clean_rx_irq_zc(rx_ring, napi, budget);
+	needs_wakeup &= rx_work_done < budget;
 
 polling_done:
 #endif /* ENA_AF_XDP_SUPPORT */

kernel/linux/ena/ena_netdev.h
@@ -586,4 +586,11 @@ int ena_up(struct ena_adapter *adapter);
 void ena_unmask_interrupt(struct ena_ring *tx_ring, struct ena_ring *rx_ring);
 void ena_update_ring_numa_node(struct ena_ring *tx_ring,
 			       struct ena_ring *rx_ring);
+void ena_rx_checksum(struct ena_ring *rx_ring,
+		     struct ena_com_rx_ctx *ena_rx_ctx,
+		     struct sk_buff *skb);
+void ena_set_rx_hash(struct ena_ring *rx_ring,
+		     struct ena_com_rx_ctx *ena_rx_ctx,
+		     struct sk_buff *skb);
+int ena_refill_rx_bufs(struct ena_ring *rx_ring, u32 num);
 #endif /* !(ENA_H) */

commit 3df210dc4950055f219c4e4c80140072ff61dbff

linux/ena: Don't recycle redirected buffers on failure
When using AF XDP, the eBPF program passes packets to the user-space
application through the XDP_REDIRECT return value.
The driver then calls `xdp_do_redirect` to perform the redirection. In
case the userspace application doesn't have enough room in its RX CQ,
the redirection would fail.

Starting version 6.8 (commit 269009893146c [1]), when the redirected
buffer belongs to a UMEM buffer (zero-copy mode) the kernel takes care
to "free" the buffer and re-enqueues it into the FILL ring. This
essentially breaks the ownership context since it's the driver that
allocated the memory.

As noticed by a Github customer [2], when xdp_do_redirect() fails, the
driver, that is convinced that the buffer is in its ownership, re-uses
the same buffer when submitting an RX descriptor to the device.
Since the buffer was *also* recycled by the kernel, once the driver
allocates a UMEM buffer, the kernel returns the same address that was
already recycled.
This creates a memory corruption due to simultaneous writes to the same
buffer.

To avoid guessing whether the kernel recycled the buffer on failure and
whether zero-copy is in use, this patch makes all redirection errors
remove driver ownership of the buffer and not recycle it.

*Note* that the driver calls xdp_return_buff() unconditionally:
    - For non AF XDP traffic, the kernel doesn't try to free the buffer
      as well, and so it's safe to free it in the driver
    - For AF XDP copy mode, the buffer is also not freed by the kernel
      and it's safe to free it in the driver
    - For AF XDP zero-copy mode, even if the kernel freed the buffer already,
      starting version 5.16 (commit [3]) the kernel has a protection
      against double free, and so it should be safe.
      There is a theoretical risk that commit [1] would be backported
      but [3] wouldn't but this seems unlikely.

kcompat entry:
Unfortunately the generic xdp_return_buff() which can handle the free
operations for both UMEM memory and page-pool pages has been exported
only in kernel 5.18 [4].
Since the function existed before that, but only changed its linking
status, ECC cannot be used to detect it since it doesn't do linking
phase.

There is an edge case in which the kcompat implementation doesn't
succeed: If the conversion of an xdp_buff to xdp_frame fails, the driver
has no other option but to give up and not free the buffer and recycle
it.

[1]: 269009893146 ("xsk: recycle buffer in case Rx queue was full")
https://github.com/torvalds/linux/commit/269009893146c
[2]: https://github.com/amzn/amzn-drivers/issues/330
[3]: 199d983bc015 ("xsk: Fix crash on double free in buffer pool")
https://github.com/torvalds/linux/commit/199d983bc0151
[4]: 718a18a0c8a6 ("veth: Rework veth_xdp_rcv_skb in order to accept non-linear skb")
https://github.com/torvalds/linux/commit/718a18a0c8a67

Fixes: 0f361baa6286 ("linux/ena: Add AF XDP zero-copy RX polling function")

kernel/linux/ena/ena_netdev.c
@@ -1407,7 +1407,7 @@ static int ena_xdp_handle_buff(struct ena_ring *rx_ring, struct xdp_buff *xdp, u
 		netdev_err_once(rx_ring->adapter->netdev,
 				"xdp: dropped unsupported multi-buffer packets\n");
 		ena_increase_stat(&rx_ring->rx_stats.xdp_drop, 1, &rx_ring->syncp);
-		return ENA_XDP_DROP;
+		return ENA_XDP_RECYCLE;
 	}
 
 	rx_info = &rx_ring->rx_buffer_info[rx_ring->ena_bufs[0].req_id];
@@ -1520,7 +1520,8 @@ static int ena_clean_rx_irq(struct ena_ring *rx_ring, struct napi_struct *napi,
 				/* Packets was passed for transmission, unmap it
 				 * from RX side.
 				 */
-				if (xdp_verdict & ENA_XDP_FORWARDED) {
+				if (xdp_verdict &
+				    (ENA_XDP_FORWARDED | ENA_XDP_DROP)) {
 					ena_unmap_rx_buff_attrs(rx_ring,
 								&rx_ring->rx_buffer_info[req_id],
 								ENA_DMA_ATTR_SKIP_CPU_SYNC);

kernel/linux/ena/ena_xdp.c
@@ -890,7 +890,7 @@ static bool ena_xdp_clean_rx_irq_zc(struct ena_ring *rx_ring,
 			netdev_err_once(rx_ring->adapter->netdev,
 					"xdp: dropped unsupported multi-buffer packets\n");
 			ena_increase_stat(&rx_ring->rx_stats.xdp_drop, 1, &rx_ring->syncp);
-			xdp_verdict = ENA_XDP_DROP;
+			xdp_verdict = ENA_XDP_RECYCLE;
 		} else if (likely(xdp_prog_present)) {
 			xdp_verdict = ena_xdp_execute(rx_ring, xdp);
 		}
@@ -911,8 +911,8 @@ static bool ena_xdp_clean_rx_irq_zc(struct ena_ring *rx_ring,
 			total_len += ena_rx_ctx.ena_bufs[0].len;
 			xdp_flags |= xdp_verdict;
 
-			/* Mark buffer as consumed when it is redirected */
-			if (likely(xdp_verdict & ENA_XDP_FORWARDED))
+			/* Mark buffer as consumed when it is redirected or freed */
+			if (likely(xdp_verdict & (ENA_XDP_FORWARDED | ENA_XDP_DROP)))
 				rx_info->xdp = NULL;
 
 			continue;

kernel/linux/ena/ena_xdp.h
@@ -37,7 +37,8 @@ enum ENA_XDP_ACTIONS {
 	ENA_XDP_PASS		= 0,
 	ENA_XDP_TX		= BIT(0),
 	ENA_XDP_REDIRECT	= BIT(1),
-	ENA_XDP_DROP		= BIT(2)
+	ENA_XDP_RECYCLE		= BIT(2),
+	ENA_XDP_DROP		= BIT(3)
 };
 
 #ifdef ENA_HAVE_NETDEV_XDP_ACT_XSK_ZEROCOPY
@@ -138,7 +139,7 @@ static inline int ena_xdp_execute(struct ena_ring *rx_ring, struct xdp_buff *xdp
 		if (unlikely(!xdpf)) {
 			trace_xdp_exception(rx_ring->netdev, xdp_prog, verdict);
 			xdp_stat = &rx_ring->rx_stats.xdp_aborted;
-			verdict = ENA_XDP_DROP;
+			verdict = ENA_XDP_RECYCLE;
 			break;
 		}
 
@@ -163,16 +164,20 @@ static inline int ena_xdp_execute(struct ena_ring *rx_ring, struct xdp_buff *xdp
 		}
 		trace_xdp_exception(rx_ring->netdev, xdp_prog, verdict);
 		xdp_stat = &rx_ring->rx_stats.xdp_aborted;
-		verdict = ENA_XDP_DROP;
+		if (likely(!ena_xdp_return_buff(xdp)))
+			verdict = ENA_XDP_DROP;
+		else
+			verdict = ENA_XDP_RECYCLE;
+
 		break;
 	case XDP_ABORTED:
 		trace_xdp_exception(rx_ring->netdev, xdp_prog, verdict);
 		xdp_stat = &rx_ring->rx_stats.xdp_aborted;
-		verdict = ENA_XDP_DROP;
+		verdict = ENA_XDP_RECYCLE;
 		break;
 	case XDP_DROP:
 		xdp_stat = &rx_ring->rx_stats.xdp_drop;
-		verdict = ENA_XDP_DROP;
+		verdict = ENA_XDP_RECYCLE;
 		break;
 	case XDP_PASS:
 		xdp_stat = &rx_ring->rx_stats.xdp_pass;
@@ -181,7 +186,7 @@ static inline int ena_xdp_execute(struct ena_ring *rx_ring, struct xdp_buff *xdp
 	default:
 		bpf_warn_invalid_xdp_action(rx_ring->netdev, xdp_prog, verdict);
 		xdp_stat = &rx_ring->rx_stats.xdp_invalid;
-		verdict = ENA_XDP_DROP;
+		verdict = ENA_XDP_RECYCLE;
 	}
 
 	ena_increase_stat(xdp_stat, 1, &rx_ring->syncp);

kernel/linux/ena/kcompat.h
@@ -1154,4 +1154,44 @@ static inline int irq_update_affinity_hint(unsigned int irq, const struct cpumas
 #define xdp_set_features_flag(netdev, features)
 #endif /* ENA_HAVE_XDP_SET_FEATURES_FLAG */
 
+#if defined(ENA_XDP_SUPPORT) && LINUX_VERSION_CODE < KERNEL_VERSION(5, 18, 0)
+#ifdef ENA_AF_XDP_SUPPORT
+#include <net/xdp_sock_drv.h>
+#endif /* ENA_AF_XDP_SUPPORT */
+static inline int ena_xdp_return_buff(struct xdp_buff *xdp)
+{
+	struct xdp_frame *xdpf;
+
+#ifdef ENA_AF_XDP_SUPPORT
+	if (xdp->rxq->mem.type == MEM_TYPE_XSK_BUFF_POOL) {
+		xsk_buff_free(xdp);
+		return 0;
+	}
+#endif
+	/* Non zero-copy pages should be safe to convert to
+	 * an xdp_frame, which has xdp_return_frame() function exported
+	 * to all kernels that support XDP
+	 */
+#ifdef XDP_CONVERT_TO_FRAME_NAME_CHANGED
+	xdpf = xdp_convert_buff_to_frame(xdp);
+#else
+	xdpf = convert_to_xdp_frame(xdp);
+#endif /* XDP_CONVERT_TO_FRAME_NAME_CHANGED */
+	if (!xdpf)
+		return -EOVERFLOW;
+
+	xdp_return_frame(xdpf);
+
+	return 0;
+}
+#elif defined(ENA_XDP_SUPPORT)
+static inline int ena_xdp_return_buff(struct xdp_buff *xdp)
+{
+	/* If xdp_return_buff() is defined it cannot fail */
+	xdp_return_buff(xdp);
+
+	return 0;
+}
+#endif /* defined(ENA_XDP_SUPPORT) && LINUX_VERSION_CODE < KERNEL_VERSION(5, 17, 0) */
+
 #endif /* _KCOMPAT_H_ */