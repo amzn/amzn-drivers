amzn/amzn-drivers

commit 8770d557df1681e5a7923ba052ab246d65acca88

linux/ena: update ENA linux driver to version 1.5.0
**New Features:**
* improve driver robustness - add mechanism for detection and recovery
       from lost/misrouted interrupt.

**Bug Fixes:**
* don't enable interrupts until ENA_FLAG_DEV_UP flag is set - this
       might potentially cause a race resulting in ignored interrupts.
* add error handling to ena_down() sequence - errors, if not handled
       correctly, might affect subsequent ena_open() procedure.

kernel/linux/ena/ena_netdev.c
@@ -79,6 +79,9 @@ static struct workqueue_struct *ena_wq;
 MODULE_DEVICE_TABLE(pci, ena_pci_tbl);
 
 static int ena_rss_init_default(struct ena_adapter *adapter);
+static void check_for_admin_com_state(struct ena_adapter *adapter);
+static void ena_destroy_device(struct ena_adapter *adapter);
+static int ena_restore_device(struct ena_adapter *adapter);
 
 static void ena_tx_timeout(struct net_device *dev)
 {
@@ -174,6 +177,8 @@ static void ena_init_io_rings_common(struct ena_adapter *adapter,
 	ring->per_napi_packets = 0;
 	ring->per_napi_bytes = 0;
 	ring->cpu = 0;
+	ring->first_interrupt = false;
+	ring->no_interrupt_event_cnt = 0;
 	u64_stats_init(&ring->syncp);
 }
 
@@ -1355,6 +1360,9 @@ static irqreturn_t ena_intr_msix_io(int irq, void *data)
 	napi_schedule_irqoff(&ena_napi->napi);
 #endif
 
+	ena_napi->tx_ring->first_interrupt = true;
+	ena_napi->rx_ring->first_interrupt = true;
+
 	return IRQ_HANDLED;
 }
 
@@ -1708,7 +1716,7 @@ static int ena_rss_configure(struct ena_adapter *adapter)
 
 static int ena_up_complete(struct ena_adapter *adapter)
 {
-	int rc, i;
+	int rc;
 
 	rc = ena_rss_configure(adapter);
 	if (rc)
@@ -1726,17 +1734,6 @@ static int ena_up_complete(struct ena_adapter *adapter)
 	ena_restore_ethtool_params(adapter);
 
 	ena_napi_enable_all(adapter);
-
-	/* Enable completion queues interrupt */
-	for (i = 0; i < adapter->num_queues; i++)
-		ena_unmask_interrupt(&adapter->tx_ring[i],
-				     &adapter->rx_ring[i]);
-
-	/* schedule napi in case we had pending packets
-	 * from the last time we disable napi
-	 */
-	for (i = 0; i < adapter->num_queues; i++)
-		napi_schedule(&adapter->ena_napi[i].napi);
 
 	return 0;
 }
@@ -1874,7 +1871,7 @@ create_err:
 
 static int ena_up(struct ena_adapter *adapter)
 {
-	int rc;
+	int rc, i;
 
 	netdev_dbg(adapter->netdev, "%s\n", __func__);
 
@@ -1916,6 +1913,17 @@ static int ena_up(struct ena_adapter *adapter)
 	u64_stats_update_end(&adapter->syncp);
 
 	set_bit(ENA_FLAG_DEV_UP, &adapter->flags);
+
+	/* Enable completion queues interrupt */
+	for (i = 0; i < adapter->num_queues; i++)
+		ena_unmask_interrupt(&adapter->tx_ring[i],
+				     &adapter->rx_ring[i]);
+
+	/* schedule napi in case we had pending packets
+	 * from the last time we disable napi
+	 */
+	for (i = 0; i < adapter->num_queues; i++)
+		napi_schedule(&adapter->ena_napi[i].napi);
 
 	return rc;
 
@@ -2026,6 +2034,17 @@ static int ena_close(struct net_device *netdev)
 
 	if (test_bit(ENA_FLAG_DEV_UP, &adapter->flags))
 		ena_down(adapter);
+
+	/* Check for device status and issue reset if needed*/
+	check_for_admin_com_state(adapter);
+	if (unlikely(test_bit(ENA_FLAG_TRIGGER_RESET, &adapter->flags))) {
+		netif_err(adapter, ifdown, adapter->netdev,
+			  "Destroy failure, restarting device\n");
+		ena_dump_stats_to_dmesg(adapter);
+		/* rtnl lock already obtained in dev_ioctl() layer */
+		ena_destroy_device(adapter);
+		ena_restore_device(adapter);
+	}
 
 	return 0;
 }
@@ -2836,11 +2855,12 @@ static void ena_destroy_device(struct ena_adapter *adapter)
 	ena_sysfs_terminate(&adapter->pdev->dev);
 	ena_com_set_admin_running_state(ena_dev, false);
 
-	ena_close(netdev);
+	if (test_bit(ENA_FLAG_DEV_UP, &adapter->flags))
+		ena_down(adapter);
 
 	/* Before releasing the ENA resources, a device reset is required.
 	 * (to prevent the device from accessing them).
-	 * In case the reset flag is set and the device is up, ena_close
+	 * In case the reset flag is set and the device is up, ena_down()
 	 * already perform the reset, so it can be skipped.
 	 */
 	if (!(test_bit(ENA_FLAG_TRIGGER_RESET, &adapter->flags) && dev_up))
@@ -2947,7 +2967,30 @@ static void ena_fw_reset_device(struct work_struct *work)
 	rtnl_unlock();
 }
 
-static int check_missing_comp_in_queue(struct ena_adapter *adapter,
+static int check_for_rx_interrupt_queue(struct ena_adapter *adapter,
+					struct ena_ring *rx_ring)
+{
+	if (likely(rx_ring->first_interrupt))
+		return 0;
+
+	if (ena_com_cq_empty(rx_ring->ena_com_io_cq))
+		return 0;
+
+	rx_ring->no_interrupt_event_cnt++;
+
+	if (rx_ring->no_interrupt_event_cnt == ENA_MAX_NO_INTERRUPT_ITERATIONS) {
+	       netif_err(adapter, rx_err, adapter->netdev,
+			 "Potential MSIX issue on Rx side Queue = %d. Reset the device\n",
+			 rx_ring->qid);
+	       adapter->reset_reason = ENA_REGS_RESET_MISS_INTERRUPT;
+	       set_bit(ENA_FLAG_TRIGGER_RESET, &adapter->flags);
+	       return -EIO;
+	}
+
+	return 0;
+}
+
+static int check_missing_comp_in_tx_queue(struct ena_adapter *adapter,
 				       struct ena_ring *tx_ring)
 {
 	struct ena_tx_buffer *tx_buf;
@@ -2958,8 +3001,24 @@ static int check_missing_comp_in_queue(struct ena_adapter *adapter,
 	for (i = 0; i < tx_ring->ring_size; i++) {
 		tx_buf = &tx_ring->tx_buffer_info[i];
 		last_jiffies = tx_buf->last_jiffies;
-		if (unlikely(last_jiffies &&
-			     time_is_before_jiffies(last_jiffies + adapter->missing_tx_completion_to))) {
+
+		if (last_jiffies == 0)
+			/* no pending Tx at this location */
+			continue;
+
+		if (unlikely(!tx_ring->first_interrupt && time_is_before_jiffies(last_jiffies +
+					 2 * adapter->missing_tx_completion_to))) {
+			/* If after graceful period interrupt is still not received, we schedule a reset*/
+			       netif_err(adapter, tx_err, adapter->netdev,
+					 "Potential MSIX issue on Tx side Queue = %d. Reset the device\n",
+					 tx_ring->qid);
+			       adapter->reset_reason = ENA_REGS_RESET_MISS_INTERRUPT;
+			       set_bit(ENA_FLAG_TRIGGER_RESET, &adapter->flags);
+			       return -EIO;
+		}
+
+		if (unlikely(time_is_before_jiffies(last_jiffies +
+				adapter->missing_tx_completion_to))) {
 			if (!tx_buf->print_once)
 				netif_notice(adapter, tx_err, adapter->netdev,
 					     "Found a Tx that wasn't completed on time, qid %d, index %d.\n",
@@ -2988,9 +3047,10 @@ static int check_missing_comp_in_queue(struct ena_adapter *adapter,
 	return rc;
 }
 
-static void check_for_missing_tx_completions(struct ena_adapter *adapter)
+static void check_for_missing_completions(struct ena_adapter *adapter)
 {
 	struct ena_ring *tx_ring;
+	struct ena_ring *rx_ring;
 	int i, budget, rc;
 
 	/* Make sure the driver doesn't turn the device in other process */
@@ -3009,8 +3069,13 @@ static void check_for_missing_tx_completions(struct ena_adapter *adapter)
 
 	for (i = adapter->last_monitored_tx_qid; i < adapter->num_queues; i++) {
 		tx_ring = &adapter->tx_ring[i];
+		rx_ring = &adapter->rx_ring[i];
 
-		rc = check_missing_comp_in_queue(adapter, tx_ring);
+		rc = check_missing_comp_in_tx_queue(adapter, tx_ring);
+		if (unlikely(rc))
+			return;
+
+		rc = check_for_rx_interrupt_queue(adapter, rx_ring);
 		if (unlikely(rc))
 			return;
 
@@ -3169,7 +3234,7 @@ static void ena_timer_service(unsigned long data)
 
 	check_for_admin_com_state(adapter);
 
-	check_for_missing_tx_completions(adapter);
+	check_for_missing_completions(adapter);
 
 	check_for_empty_rx_ring(adapter);
 

kernel/linux/common/ena_com/ena_eth_com.c
@@ -504,3 +504,15 @@ int ena_com_tx_comp_req_id_get(struct ena_com_io_cq *io_cq, u16 *req_id)
 
 	return 0;
 }
+
+bool ena_com_cq_empty(struct ena_com_io_cq *io_cq)
+{
+	struct ena_eth_io_rx_cdesc_base *cdesc;
+
+	cdesc = ena_com_get_next_rx_cdesc(io_cq);
+	if(cdesc)
+		return false;
+	else
+		return true;
+}
+

kernel/linux/common/ena_com/ena_eth_com.h
@@ -88,6 +88,8 @@ int ena_com_add_single_rx_desc(struct ena_com_io_sq *io_sq,
 
 int ena_com_tx_comp_req_id_get(struct ena_com_io_cq *io_cq, u16 *req_id);
 
+bool ena_com_cq_empty(struct ena_com_io_cq *io_cq);
+
 static inline void ena_com_unmask_intr(struct ena_com_io_cq *io_cq,
 				       struct ena_eth_io_intr_reg *intr_reg)
 {

kernel/linux/common/ena_com/ena_regs_defs.h
@@ -60,6 +60,8 @@ enum ena_regs_reset_reason_types {
 	ENA_REGS_RESET_USER_TRIGGER		= 12,
 
 	ENA_REGS_RESET_GENERIC			= 13,
+
+	ENA_REGS_RESET_MISS_INTERRUPT		= 14,
 };
 
 /* ena_registers offsets */

kernel/linux/ena/README
@@ -72,8 +72,11 @@ RHEL / Open Suse / Ubuntu:
 --------------------------
 sudo vi /etc/modules-load.d/ena.conf
 insert "ena" to the file
-copy the ena.ko to /lib/modules/(uname -r)/
+copy the ena.ko to /lib/modules/$(uname -r)/
 sudo depmod
+if previous driver was loaded from initramfs - it will have to be
+updated as well (i.e. dracut)
+
 restart the OS (sudo reboot and reconnect)
 #endif
 

kernel/linux/ena/ena_netdev.h
@@ -46,7 +46,7 @@
 #include "ena_eth_com.h"
 
 #define DRV_MODULE_VER_MAJOR	1
-#define DRV_MODULE_VER_MINOR	4
+#define DRV_MODULE_VER_MINOR	5
 #define DRV_MODULE_VER_SUBMINOR 0
 
 #define DRV_MODULE_NAME		"ena"
@@ -124,6 +124,7 @@
  * We wait for 6 sec just to be on the safe side.
  */
 #define ENA_DEVICE_KALIVE_TIMEOUT	(6 * HZ)
+#define ENA_MAX_NO_INTERRUPT_ITERATIONS 3
 
 #define ENA_MMIO_DISABLE_REG_READ	BIT(0)
 
@@ -245,6 +246,9 @@ struct ena_ring {
 
 	/* The maximum header length the device can handle */
 	u8 tx_max_header_size;
+
+	bool first_interrupt;
+	u16 no_interrupt_event_cnt;
 
 	/* cpu for TPH */
 	int cpu;

kernel/linux/rpm/Makefile
@@ -3,7 +3,7 @@
 # Author: Cristian Gafton <gafton@amazon.com>
 
 NAME	= ena
-VERSION	= 1.4.0
+VERSION	= 1.5.0
 
 TOPDIR	:= $(shell git rev-parse --show-toplevel)
 

kernel/linux/rpm/ena.spec
@@ -1,5 +1,5 @@
 %define kmod_name		ena
-%define kmod_driver_version	1.4.0
+%define kmod_driver_version	1.5.0
 %define kmod_rpm_release	1
 %define kmod_git_hash		3ac3e0bf079b2c0468f759f2213541e214a6dd77
 %define kmod_kbuild_dir		kernel/linux/ena
@@ -22,7 +22,7 @@ Source7:	preamble
 
 Name:		%{kmod_name}
 Version:	%{kmod_driver_version}
-Release:	%{kmod_rpm_release}%{?dist}.3
+Release:	%{kmod_rpm_release}%{?dist}.4
 Summary:	%{kmod_name} kernel module
 
 Group:		System/Kernel
@@ -99,6 +99,9 @@ install -m 644 -D source/%{kmod_kbuild_dir}/RELEASENOTES.md $RPM_BUILD_ROOT/usr/
 rm -rf $RPM_BUILD_ROOT
 
 %changelog
+* Wed Nov 29 2017 Netanel Belgazal netanel@amazon.com - 1.4.0-1.4
+- Update ENA driver to version 1.5.0
+
 * Mon Nov 13 2017 Netanel Belgazal netanel@amazon.com - 1.4.0-1.3
 - Update ENA driver to version 1.4.0
 

commit 3efeaf1e38bb914c208ec11c93dd77ed42071154

linux/ena: Update queue stats on error paths
This commit fixes the following error paths where currently the queue
stats are not properly updated.

In ena_clean_rx_irq() if we receive an error from ena_com_rx_pkt() we
goto error and don't add the processed packets stats.

In check_missing_comp_in_tx_queue(), if we didn't receive first
interrupt we don't count all packets that didn't get a completion when
we hit graceful timeout.

Fixes: 8770d557df16 ("linux/ena: update ENA linux driver to version
1.5.0")
Fixes: 798070ab13c6 ("First commit of Amazon Elastic Network Adapter (ENA) Linux driver")
Signed-off-by: Ofir Tabachnik <ofirt@amazon.com>

kernel/linux/ena/ena_netdev.c
@@ -1658,6 +1658,12 @@ static int ena_clean_rx_irq(struct ena_ring *rx_ring, struct napi_struct *napi,
 	return work_done;
 
 error:
+	work_done = budget - res_budget;
+	u64_stats_update_begin(&rx_ring->syncp);
+	rx_ring->rx_stats.bytes += total_len;
+	rx_ring->rx_stats.cnt += work_done;
+	rx_ring->rx_stats.rx_copybreak_pkt += rx_copybreak_pkt;
+	u64_stats_update_end(&rx_ring->syncp);
 #ifdef ENA_XDP_SUPPORT
 	if (xdp_flags & ENA_XDP_REDIRECT)
 		xdp_do_flush();
@@ -4520,6 +4526,25 @@ static enum ena_regs_reset_reason_types check_cdesc_in_tx_cq(struct ena_adapter 
 	return ENA_REGS_RESET_MISS_INTERRUPT;
 }
 
+static void check_pending_pkts_in_txsq(struct ena_ring *tx_ring,
+				       int first_index, u32 *new_missed_tx,
+				       unsigned long miss_tx_comp_to_jiffies)
+{
+	struct ena_tx_buffer *tx_buf;
+	unsigned long timeout;
+	int i;
+
+	for (i = first_index; i < tx_ring->ring_size; i++) {
+		tx_buf = &tx_ring->tx_buffer_info[i];
+		if (tx_buf->tx_sent_jiffies == 0 || tx_buf->print_once)
+			continue;
+
+		timeout = tx_buf->tx_sent_jiffies + miss_tx_comp_to_jiffies;
+		if (time_is_before_jiffies(timeout))
+			(*new_missed_tx)++;
+	}
+}
+
 static int check_missing_comp_in_tx_queue(struct ena_adapter *adapter, struct ena_ring *tx_ring)
 {
 	unsigned long miss_tx_comp_to_jiffies = adapter->missing_tx_completion_to_jiffies;
@@ -4550,8 +4575,11 @@ static int check_missing_comp_in_tx_queue(struct ena_adapter *adapter, struct en
 			netif_err(adapter, tx_err, netdev,
 				  "Potential MSIX issue on Tx side Queue = %d. Reset the device\n",
 				  tx_ring->qid);
-			ena_reset_device(adapter, ENA_REGS_RESET_MISS_FIRST_INTERRUPT);
-			return -EIO;
+			check_pending_pkts_in_txsq(tx_ring, i, &new_missed_tx,
+						   miss_tx_comp_to_jiffies);
+			reset_reason = ENA_REGS_RESET_MISS_FIRST_INTERRUPT;
+			rc = -EIO;
+			goto out;
 		}
 
 		/* Checking if current TX buffer got timeout */
@@ -4611,10 +4639,14 @@ static int check_missing_comp_in_tx_queue(struct ena_adapter *adapter, struct en
 		napi_scheduled = !!(READ_ONCE(ena_napi->napi.state) & NAPIF_STATE_SCHED);
 		if (!napi_scheduled)
 			reset_reason = check_cdesc_in_tx_cq(adapter, tx_ring);
-		/* Update reset reason */
-		ena_reset_device(adapter, reset_reason);
+
 		rc = -EIO;
 	}
+
+out:
+	if (rc)
+		/* Update reset reason */
+		ena_reset_device(adapter, reset_reason);
 
 	/* Add the newly discovered missing TX completions */
 	ena_increase_stat(&tx_ring->tx_stats.missed_tx, new_missed_tx, &tx_ring->syncp);