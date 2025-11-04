amzn/amzn-drivers

commit 6cdfb8fac5c43f9ec41413c25a49caad9534a3ef

linux/ena: Use local vars instead of struct fields
Stats ena_srd_info and eni_stats are filled and used only in
ena_metrics_stats() when querying stats via ethtool, therefore there is
no need store them in struct ena_adapter.

Signed-off-by: Ofir Tabachnik <ofirt@amazon.com>

kernel/linux/ena/ena_ethtool.c
@@ -239,34 +239,37 @@ static void ena_metrics_stats(struct ena_adapter *adapter, u64 **data)
 		(*data) += supported_metrics_count;
 
 	} else if (ena_com_get_cap(dev, ENA_ADMIN_ENI_STATS)) {
-		ena_com_get_eni_stats(dev, &adapter->eni_stats);
+		struct ena_admin_eni_stats eni_stats;
+
+		ena_com_get_eni_stats(dev, &eni_stats);
 		/* Updating regardless of rc - once we told ethtool how many stats we have
 		 * it will print that much stats. We can't leave holes in the stats
 		 */
 		for (i = 0; i < ENA_STATS_ARRAY_ENI; i++) {
 			ena_stats = &ena_stats_eni_strings[i];
-
-			ptr = (u64 *)&adapter->eni_stats +
-				ena_stats->stat_offset;
-
-			ena_safe_update_stat(ptr, (*data)++, &adapter->syncp);
+			ptr = (u64 *)&eni_stats + ena_stats->stat_offset;
+			**data = *ptr;
+			(*data)++;
 		}
 	}
 
 	if (ena_com_get_cap(dev, ENA_ADMIN_ENA_SRD_INFO)) {
-		ena_com_get_ena_srd_info(dev, &adapter->ena_srd_info);
+		struct ena_admin_ena_srd_info ena_srd_info;
+
+		ena_com_get_ena_srd_info(dev, &ena_srd_info);
 		/* Get ENA SRD mode */
-		ptr = (u64 *)&adapter->ena_srd_info;
-		ena_safe_update_stat(ptr, (*data)++, &adapter->syncp);
+		ena_stats = &ena_srd_info_strings[0];
+		ptr = (u64 *)&ena_srd_info + ena_stats->stat_offset;
+		**data = *ptr;
+		(*data)++;
 		for (i = 1; i < ENA_STATS_ARRAY_ENA_SRD; i++) {
 			ena_stats = &ena_srd_info_strings[i];
 			/* Wrapped within an outer struct - need to accommodate an
 			 * additional offset of the ENA SRD mode that was already processed
 			 */
-			ptr = (u64 *)&adapter->ena_srd_info +
-				ena_stats->stat_offset + 1;
-
-			ena_safe_update_stat(ptr, (*data)++, &adapter->syncp);
+			ptr = (u64 *)&ena_srd_info + ena_stats->stat_offset + 1;
+			**data = *ptr;
+			(*data)++;
 		}
 	}
 }

kernel/linux/ena/ena_netdev.h
@@ -494,8 +494,6 @@ struct ena_adapter {
 
 	struct u64_stats_sync syncp;
 	struct ena_stats_dev dev_stats;
-	struct ena_admin_eni_stats eni_stats;
-	struct ena_admin_ena_srd_info ena_srd_info;
 
 	/* last queue index that was checked for missing completions / interrupts */
 	u32 last_monitored_qid;

commit 06d1b96e2037f23cf97d9bb52c5bb3101fb851c4

linux/ena: Zero-initialize structs ena_srd_info and eni_stats
Currently, if ena_com_get_eni_stats() or ena_com_get_ena_srd_info()
fails, the driver may expose uninitialized stack memory as garbage values.

This commit fixes the issue by zero-initializing the relevant structs.
This ensures that even in failure scenarios, the reported values would
be zeros and not garbage values.

Fixes: 6cdfb8fac5c4 ("linux/ena: Use local vars instead of struct fields")
Signed-off-by: Gilad Ben Yakov <giladben@amazon.com>

kernel/linux/ena/ena_ethtool.c
@@ -285,7 +285,7 @@ static void ena_metrics_stats(struct ena_adapter *adapter, u64 **data)
 		(*data) += supported_metrics_count;
 
 	} else if (ena_com_get_cap(dev, ENA_ADMIN_ENI_STATS)) {
-		struct ena_admin_eni_stats eni_stats;
+		struct ena_admin_eni_stats eni_stats = {};
 
 		ena_com_get_eni_stats(dev, &eni_stats);
 		/* Updating regardless of rc - once we told ethtool how many stats we have
@@ -300,7 +300,7 @@ static void ena_metrics_stats(struct ena_adapter *adapter, u64 **data)
 	}
 
 	if (ena_com_get_cap(dev, ENA_ADMIN_ENA_SRD_INFO)) {
-		struct ena_admin_ena_srd_info ena_srd_info;
+		struct ena_admin_ena_srd_info ena_srd_info = {};
 
 		ena_com_get_ena_srd_info(dev, &ena_srd_info);
 		/* Get ENA SRD mode */