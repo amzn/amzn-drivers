8edb6c0dd657409ed677590ef6040a080a818598
Michael Margolin <mrgolin@amazon.com>
2026-03-19 14:53:45 +0000

    linux/efa: Protect concurrent config.h appends with flock
    Multiple compile_conftest.sh processes run in parallel and append
    to shared config.h and output.log files. While small writes are
    unlikely to interleave on local filesystems, POSIX does not
    guarantee write atomicity for regular files.
    
    Use flock on the output file descriptor to serialize appends,
    eliminating any possibility of interleaved or lost writes.
    
    Signed-off-by: Michael Margolin <mrgolin@amazon.com>

--- kernel/linux/efa/config/compile_conftest.sh
+++ kernel/linux/efa/config/compile_conftest.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # SPDX-License-Identifier: GPL-2.0 OR BSD-2-Clause
-# Copyright 2021 Amazon.com, Inc. or its affiliates. All rights reserved.
+# Copyright 2021-2026 Amazon.com, Inc. or its affiliates. All rights reserved.
 
 tmpdir=$1
 kerneldir=$2
@@ -11,12 +11,12 @@ make -C $tmpdir KERNEL_DIR=$kerneldir >/dev/null 2>$tmpdir/output.log
 err=$?
 
 if [[ "$err" -eq 0 ]] && [[ -n "$success" ]]; then
-	echo "#define $success 1" >> config.h
+	{ flock 1; echo "#define $success 1"; } >> config.h
 fi
 
 if [[ "$err" -ne 0 ]] && [[ -n "$fail" ]]; then
-	echo "#define $fail 1" >> config.h
+	{ flock 1; echo "#define $fail 1"; } >> config.h
 fi
 
-cat $tmpdir/output.log >> ../output.log
+{ flock 1; cat $tmpdir/output.log; } >> ../output.log
 rm -fr $tmpdir
