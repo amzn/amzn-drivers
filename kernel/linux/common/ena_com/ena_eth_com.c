amzn/amzn-drivers

commit 2befbb717839f1025dc77666e10ba35039b7d9d6

linux/ena: Dump invalid rx/tx descriptors
This patch adds descriptors dump in case a descriptor is faulty.
The added information can help detect and debug HW faults.

Signed-off-by: Osama Abboud <osamaabb@amazon.com>

kernel/linux/common/ena_com/ena_eth_com.c
@@ -5,7 +5,7 @@
 
 #include "ena_eth_com.h"
 
-static struct ena_eth_io_rx_cdesc_base *ena_com_get_next_rx_cdesc(
+struct ena_eth_io_rx_cdesc_base *ena_com_get_next_rx_cdesc(
 	struct ena_com_io_cq *io_cq)
 {
 	struct ena_eth_io_rx_cdesc_base *cdesc;
@@ -30,6 +30,52 @@ static struct ena_eth_io_rx_cdesc_base *ena_com_get_next_rx_cdesc(
 	dma_rmb();
 
 	return cdesc;
+}
+
+void ena_com_dump_single_rx_cdesc(struct ena_com_io_cq *io_cq,
+				  struct ena_eth_io_rx_cdesc_base *desc)
+{
+	if (desc) {
+		u32 *desc_arr = (u32 *)desc;
+
+		netdev_err(ena_com_io_cq_to_ena_dev(io_cq)->net_device,
+			   "RX descriptor value[0x%08x 0x%08x 0x%08x 0x%08x] phase[%u] first[%u] last[%u] MBZ7[%u] MZB17[%u]\n",
+			   desc_arr[0], desc_arr[1], desc_arr[2], desc_arr[3],
+			   ENA_FIELD_GET(desc->status, (u32)ENA_ETH_IO_RX_DESC_PHASE_MASK, 0),
+			   ENA_FIELD_GET(desc->status, (u32)ENA_ETH_IO_RX_DESC_FIRST_MASK,
+					 ENA_ETH_IO_RX_DESC_FIRST_SHIFT),
+			   ENA_FIELD_GET(desc->status, (u32)ENA_ETH_IO_RX_DESC_LAST_MASK,
+					 ENA_ETH_IO_RX_DESC_LAST_SHIFT),
+			   ENA_FIELD_GET(desc->status, (u32)ENA_ETH_IO_RX_CDESC_BASE_MBZ7_MASK,
+					 ENA_ETH_IO_RX_CDESC_BASE_MBZ7_SHIFT),
+			   ENA_FIELD_GET(desc->status, (u32)ENA_ETH_IO_RX_CDESC_BASE_MBZ17_MASK,
+					 ENA_ETH_IO_RX_CDESC_BASE_MBZ17_SHIFT));
+	}
+}
+
+void ena_com_dump_single_tx_cdesc(struct ena_com_io_cq *io_cq,
+				  struct ena_eth_io_tx_cdesc *desc)
+{
+	if (desc) {
+		u32 *desc_arr = (u32 *)desc;
+
+		netdev_err(ena_com_io_cq_to_ena_dev(io_cq)->net_device,
+			   "TX descriptor value[0x%08x 0x%08x] phase[%u] MBZ6[%u]\n", desc_arr[0],
+			   desc_arr[1],
+			   ENA_FIELD_GET(desc->flags, (u32)ENA_ETH_IO_TX_CDESC_PHASE_MASK, 0),
+			   ENA_FIELD_GET(desc->flags, (u32)ENA_ETH_IO_TX_CDESC_MBZ6_MASK,
+					 ENA_ETH_IO_TX_CDESC_MBZ6_SHIFT));
+	}
+}
+
+struct ena_eth_io_tx_cdesc *ena_com_tx_cdesc_idx_to_ptr(
+	struct ena_com_io_cq *io_cq, u16 idx)
+{
+	idx &= (io_cq->q_depth - 1);
+
+	return (struct ena_eth_io_tx_cdesc *)
+		((uintptr_t)io_cq->cdesc_addr.virt_addr +
+		idx * io_cq->cdesc_entry_size_in_bytes);
 }
 
 static void *get_sq_desc_regular_queue(struct ena_com_io_sq *io_sq)
@@ -225,7 +271,7 @@ static int ena_com_sq_update_tail(struct ena_com_io_sq *io_sq)
 	return ena_com_sq_update_reqular_queue_tail(io_sq);
 }
 
-static struct ena_eth_io_rx_cdesc_base *
+struct ena_eth_io_rx_cdesc_base *
 	ena_com_rx_cdesc_idx_to_ptr(struct ena_com_io_cq *io_cq, u16 idx)
 {
 	idx &= (io_cq->q_depth - 1);
@@ -251,7 +297,6 @@ static int ena_com_cdesc_rx_pkt_get(struct ena_com_io_cq *io_cq,
 			break;
 		status = READ_ONCE(cdesc->status);
 
-		ena_com_cq_inc_head(io_cq);
 		if (unlikely((status & ENA_ETH_IO_RX_CDESC_BASE_FIRST_MASK) >>
 				     ENA_ETH_IO_RX_CDESC_BASE_FIRST_SHIFT &&
 			     count != 0)) {
@@ -270,6 +315,7 @@ static int ena_com_cdesc_rx_pkt_get(struct ena_com_io_cq *io_cq,
 			return -EFAULT;
 		}
 
+		ena_com_cq_inc_head(io_cq);
 		count++;
 		last = (status & ENA_ETH_IO_RX_CDESC_BASE_LAST_MASK) >>
 			ENA_ETH_IO_RX_CDESC_BASE_LAST_SHIFT;

kernel/linux/common/ena_com/ena_eth_com.h
@@ -13,6 +13,17 @@
 #define ENA_LLQ_HEADER		(128UL - ENA_LLQ_ENTRY_DESC_CHUNK_SIZE)
 #define ENA_LLQ_LARGE_HEADER	(256UL - ENA_LLQ_ENTRY_DESC_CHUNK_SIZE)
 
+void ena_com_dump_single_rx_cdesc(struct ena_com_io_cq *io_cq,
+				  struct ena_eth_io_rx_cdesc_base *desc);
+void ena_com_dump_single_tx_cdesc(struct ena_com_io_cq *io_cq,
+				  struct ena_eth_io_tx_cdesc *desc);
+struct ena_eth_io_rx_cdesc_base *ena_com_get_next_rx_cdesc(
+	struct ena_com_io_cq *io_cq);
+struct ena_eth_io_rx_cdesc_base *ena_com_rx_cdesc_idx_to_ptr(
+	struct ena_com_io_cq *io_cq, u16 idx);
+struct ena_eth_io_tx_cdesc *ena_com_tx_cdesc_idx_to_ptr(
+	struct ena_com_io_cq *io_cq, u16 idx);
+
 struct ena_com_tx_ctx {
 	struct ena_com_tx_meta ena_meta;
 	struct ena_com_buf *ena_bufs;

kernel/linux/ena/ena_netdev.c
@@ -957,6 +957,24 @@ static void ena_destroy_all_io_queues(struct ena_adapter *adapter)
 	ena_destroy_all_rx_queues(adapter);
 }
 
+void ena_get_and_dump_head_rx_cdesc(struct ena_com_io_cq *io_cq)
+{
+	struct ena_eth_io_rx_cdesc_base *cdesc;
+
+	cdesc = ena_com_get_next_rx_cdesc(io_cq);
+	ena_com_dump_single_rx_cdesc(io_cq, cdesc);
+}
+
+void ena_get_and_dump_head_tx_cdesc(struct ena_com_io_cq *io_cq)
+{
+	struct ena_eth_io_tx_cdesc *cdesc;
+	u16 target_cdesc_idx;
+
+	target_cdesc_idx = io_cq->head & (io_cq->q_depth - 1);
+	cdesc = ena_com_tx_cdesc_idx_to_ptr(io_cq, target_cdesc_idx);
+	ena_com_dump_single_tx_cdesc(io_cq, cdesc);
+}
+
 int handle_invalid_req_id(struct ena_ring *ring, u16 req_id,
 			  struct ena_tx_buffer *tx_info, bool is_xdp)
 {
@@ -973,6 +991,7 @@ int handle_invalid_req_id(struct ena_ring *ring, u16 req_id,
 			  "Invalid req_id %u in qid %u\n",
 			  req_id, ring->qid);
 
+	ena_get_and_dump_head_tx_cdesc(ring->ena_com_io_cq);
 	ena_increase_stat(&ring->tx_stats.bad_req_id, 1, &ring->syncp);
 	ena_reset_device(ring->adapter, ENA_REGS_RESET_INV_TX_REQ_ID);
 
@@ -1011,6 +1030,7 @@ static int ena_clean_tx_irq(struct ena_ring *tx_ring, u32 budget)
 			if (unlikely(rc == -EINVAL))
 				handle_invalid_req_id(tx_ring, req_id, NULL, false);
 			else if (unlikely(rc == -EFAULT)) {
+				ena_get_and_dump_head_tx_cdesc(tx_ring->ena_com_io_cq);
 				ena_reset_device(tx_ring->adapter,
 						 ENA_REGS_RESET_TX_DESCRIPTOR_MALFORMED);
 			}
@@ -1591,10 +1611,12 @@ error:
 		ena_increase_stat(&rx_ring->rx_stats.bad_desc_num, 1, &rx_ring->syncp);
 		ena_reset_device(adapter, ENA_REGS_RESET_TOO_MANY_RX_DESCS);
 	} else if (rc == -EFAULT) {
+		ena_get_and_dump_head_rx_cdesc(rx_ring->ena_com_io_cq);
 		ena_reset_device(adapter, ENA_REGS_RESET_RX_DESCRIPTOR_MALFORMED);
 	} else {
 		ena_increase_stat(&rx_ring->rx_stats.bad_req_id, 1,
 				  &rx_ring->syncp);
+		ena_get_and_dump_head_rx_cdesc(rx_ring->ena_com_io_cq);
 		ena_reset_device(adapter, ENA_REGS_RESET_INV_RX_REQ_ID);
 	}
 	return 0;
@@ -4037,6 +4059,9 @@ static enum ena_regs_reset_reason_types check_cdesc_in_tx_cq(struct ena_adapter 
 			  tx_ring->qid);
 
 		return ENA_REGS_RESET_MISS_TX_CMPL;
+	} else if (rc == -EFAULT) {
+		netif_err(adapter, tx_err, netdev, "Faulty descriptor found in CQ %d", tx_ring->qid);
+		ena_get_and_dump_head_tx_cdesc(tx_ring->ena_com_io_cq);
 	}
 
 	/* TX CQ has cdescs */

kernel/linux/ena/ena_netdev.h
@@ -659,6 +659,8 @@ struct page *ena_alloc_map_page(struct ena_ring *rx_ring, dma_addr_t *dma);
 
 int ena_destroy_device(struct ena_adapter *adapter, bool graceful);
 int ena_restore_device(struct ena_adapter *adapter);
+void ena_get_and_dump_head_tx_cdesc(struct ena_com_io_cq *io_cq);
+void ena_get_and_dump_head_rx_cdesc(struct ena_com_io_cq *io_cq);
 int handle_invalid_req_id(struct ena_ring *ring, u16 req_id,
 			  struct ena_tx_buffer *tx_info, bool is_xdp);
 

kernel/linux/ena/ena_xdp.c
@@ -577,6 +577,7 @@ static int ena_clean_xdp_irq(struct ena_ring *tx_ring, u32 budget)
 			if (unlikely(rc == -EINVAL))
 				handle_invalid_req_id(tx_ring, req_id, NULL, true);
 			else if (unlikely(rc == -EFAULT)) {
+				ena_get_and_dump_head_tx_cdesc(tx_ring->ena_com_io_cq);
 				ena_reset_device(tx_ring->adapter,
 						 ENA_REGS_RESET_TX_DESCRIPTOR_MALFORMED);
 			}
@@ -905,8 +906,10 @@ skip_xdp_prog:
 			ena_reset_device(adapter, ENA_REGS_RESET_TOO_MANY_RX_DESCS);
 		} else if (rc == -EIO) {
 			ena_increase_stat(&rx_ring->rx_stats.bad_req_id, 1, &rx_ring->syncp);
+			ena_get_and_dump_head_rx_cdesc(rx_ring->ena_com_io_cq);
 			ena_reset_device(adapter, ENA_REGS_RESET_INV_RX_REQ_ID);
 		} else if (rc == -EFAULT) {
+			ena_get_and_dump_head_rx_cdesc(rx_ring->ena_com_io_cq);
 			ena_reset_device(adapter, ENA_REGS_RESET_RX_DESCRIPTOR_MALFORMED);
 		}
 

commit 63d76c87c9106d79eb5b6e9c91348ebcb0b30dd5

linux/ena: Fix bits shift of rx_cdesc on dump
Commit [1] added a handler for dumping rx cdesc, but defines used
to extract first/last/phase bits are of the send descriptor instead
of completion descriptor.
Fix by replacing with defines related to completion descriptor.

[1]: See Fixes

Fixes: 2befbb717839 ("linux/ena: Dump invalid rx/tx descriptors")
Signed-off-by: Evgeny Ostrovsky <evostrov@amazon.com>

kernel/linux/common/ena_com/ena_eth_com.c
@@ -40,9 +40,9 @@ void ena_com_dump_single_rx_cdesc(struct ena_com_io_cq *io_cq,
 		netdev_err(ena_com_io_cq_to_ena_dev(io_cq)->net_device,
 			   "RX descriptor value[0x%08x 0x%08x 0x%08x 0x%08x] phase[%u] first[%u] last[%u] MBZ7[%u] MBZ17[%u]\n",
 			   desc_arr[0], desc_arr[1], desc_arr[2], desc_arr[3],
-			   FIELD_GET((u32)ENA_ETH_IO_RX_DESC_PHASE_MASK, desc->base.status),
-			   FIELD_GET((u32)ENA_ETH_IO_RX_DESC_FIRST_MASK, desc->base.status),
-			   FIELD_GET((u32)ENA_ETH_IO_RX_DESC_LAST_MASK, desc->base.status),
+			   FIELD_GET((u32)ENA_ETH_IO_RX_CDESC_BASE_PHASE_MASK, desc->base.status),
+			   FIELD_GET((u32)ENA_ETH_IO_RX_CDESC_BASE_FIRST_MASK, desc->base.status),
+			   FIELD_GET((u32)ENA_ETH_IO_RX_CDESC_BASE_LAST_MASK, desc->base.status),
 			   FIELD_GET((u32)ENA_ETH_IO_RX_CDESC_BASE_MBZ7_MASK, desc->base.status),
 			   FIELD_GET((u32)ENA_ETH_IO_RX_CDESC_BASE_MBZ17_MASK, desc->base.status));
 		if (unlikely(ena_com_is_extended_rx_cdesc(io_cq)))