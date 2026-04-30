amzn/amzn-drivers

commit e75691c4fa87664e4b38fe9c0b1c0d8cd01e1d41

linux/ena: Unify skb allocation path and use build_skb()
Convert the RX path to use build_skb() for packets larger than
copybreak (set to 256 by default). build_skb() makes the first
descriptor's page to be the linear part of the sk_buff struct buffer.

Signed-off-by: Shay Agroskin <shayagr@amazon.com>
Signed-off-by: Arthur Kiyanovski <akiyano@amazon.com>

kernel/linux/ena/ena_netdev.c
@@ -45,6 +45,10 @@ MODULE_VERSION(DRV_MODULE_GENERATION);
 
 #define DEFAULT_MSG_ENABLE (NETIF_MSG_DRV | NETIF_MSG_PROBE | NETIF_MSG_IFUP | \
 		NETIF_MSG_TX_DONE | NETIF_MSG_TX_ERR | NETIF_MSG_RX_ERR)
+#ifndef ENA_LINEAR_FRAG_SUPPORTED
+
+#define ENA_SKB_PULL_MIN_LEN 64
+#endif
 static int debug = -1;
 module_param(debug, int, 0444);
 MODULE_PARM_DESC(debug, "Debug level (-1=default,0=none,...,16=all)");
@@ -582,7 +586,7 @@ static void ena_xdp_exchange_program_rx_in_range(struct ena_adapter *adapter,
 			rx_ring->rx_headroom = XDP_PACKET_HEADROOM;
 		} else {
 			ena_xdp_unregister_rxq_info(rx_ring);
-			rx_ring->rx_headroom = 0;
+			rx_ring->rx_headroom = NET_SKB_PAD;
 		}
 	}
 }
@@ -790,6 +794,7 @@ static void ena_init_io_rings(struct ena_adapter *adapter,
 			rxr->smoothed_interval =
 				ena_com_get_nonadaptive_moderation_interval_rx(ena_dev);
 			rxr->empty_rx_queue = 0;
+			rxr->rx_headroom = NET_SKB_PAD;
 			adapter->ena_napi[i].dim.mode = DIM_CQ_PERIOD_MODE_START_FROM_EQE;
 #ifdef ENA_XDP_SUPPORT
 			rxr->xdp_ring = &adapter->tx_ring[i + adapter->num_io_queues];
@@ -1195,6 +1200,7 @@ static int ena_alloc_rx_page(struct ena_ring *rx_ring,
 	struct ena_com_buf *ena_buf;
 	struct page *page;
 	dma_addr_t dma;
+	int tailroom;
 
 	/* restore page offset value in case it has been changed by device */
 	rx_info->page_offset = headroom;
@@ -1214,10 +1220,12 @@ static int ena_alloc_rx_page(struct ena_ring *rx_ring,
 	netif_dbg(rx_ring->adapter, rx_status, rx_ring->netdev,
 		  "Allocate page %p, rx_info %p\n", page, rx_info);
 
+	tailroom = SKB_DATA_ALIGN(sizeof(struct skb_shared_info));
+
 	rx_info->page = page;
 	ena_buf = &rx_info->ena_buf;
 	ena_buf->paddr = dma + headroom;
-	ena_buf->len = ENA_PAGE_SIZE - headroom;
+	ena_buf->len = ENA_PAGE_SIZE - headroom - tailroom;
 
 	return 0;
 }
@@ -1630,21 +1638,30 @@ static int ena_clean_tx_irq(struct ena_ring *tx_ring, u32 budget)
 	return tx_pkts;
 }
 
-static struct sk_buff *ena_alloc_skb(struct ena_ring *rx_ring, bool frags)
+static struct sk_buff *ena_alloc_skb(struct ena_ring *rx_ring, void *first_frag)
 {
 	struct sk_buff *skb;
+#ifdef ENA_LINEAR_FRAG_SUPPORTED
 
-	if (frags)
-		skb = napi_get_frags(rx_ring->napi);
-	else
+	if (!first_frag)
 		skb = netdev_alloc_skb_ip_align(rx_ring->netdev,
 						rx_ring->rx_copybreak);
+	else
+		skb = build_skb(first_frag, ENA_PAGE_SIZE);
+#else
+	u32 linear_size = max_t(u32, ENA_SKB_PULL_MIN_LEN, rx_ring->rx_copybreak);
+
+	skb = netdev_alloc_skb_ip_align(rx_ring->netdev,
+					linear_size);
+#endif
 
 	if (unlikely(!skb)) {
 		ena_increase_stat(&rx_ring->rx_stats.skb_alloc_fail, 1,
 				  &rx_ring->syncp);
+
 		netif_dbg(rx_ring->adapter, rx_err, rx_ring->netdev,
-			  "Failed to allocate skb. frags: %d\n", frags);
+			  "Failed to allocate skb. first_frag %s\n",
+			  first_frag ? "provided" : "not provided");
 		return NULL;
 	}
 
@@ -1659,10 +1676,12 @@ static struct sk_buff *ena_rx_skb(struct ena_ring *rx_ring,
 	struct ena_rx_buffer *rx_info;
 	u16 len, req_id, buf = 0;
 	struct sk_buff *skb;
-#ifdef ENA_BUSY_POLL_SUPPORT
-	bool polling;
+	void *page_addr;
+	u32 page_offset;
+	void *data_addr;
+#ifndef ENA_LINEAR_FRAG_SUPPORTED
+	u16 hlen;
 #endif
-	void *va;
 
 	len = ena_bufs[buf].len;
 	req_id = ena_bufs[buf].req_id;
@@ -1680,12 +1699,14 @@ static struct sk_buff *ena_rx_skb(struct ena_ring *rx_ring,
 		  rx_info, rx_info->page);
 
 	/* save virt address of first buffer */
-	va = page_address(rx_info->page) + rx_info->page_offset;
+	page_addr = page_address(rx_info->page);
+	page_offset = rx_info->page_offset;
+	data_addr = page_addr + page_offset;
 
-	prefetch(va);
+	prefetch(data_addr);
 
 	if (len <= rx_ring->rx_copybreak) {
-		skb = ena_alloc_skb(rx_ring, false);
+		skb = ena_alloc_skb(rx_ring, NULL);
 		if (unlikely(!skb))
 			return NULL;
 
@@ -1698,7 +1719,7 @@ static struct sk_buff *ena_rx_skb(struct ena_ring *rx_ring,
 					dma_unmap_addr(&rx_info->ena_buf, paddr),
 					len,
 					DMA_FROM_DEVICE);
-		skb_copy_to_linear_data(skb, va, len);
+		skb_copy_to_linear_data(skb, data_addr, len);
 		dma_sync_single_for_device(rx_ring->dev,
 					   dma_unmap_addr(&rx_info->ena_buf, paddr),
 					   len,
@@ -1715,22 +1736,30 @@ static struct sk_buff *ena_rx_skb(struct ena_ring *rx_ring,
 		return skb;
 	}
 
-#ifdef ENA_BUSY_POLL_SUPPORT
-	polling = ena_bp_busy_polling(rx_ring);
-	/* For busy poll don't allocate frag */
-	skb = ena_alloc_skb(rx_ring, !polling);
-#else
-	skb = ena_alloc_skb(rx_ring, true);
-#endif
+	ena_unmap_rx_buff(rx_ring, rx_info);
+
+	skb = ena_alloc_skb(rx_ring, page_addr);
 	if (unlikely(!skb))
 		return NULL;
 
+#ifdef ENA_LINEAR_FRAG_SUPPORTED
+	/* Populate skb's linear part */
+	skb_reserve(skb, page_offset);
+	skb_put(skb, len);
+#else
+	/* GRO expects us to have the ethernet header in the linear part.
+	 * Copy the first ENA_SKB_PULL_MIN_LEN bytes because it is more
+	 * efficient.
+	 */
+	hlen = min_t(u16, len, ENA_SKB_PULL_MIN_LEN);
+	memcpy(__skb_put(skb, hlen), data_addr, hlen);
+	if (hlen < len)
+		skb_add_rx_frag(skb, 0, rx_info->page,
+				page_offset + hlen, len - hlen, ENA_PAGE_SIZE);
+#endif
+	skb->protocol = eth_type_trans(skb, rx_ring->netdev);
+
 	do {
-		ena_unmap_rx_buff(rx_ring, rx_info);
-
-		skb_add_rx_frag(skb, skb_shinfo(skb)->nr_frags, rx_info->page,
-				rx_info->page_offset, len, ENA_PAGE_SIZE);
-
 		netif_dbg(rx_ring->adapter, rx_status, rx_ring->netdev,
 			  "RX skb updated. len %d. data_len %d\n",
 			  skb->len, skb->data_len);
@@ -1749,24 +1778,17 @@ static struct sk_buff *ena_rx_skb(struct ena_ring *rx_ring,
 		req_id = ena_bufs[buf].req_id;
 
 		rx_info = &rx_ring->rx_buffer_info[req_id];
+
+		ena_unmap_rx_buff(rx_ring, rx_info);
+
+		skb_add_rx_frag(skb, skb_shinfo(skb)->nr_frags, rx_info->page,
+				rx_info->page_offset, len, ENA_PAGE_SIZE);
+
 	} while (1);
 
 #ifdef ENA_BUSY_POLL_SUPPORT
-	if (polling) {
-		int hlen;
+	skb_mark_napi_id(skb, rx_ring->napi);
 
-		/* copy header into the skb linear data */
-		hlen = rx_ring->rx_copybreak;
-		skb_copy_to_linear_data(skb, va, hlen);
-
-		/* adjust the first segment and skb len */
-		skb_shinfo(skb)->frags[0].page_offset += hlen;
-		skb_shinfo(skb)->frags[0].size -= hlen;
-		skb->data_len -= hlen;
-		skb->tail += hlen;
-		skb->protocol = eth_type_trans(skb, rx_ring->netdev);
-		skb_mark_napi_id(skb, rx_ring->napi);
-	}
 #endif
 	return skb;
 }
@@ -2003,28 +2025,19 @@ static int ena_clean_rx_irq(struct ena_ring *rx_ring, struct napi_struct *napi,
 
 		skb_record_rx_queue(skb, rx_ring->qid);
 
-		if (rx_ring->ena_bufs[0].len <= rx_ring->rx_copybreak) {
-			total_len += rx_ring->ena_bufs[0].len;
+		if (rx_ring->ena_bufs[0].len <= rx_ring->rx_copybreak)
 			rx_copybreak_pkt++;
+
+		total_len += skb->len;
+
 #ifdef ENA_BUSY_POLL_SUPPORT
-			if (ena_bp_busy_polling(rx_ring))
-				netif_receive_skb(skb);
-			else
-				napi_gro_receive(napi, skb);
-#else
+		if (ena_bp_busy_polling(rx_ring))
+			netif_receive_skb(skb);
+		else
 			napi_gro_receive(napi, skb);
-#endif
-		} else {
-			total_len += skb->len;
-#ifdef ENA_BUSY_POLL_SUPPORT
-			if (ena_bp_busy_polling(rx_ring))
-				netif_receive_skb(skb);
-			else
-				napi_gro_frags(napi);
 #else
-			napi_gro_frags(napi);
-#endif
-		}
+		napi_gro_receive(napi, skb);
+#endif /* ENA_BUSY_POLL_SUPPORT */
 
 		res_budget--;
 	} while (likely(res_budget));

kernel/linux/ena/kcompat.h
@@ -799,4 +799,8 @@ static inline int numa_mem_id(void)
 #define AF_XDP_BUSY_POLL_SUPPORTED
 #endif
 
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(3, 5, 0)
+#define ENA_LINEAR_FRAG_SUPPORTED
+#endif
+
 #endif /* _KCOMPAT_H_ */

commit 539a95cdd1db5082f9eb239453e612b340f89374

linux/ena: Include ethernet header in rx_bytes statistics
Fix rx_bytes calculation by measuring skb->len before
eth_type_trans() removes the ethernet header.

This also aligns with XDP packet accounting and matches behavior
of other drivers.

Fixes: e75691c4fa87 ("linux/ena: Unify skb allocation path and use build_skb()")
Fixes: 0f361baa6286 ("linux/ena: Add AF XDP zero-copy RX polling function")
Fixes: 2e1f0ccdf2db ("linux/ena: Add rx_copybreak support for XDP_PASS cases")
Fixes: 50bd79f97b20 ("linux/ena: Add support for XDP multi-buffer RX path")
Signed-off-by: Arthur Kiyanovski <akiyano@amazon.com>

kernel/linux/ena/ena_netdev.c
@@ -1383,7 +1383,6 @@ struct sk_buff *ena_rx_skb_copybreak(struct ena_ring *rx_ring,
 #ifdef ENA_BUSY_POLL_SUPPORT
 	skb_mark_napi_id(skb, rx_ring->napi);
 #endif
-	skb->protocol = eth_type_trans(skb, rx_ring->netdev);
 
 	/* Don't reuse the RX page if we're on the wrong NUMA */
 	if (page_to_nid(rx_info->page) != numa_mem_id())
@@ -1466,7 +1465,6 @@ static struct sk_buff *ena_rx_skb(struct ena_ring *rx_ring, u32 descs)
 	/* Populate skb's linear part */
 	skb_reserve(skb, buf_offset);
 	skb_put(skb, len);
-	skb->protocol = eth_type_trans(skb, rx_ring->netdev);
 
 	netif_dbg(rx_ring->adapter, rx_status, rx_ring->netdev,
 		  "RX skb created. len %d.\n",
@@ -1692,6 +1690,10 @@ static int ena_clean_rx_irq(struct ena_ring *rx_ring, struct napi_struct *napi,
 		if (unlikely(!skb))
 			break;
 
+		total_len += skb->len;
+
+		skb->protocol = eth_type_trans(skb, rx_ring->netdev);
+
 		ena_rx_checksum(rx_ring, &ena_rx_ctx, skb);
 
 		ena_set_rx_hash(rx_ring, &ena_rx_ctx, skb);
@@ -1704,8 +1706,6 @@ static int ena_clean_rx_irq(struct ena_ring *rx_ring, struct napi_struct *napi,
 		if ((rx_ring->ena_bufs[0].len <= rx_ring->rx_copybreak) &&
 		    likely(ena_rx_ctx.descs == 1))
 			rx_copybreak_pkt++;
-
-		total_len += skb->len;
 
 #ifdef ENA_BUSY_POLL_SUPPORT
 		if (ena_bp_busy_polling(rx_ring))

kernel/linux/ena/ena_xdp.c
@@ -1000,8 +1000,6 @@ static struct sk_buff *ena_xdp_rx_skb_zc(struct ena_ring *rx_ring, struct xdp_bu
 	skb_reserve(skb, headroom);
 	memcpy(__skb_put(skb, data_len), data_addr, data_len);
 
-	skb->protocol = eth_type_trans(skb, rx_ring->netdev);
-
 	return skb;
 }
 
@@ -1108,6 +1106,7 @@ static bool ena_xdp_clean_rx_irq_zc(struct ena_ring *rx_ring,
 		pkt_copy++;
 		work_done++;
 		total_len += skb->len;
+		skb->protocol = eth_type_trans(skb, rx_ring->netdev);
 		ena_rx_checksum(rx_ring, &ena_rx_ctx, skb);
 		ena_set_rx_hash(rx_ring, &ena_rx_ctx, skb);
 		skb_record_rx_queue(skb, rx_ring->qid);
@@ -1295,8 +1294,6 @@ struct sk_buff *ena_rx_skb_after_xdp_pass(struct ena_ring *rx_ring,
 
 	skb_reserve(skb, buf_offset);
 	skb_put(skb, len);
-
-	skb->protocol = eth_type_trans(skb, rx_ring->netdev);
 
 #ifdef ENA_XDP_MB_SUPPORT
 	if (nr_frags) {