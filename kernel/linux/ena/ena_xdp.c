amzn/amzn-drivers

commit ca6d1f2b320012a8702f8fb9fda87012f6d00791

linux/ena: HW packet timestamping support
Add support for retrieving HW timestamps for TX and RX packets.
The feature is configurable through ioctls and the timestamps
can be retrieved by using the relevant socket operations.

A documentation entry has been added to provide additional
information (and a reference to [1]) as well as an
alternative method of retrieving the configuration through
sysfs.

[1]: https://www.kernel.org/doc/html/latest/networking/timestamping.html

Signed-off-by: David Arinzon <darinzon@amazon.com>

kernel/linux/ena/ena_xdp.c
@@ -672,6 +672,7 @@ static int ena_clean_xdp_irq(struct ena_ring *tx_ring, u32 budget)
 	u16 next_to_clean, req_id;
 	int rc, tx_pkts = 0;
 	u32 total_done = 0;
+	u64 hw_timestamp;
 
 	next_to_clean = tx_ring->next_to_clean;
 
@@ -679,8 +680,9 @@ static int ena_clean_xdp_irq(struct ena_ring *tx_ring, u32 budget)
 		struct ena_tx_buffer *tx_info;
 		struct xdp_frame *xdpf;
 
-		rc = ena_com_tx_comp_req_id_get(tx_ring->ena_com_io_cq,
-						&req_id);
+		rc = ena_com_tx_comp_metadata_get(tx_ring->ena_com_io_cq,
+						  &req_id,
+						  &hw_timestamp);
 		if (rc) {
 			handle_tx_comp_poll_error(tx_ring, req_id, rc);
 			break;
@@ -730,14 +732,16 @@ static bool ena_xdp_clean_tx_zc(struct ena_ring *tx_ring, u32 budget)
 	struct xsk_buff_pool *xsk_pool = tx_ring->xsk_pool;
 	int rc, cleaned_pkts, zc_pkts, acked_pkts;
 	struct ena_tx_buffer *tx_info;
+	u64 hw_timestamp;
 	u32 total_done;
 	u16 req_id;
 
 	acked_pkts = 0;
 
 	while (acked_pkts < budget) {
-		rc = ena_com_tx_comp_req_id_get(tx_ring->ena_com_io_cq,
-						&req_id);
+		rc = ena_com_tx_comp_metadata_get(tx_ring->ena_com_io_cq,
+						  &req_id,
+						  &hw_timestamp);
 		if (rc) {
 			handle_tx_comp_poll_error(tx_ring, req_id, rc);
 			break;

kernel/linux/common/ena_com/ena_admin_defs.h
@@ -71,6 +71,7 @@ enum ena_admin_aq_feature_id {
 	ENA_ADMIN_HOST_ATTR_CONFIG                  = 28,
 	ENA_ADMIN_PHC_CONFIG                        = 29,
 	ENA_ADMIN_FLOW_STEERING_CONFIG              = 30,
+	ENA_ADMIN_HW_TIMESTAMP                      = 31,
 	ENA_ADMIN_FEATURES_OPCODE_NUM               = 32,
 };
 
@@ -171,6 +172,21 @@ enum ena_admin_ena_srd_flags {
 enum ena_admin_frag_bypass_feature_version {
 	/* Enable only */
 	ENA_ADMIN_FRAG_BYPASS_FEATURE_VERSION_0     = 0,
+};
+
+enum ena_admin_hw_timestamp_feature_version {
+	/* RX only - NONE/All Traffic */
+	ENA_ADMIN_HW_TIMESTAMP_FEATURE_VERSION_1    = 1,
+};
+
+enum ena_admin_hw_timestamp_tx_support {
+	ENA_ADMIN_HW_TIMESTAMP_TX_SUPPORT_NONE      = 0,
+	ENA_ADMIN_HW_TIMESTAMP_TX_SUPPORT_ALL       = 1,
+};
+
+enum ena_admin_hw_timestamp_rx_support {
+	ENA_ADMIN_HW_TIMESTAMP_RX_SUPPORT_NONE      = 0,
+	ENA_ADMIN_HW_TIMESTAMP_RX_SUPPORT_ALL       = 1,
 };
 
 struct ena_admin_aq_common_desc {
@@ -714,6 +730,29 @@ struct ena_admin_feature_llq_desc {
 	struct ena_admin_accel_mode_req accel_mode;
 };
 
+struct ena_admin_feature_hw_ts_desc {
+	/* HW timestamp version as defined in
+	 * enum ena_admin_hw_timestamp_feature_version,
+	 * used only for GET command as max supported HW timestamp version by
+	 * device.
+	 */
+	u8 version;
+
+	/* TX state
+	 * Used for GET command as device support indication
+	 * Used for SET command as enable/disable
+	 * Supported values from enum ena_admin_hw_timestamp_tx_support
+	 */
+	u8 tx;
+
+	/* RX state
+	 * Used for GET command as device support indication
+	 * Used for SET command as enable/disable
+	 * Supported values from enum ena_admin_hw_timestamp_rx_support
+	 */
+	u8 rx;
+};
+
 struct ena_admin_flow_steering_rule_params {
 	u8 dst_ip[16];
 
@@ -1230,6 +1269,8 @@ struct ena_admin_get_feat_resp {
 
 		struct ena_admin_feature_phc_desc phc;
 
+		struct ena_admin_feature_hw_ts_desc hw_ts;
+
 		struct ena_admin_get_extra_properties_strings_desc extra_properties_strings;
 
 		struct ena_admin_get_extra_properties_flags_desc extra_properties_flags;
@@ -1275,6 +1316,8 @@ struct ena_admin_set_feat_cmd {
 
 		/* Fragment bypass configuration */
 		struct ena_admin_feature_frag_bypass_desc frag_bypass;
+
+		struct ena_admin_feature_hw_ts_desc hw_ts;
 	} u;
 };
 

kernel/linux/common/ena_com/ena_com.c
@@ -1089,6 +1089,80 @@ static int ena_com_get_feature(struct ena_com_dev *ena_dev,
 				      feature_ver);
 }
 
+bool ena_com_hw_timestamping_supported(struct ena_com_dev *ena_dev)
+{
+	return ena_com_check_supported_feature_id(ena_dev,
+						  ENA_ADMIN_HW_TIMESTAMP);
+}
+
+int ena_com_get_hw_timestamping_support(struct ena_com_dev *ena_dev,
+					u8 *tx_support,
+					u8 *rx_support)
+{
+	struct ena_admin_get_feat_resp get_resp;
+	int ret;
+
+	*tx_support = ENA_ADMIN_HW_TIMESTAMP_TX_SUPPORT_NONE;
+	*rx_support = ENA_ADMIN_HW_TIMESTAMP_RX_SUPPORT_NONE;
+
+	if (!ena_com_hw_timestamping_supported(ena_dev)) {
+		netdev_dbg(ena_dev->net_device, "HW timestamping is not supported\n");
+		return -EOPNOTSUPP;
+	}
+
+	ret = ena_com_get_feature(ena_dev,
+				  &get_resp,
+				  ENA_ADMIN_HW_TIMESTAMP,
+				  ENA_ADMIN_HW_TIMESTAMP_FEATURE_VERSION_1);
+
+	if (unlikely(ret)) {
+		netdev_err(ena_dev->net_device,
+			   "Failed to get HW timestamp configuration, error: %d\n", ret);
+		return ret;
+	}
+
+	*tx_support = get_resp.u.hw_ts.tx;
+	*rx_support = get_resp.u.hw_ts.rx;
+
+	return 0;
+}
+
+int ena_com_set_hw_timestamping_configuration(struct ena_com_dev *ena_dev,
+					      u8 tx_enable,
+					      u8 rx_enable)
+{
+	struct ena_admin_set_feat_resp resp;
+	struct ena_admin_set_feat_cmd cmd;
+	int ret;
+
+	if (!ena_com_hw_timestamping_supported(ena_dev)) {
+		netdev_dbg(ena_dev->net_device, "HW timestamping is not supported\n");
+		return -EOPNOTSUPP;
+	}
+
+	memset(&cmd, 0x0, sizeof(cmd));
+
+	cmd.aq_common_descriptor.opcode = ENA_ADMIN_SET_FEATURE;
+	cmd.feat_common.feature_id = ENA_ADMIN_HW_TIMESTAMP;
+	cmd.u.hw_ts.tx = tx_enable ? ENA_ADMIN_HW_TIMESTAMP_TX_SUPPORT_ALL :
+				     ENA_ADMIN_HW_TIMESTAMP_TX_SUPPORT_NONE;
+	cmd.u.hw_ts.rx = rx_enable ? ENA_ADMIN_HW_TIMESTAMP_RX_SUPPORT_ALL :
+				     ENA_ADMIN_HW_TIMESTAMP_RX_SUPPORT_NONE;
+
+	ret = ena_com_execute_admin_command(&ena_dev->admin_queue,
+					    (struct ena_admin_aq_entry *)&cmd,
+					    sizeof(cmd),
+					    (struct ena_admin_acq_entry *)&resp,
+					    sizeof(resp));
+	if (unlikely(ret)) {
+		netdev_err(ena_dev->net_device,
+			   "Failed to set HW timestamping configuration, error: %d\n", ret);
+		return ret;
+	}
+
+	return 0;
+}
+
 int ena_com_get_current_hash_function(struct ena_com_dev *ena_dev)
 {
 	return ena_dev->rss.hash_func;

kernel/linux/common/ena_com/ena_com.h
@@ -1237,6 +1237,38 @@ static inline void ena_com_disable_adaptive_moderation(struct ena_com_dev *ena_d
 	ena_dev->adaptive_coalescing = false;
 }
 
+/* ena_com_hw_timestamping_supported - query whether HW timestamping is
+ *                                     supported by the device
+ * @ena_dev: ENA communication layer struct
+ *
+ * @return - true if the feature is supported or false otherwise
+ */
+bool ena_com_hw_timestamping_supported(struct ena_com_dev *ena_dev);
+
+/* ena_com_get_hw_timestamping_support - query HW timestamping support options
+ *                                       in the device
+ * @ena_dev: ENA communication layer struct
+ * @tx_support: Will hold the TX side supported options
+ * @rx_support: Will hold the RX side supported options
+ *
+ * @return - 0 on success, negative value on failure.
+ */
+int ena_com_get_hw_timestamping_support(struct ena_com_dev *ena_dev,
+					u8 *tx_support,
+					u8 *rx_support);
+
+/* ena_com_set_hw_timestamping_configuration - set HW timestamping configuration
+ *
+ * @ena_dev: ENA communication layer struct
+ * @tx_enable: Enable/Disable TX HW timestamping
+ * @rx_enable: Enable/Disable RX HW timestamping
+ *
+ * @return - 0 on success, negative value on failure.
+ */
+int ena_com_set_hw_timestamping_configuration(struct ena_com_dev *ena_dev,
+					      u8 tx_enable,
+					      u8 rx_enable);
+
 bool ena_com_indirection_table_config_supported(struct ena_com_dev *ena_dev);
 
 /* ena_com_get_cap - query whether device supports a capability.

kernel/linux/common/ena_com/ena_eth_com.c
@@ -45,6 +45,10 @@ void ena_com_dump_single_rx_cdesc(struct ena_com_io_cq *io_cq,
 			   FIELD_GET((u32)ENA_ETH_IO_RX_DESC_LAST_MASK, desc->base.status),
 			   FIELD_GET((u32)ENA_ETH_IO_RX_CDESC_BASE_MBZ7_MASK, desc->base.status),
 			   FIELD_GET((u32)ENA_ETH_IO_RX_CDESC_BASE_MBZ17_MASK, desc->base.status));
+		if (unlikely(ena_com_is_extended_rx_cdesc(io_cq)))
+			netdev_err(ena_com_io_cq_to_ena_dev(io_cq)->net_device,
+				   "RX descriptor timestamp %llu",
+				   (u64)desc->timestamp_low | (u64)desc->timestamp_high << 32);
 	}
 }
 
@@ -59,6 +63,10 @@ void ena_com_dump_single_tx_cdesc(struct ena_com_io_cq *io_cq,
 			   desc_arr[1],
 			   FIELD_GET((u32)ENA_ETH_IO_TX_CDESC_PHASE_MASK, desc->base.flags),
 			   FIELD_GET((u32)ENA_ETH_IO_TX_CDESC_MBZ6_MASK, desc->base.flags));
+		if (unlikely(ena_com_is_extended_tx_cdesc(io_cq)))
+			netdev_err(ena_com_io_cq_to_ena_dev(io_cq)->net_device,
+				   "TX descriptor timestamp %llu",
+				   (u64)desc->timestamp_low | (u64)desc->timestamp_high << 32);
 	}
 }
 
@@ -616,6 +624,10 @@ int ena_com_rx_pkt(struct ena_com_io_cq *io_cq,
 
 	/* Get rx flags from the last pkt */
 	ena_com_rx_set_flags(ena_rx_ctx, cdesc);
+
+	if (unlikely(ena_com_is_extended_rx_cdesc(io_cq)))
+		ena_rx_ctx->timestamp = (u64)cdesc->timestamp_low |
+					(u64)cdesc->timestamp_high << 32;
 
 	netdev_dbg(ena_com_io_cq_to_ena_dev(io_cq)->net_device,
 		   "l3_proto %d l4_proto %d l3_csum_err %d l4_csum_err %d hash %d frag %d cdesc_status %x\n",

kernel/linux/common/ena_com/ena_eth_com.h
@@ -60,6 +60,7 @@ struct ena_com_rx_ctx {
 	u16 descs;
 	u16 max_bufs;
 	u8 pkt_offset;
+	u64 timestamp;
 };
 
 int ena_com_prepare_tx(struct ena_com_io_sq *io_sq,
@@ -225,8 +226,9 @@ static inline void ena_com_cq_inc_head(struct ena_com_io_cq *io_cq)
 		io_cq->phase ^= 1;
 }
 
-static inline int ena_com_tx_comp_req_id_get(struct ena_com_io_cq *io_cq,
-					     u16 *req_id)
+static inline int ena_com_tx_comp_metadata_get(struct ena_com_io_cq *io_cq,
+					       u16 *req_id,
+					       u64 *hw_timestamp)
 {
 	struct ena_com_dev *dev = ena_com_io_cq_to_ena_dev(io_cq);
 	struct ena_eth_io_tx_cdesc_ext *cdesc;
@@ -266,6 +268,10 @@ static inline int ena_com_tx_comp_req_id_get(struct ena_com_io_cq *io_cq,
 			   cdesc->base.req_id);
 		return -EINVAL;
 	}
+
+	if (unlikely(ena_com_is_extended_tx_cdesc(io_cq)))
+		*hw_timestamp = (u64)cdesc->timestamp_low |
+				(u64)cdesc->timestamp_high << 32;
 
 	ena_com_cq_inc_head(io_cq);
 

kernel/linux/common/ena_com/ena_eth_io_defs.h
@@ -166,9 +166,9 @@ struct ena_eth_io_tx_cdesc {
 struct ena_eth_io_tx_cdesc_ext {
 	struct ena_eth_io_tx_cdesc base;
 
-	u32 reserved_w2;
+	u32 timestamp_low;
 
-	u32 reserved_w3;
+	u32 timestamp_high;
 };
 
 struct ena_eth_io_rx_desc {
@@ -263,9 +263,9 @@ struct ena_eth_io_rx_cdesc_ext {
 
 	u16 reserved16;
 
-	u32 reserved_w6;
+	u32 timestamp_low;
 
-	u32 reserved_w7;
+	u32 timestamp_high;
 };
 
 struct ena_eth_io_intr_reg {

kernel/linux/ena/config/test_defs.sh
@@ -235,3 +235,30 @@ try_compile_async "#include <linux/smp.h>"                      \
                   "ENA_HAVE_SMP_STORE_RELEASE"                  \
                   ""                                            \
                   "3.14.0 <= LINUX_VERSION_CODE"
+
+try_compile_async "#include <linux/netdevice.h>"         \
+                  "{
+                    struct net_device_ops dev_ops;
+                    dev_ops.ndo_eth_ioctl = NULL;
+                  }"                                     \
+                  "ENA_HAVE_NDO_ETH_IOCTL"               \
+                  ""                                     \
+                  "5.15.0 <= LINUX_VERSION_CODE"
+
+try_compile_async "#include <linux/netdevice.h>"         \
+                  "{
+                    struct net_device_ops dev_ops;
+                    dev_ops.ndo_hwtstamp_get = NULL;
+                    dev_ops.ndo_hwtstamp_set = NULL;
+                  }"                                     \
+                  "ENA_HAVE_NDO_HWTSTAMP"                \
+                  ""                                     \
+                  "6.6.0 <= LINUX_VERSION_CODE"
+
+try_compile_async "#include <linux/net_tstamp.h>"            \
+                  "{
+                    int rx_filter = HWTSTAMP_FILTER_NTP_ALL;
+                  }"                                         \
+                  "ENA_HAVE_HWTSTAMP_FILTER_NTP_ALL"         \
+                  ""                                         \
+                  "4.13.0 <= LINUX_VERSION_CODE"

commit 577b404a3134c1c99357c14c634d441819f7f04a

linux/ena: Support skb HW timestamp on AF XDP queues
AF XDP queues are shared with the network stack and can receive
skb (when xdp prog verdict is XDP_PASS) or transmit skb via
ena_start_xmit().
Add missing logic to af xdp napi function in order to support
skb HW timestamp.

Fixes: ca6d1f2b3200 ("linux/ena: HW packet timestamping support")
Signed-off-by: Ofir Tabachnik <ofirt@amazon.com>

kernel/linux/ena/ena_xdp.c
@@ -623,6 +623,7 @@ static bool ena_xdp_clean_tx_zc(struct ena_ring *tx_ring, u32 budget)
 {
 	int rc, cleaned_pkts, zc_pkts, acked_pkts, missed_tx = 0;
 	struct xsk_buff_pool *xsk_pool = tx_ring->xsk_pool;
+	struct skb_shared_hwtstamps tx_hw_timestamp = {};
 	struct ena_tx_buffer *tx_info;
 	u32 total_done, tx_bytes = 0;
 #ifdef ENA_HAVE_XSK_TX_METADATA
@@ -675,6 +676,12 @@ static bool ena_xdp_clean_tx_zc(struct ena_ring *tx_ring, u32 budget)
 						 &ena_xsk_tx_metadata_ops,
 						 &hw_timestamp);
 #endif /* ENA_HAVE_XSK_TX_METADATA */
+
+		skb = tx_info->skb;
+		if (unlikely(skb && (skb_shinfo(skb)->tx_flags & SKBTX_IN_PROGRESS))) {
+			tx_hw_timestamp.hwtstamp = ns_to_ktime(hw_timestamp);
+			skb_tstamp_tx(skb, &tx_hw_timestamp);
+		}
 
 		tx_info->tx_sent_jiffies = 0;
 
@@ -1001,6 +1008,9 @@ static bool ena_xdp_clean_rx_irq_zc(struct ena_ring *rx_ring,
 		ena_rx_checksum(rx_ring, &ena_rx_ctx, skb);
 		ena_set_rx_hash(rx_ring, &ena_rx_ctx, skb);
 		skb_record_rx_queue(skb, rx_ring->qid);
+		if (unlikely(ena_hw_rx_timestamp_requested(rx_ring->adapter)))
+			skb_hwtstamps(skb)->hwtstamp = ns_to_ktime(ena_rx_ctx.timestamp);
+
 		napi_gro_receive(napi, skb);
 
 	} while (likely(work_done <= budget));