a5b6eca2e19961b61aa70fd40bad8d35479fa5b9
Tom Sela <tomsela@amazon.com>
2026-06-10 10:36:24 +0000

    linux/efa: Backport query_port_speed for older kernels
    On kernels older than 7.0, IB core does not support query port
    speed. Register the method via driver def so userspace can call it
    regardless of kernel version as long they support driver def
    
    Reviewed-by: Yonatan Nachum <ynachum@amazon.com>
    Signed-off-by: Tom Sela <tomsela@amazon.com>
    Signed-off-by: Michael Margolin <mrgolin@amazon.com>

--- kernel/linux/efa/src/kcompat.h
+++ kernel/linux/efa/src/kcompat.h
@@ -432,4 +432,15 @@ enum efa_uverbs_attrs_create_cq_cmd_attr_ids {
 };
 #endif
 
+#if !defined(HAVE_QUERY_PORT_SPEED)
+enum {
+	UVERBS_METHOD_QUERY_PORT_SPEED = 7,
+};
+
+enum uverbs_attrs_query_port_speed_cmd_attr_ids {
+	UVERBS_ATTR_QUERY_PORT_SPEED_PORT_NUM,
+	UVERBS_ATTR_QUERY_PORT_SPEED_RESP,
+};
+#endif
+
 #endif /* _KCOMPAT_H_ */
