amzn/amzn-drivers

commit 713ac477502b01b578a424c110ce0f0ee0502dc9

linux/ena: Add extended debug area support
Introduce an extended debug area mechanism to aid in identifying
and root-causing driver resets.

1. Export statistics retrieval functions (global, per-queue, and
   accumulated Tx/Rx) to enable centralized access for debug flows.

2. Add an admin interrupt mask getter to support the extended
   debug area.

3. Add a common header structure for the debug area section and
   register extended debug area support in driver_supported_features.

4. Extend the debug area to dump driver statistics and reset-specific
   data, fitting within the existing cacheline layout.

5. Utilize the debug area to dump relevant data for selected
   reset reasons.

Signed-off-by: Ofir Tabachnik <ofirt@amazon.com>

kernel/linux/ena/ena_netdev.c
@@ -43,6 +43,8 @@
 
 #include "ena_debugfs.h"
 
+#include "ena_debug.h"
+
 static char driver_info[] = DEVICE_NAME " v" DRV_MODULE_GENERATION "\n";
 
 MODULE_AUTHOR("Amazon.com, Inc. or its affiliates");
@@ -151,7 +153,7 @@ static void ena_tx_timeout(struct net_device *dev, unsigned int txqueue)
 	}
 
 schedule_reset:
-	ena_reset_device(adapter, reset_reason);
+	ena_reset_device_record_qid(adapter, reset_reason, txqueue);
 	ena_increase_stat(&adapter->dev_stats.tx_timeout, 1, &adapter->syncp);
 }
 
@@ -181,7 +183,8 @@ static void ena_find_and_timeout_queue(struct net_device *dev)
 
 	netdev_warn(dev, "timeout was called, but no offending queue was found\n");
 
-	ena_reset_device(adapter, ENA_REGS_RESET_OS_NETDEV_WD);
+	ena_reset_device_record_qid(adapter, ENA_REGS_RESET_OS_NETDEV_WD,
+				    ENA_MAX_NUM_IO_QUEUES);
 	ena_increase_stat(&adapter->dev_stats.tx_timeout, 1, &adapter->syncp);
 }
 
@@ -1059,7 +1062,8 @@ int handle_invalid_req_id(struct ena_ring *ring, u16 req_id,
 
 	ena_get_and_dump_head_tx_cdesc(ring->ena_com_io_cq);
 	ena_increase_stat(&ring->tx_stats.bad_req_id, 1, &ring->syncp);
-	ena_reset_device(adapter, ENA_REGS_RESET_INV_TX_REQ_ID);
+	ena_reset_device_record_qid(adapter, ENA_REGS_RESET_INV_TX_REQ_ID,
+				    ring->qid);
 
 	return -EFAULT;
 }
@@ -1420,7 +1424,8 @@ static struct sk_buff *ena_rx_skb(struct ena_ring *rx_ring, u32 descs)
 			  "SKB: Page is NULL. qid %u req_id %u\n",
 			  rx_ring->qid, req_id);
 		ena_increase_stat(&rx_ring->rx_stats.bad_req_id, 1, &rx_ring->syncp);
-		ena_reset_device(adapter, ENA_REGS_RESET_INV_RX_REQ_ID);
+		ena_reset_device_record_qid(adapter, ENA_REGS_RESET_INV_RX_REQ_ID,
+					    rx_ring->qid);
 		return NULL;
 	}
 
@@ -1768,12 +1773,16 @@ error:
 		ena_reset_device(adapter, ENA_REGS_RESET_TOO_MANY_RX_DESCS);
 	} else if (rc == -EFAULT) {
 		ena_get_and_dump_head_rx_cdesc(rx_ring->ena_com_io_cq);
-		ena_reset_device(adapter, ENA_REGS_RESET_RX_DESCRIPTOR_MALFORMED);
+		ena_reset_device_record_qid(adapter,
+					    ENA_REGS_RESET_RX_DESCRIPTOR_MALFORMED,
+					    rx_ring->qid);
 	} else {
 		ena_increase_stat(&rx_ring->rx_stats.bad_req_id, 1,
 				  &rx_ring->syncp);
 		ena_get_and_dump_head_rx_cdesc(rx_ring->ena_com_io_cq);
-		ena_reset_device(adapter, ENA_REGS_RESET_INV_RX_REQ_ID);
+		ena_reset_device_record_qid(adapter,
+					    ENA_REGS_RESET_INV_RX_REQ_ID,
+					    rx_ring->qid);
 	}
 	return 0;
 }
@@ -2905,6 +2914,7 @@ void ena_down(struct ena_adapter *adapter)
 		smp_rmb();
 
 		ena_save_persistent_stats(adapter);
+		ena_write_to_debug_area(adapter);
 		rc = ena_com_dev_reset(ena_dev, ena_get_reset_reason(adapter));
 		if (rc)
 			netif_err(adapter, ifdown, adapter->netdev,
@@ -2986,7 +2996,6 @@ static int ena_open(struct net_device *netdev)
 static int ena_close(struct net_device *netdev)
 {
 	struct ena_adapter *adapter = netdev_priv(netdev);
-	u8 *debug_area;
 	int rc;
 
 	netif_dbg(adapter, ifdown, netdev, "%s\n", __func__);
@@ -3003,9 +3012,7 @@ static int ena_close(struct net_device *netdev)
 		netif_err(adapter, ifdown, adapter->netdev,
 			  "Destroy failure, restarting device\n");
 
-		debug_area = adapter->ena_dev->host_attr.debug_area_virt_addr;
-		ena_dump_stats_to_buf(adapter, debug_area);
-		ena_dump_stats_to_dmesg(adapter);
+		ena_dump_stats_to_log(adapter);
 		/* rtnl lock already obtained in dev_ioctl() layer */
 		rc = ena_destroy_device(adapter, false);
 		rc |= ena_restore_device(adapter);
@@ -3525,7 +3532,8 @@ static void ena_config_host_info(struct ena_com_dev *ena_dev, struct pci_dev *pd
 		ENA_ADMIN_HOST_INFO_RSS_CONFIGURABLE_FUNCTION_KEY_MASK |
 		ENA_ADMIN_HOST_INFO_RX_PAGE_REUSE_MASK |
 		ENA_ADMIN_HOST_INFO_TX_IPV6_CSUM_OFFLOAD_MASK |
-		ENA_ADMIN_HOST_INFO_PHC_MASK;
+		ENA_ADMIN_HOST_INFO_PHC_MASK |
+		ENA_ADMIN_HOST_INFO_DEBUG_AREA_EXT_MASK;
 
 	rc = ena_com_set_host_attributes(ena_dev);
 	if (unlikely(rc)) {
@@ -3546,23 +3554,22 @@ err:
 static void ena_config_debug_area(struct ena_adapter *adapter)
 {
 	u32 debug_area_size;
-	int rc, ss_count;
+	int rc;
 
-	ss_count = ena_get_sset_count(adapter->netdev, ETH_SS_STATS);
-	if (ss_count <= 0) {
-		netif_err(adapter, drv, adapter->netdev,
-			  "SS count is negative\n");
-		return;
-	}
-
-	/* allocate 32 bytes for each string and 64bit for the value */
-	debug_area_size = ss_count * ETH_GSTRING_LEN + sizeof(u64) * ss_count;
+	debug_area_size = ena_get_debug_area_size(adapter);
 
 	rc = ena_com_allocate_debug_area(adapter->ena_dev, debug_area_size);
 	if (unlikely(rc)) {
 		netif_err(adapter, drv, adapter->netdev,
 			  "Cannot allocate debug area\n");
 		return;
+	}
+
+	rc = ena_reset_reason_info_alloc(adapter);
+	if (unlikely(rc)) {
+		netif_err(adapter, drv, adapter->netdev,
+			  "Failed to alloc reset reason info\n");
+		goto err;
 	}
 
 	rc = ena_com_set_host_attributes(adapter->ena_dev);
@@ -3577,6 +3584,7 @@ static void ena_config_debug_area(struct ena_adapter *adapter)
 
 	return;
 err:
+	ena_reset_reason_info_free(adapter);
 	ena_com_delete_debug_area(adapter->ena_dev);
 }
 
@@ -4618,6 +4626,9 @@ err_device_destroy:
 	ena_com_abort_admin_commands(ena_dev);
 	ena_com_wait_for_abort_completion(ena_dev);
 	ena_com_admin_destroy(ena_dev);
+	ena_set_reset_reason(adapter, ENA_REGS_RESET_DRIVER_INVALID_STATE);
+	ena_update_reset_info_invalid_state(adapter);
+	ena_write_to_debug_area(adapter);
 	ena_com_dev_reset(ena_dev, ENA_REGS_RESET_DRIVER_INVALID_STATE);
 	ena_phc_destroy(adapter);
 	ena_com_mmio_reg_read_request_destroy(ena_dev);
@@ -4648,7 +4659,7 @@ static void ena_fw_reset_device(struct work_struct *work)
 		smp_rmb();
 
 		if (ena_get_reset_reason(adapter) != ENA_REGS_RESET_NORMAL)
-			ena_dump_stats_to_dmesg(adapter);
+			ena_dump_stats_to_log(adapter);
 
 		rc |= ena_destroy_device(adapter, false);
 		rc |= ena_restore_device(adapter);
@@ -5208,7 +5219,6 @@ static void ena_timer_service(unsigned long data)
 {
 	struct ena_adapter *adapter = (struct ena_adapter *)data;
 #endif
-	u8 *debug_area = adapter->ena_dev->host_attr.debug_area_virt_addr;
 	struct ena_admin_host_info *host_info =
 		adapter->ena_dev->host_attr.host_info;
 
@@ -5221,8 +5231,6 @@ static void ena_timer_service(unsigned long data)
 	check_for_missing_completions(adapter);
 
 	check_for_empty_rx_ring(adapter);
-
-	ena_dump_stats_to_buf(adapter, debug_area);
 
 	if (host_info)
 		ena_update_host_info(host_info, adapter->netdev);
@@ -5653,8 +5661,6 @@ static int ena_probe(struct pci_dev *pdev, const struct pci_device_id *ent)
 		goto err_terminate_sysfs;
 	}
 
-	ena_config_debug_area(adapter);
-
 	rc = ena_com_flow_steering_init(ena_dev, get_feat_ctx.dev_attr.flow_steering_max_entries);
 	if (rc && (rc != -EOPNOTSUPP)) {
 		dev_err(&pdev->dev, "Cannot init Flow steering rules rc: %d\n", rc);
@@ -5678,6 +5684,8 @@ static int ena_probe(struct pci_dev *pdev, const struct pci_device_id *ent)
 		dev_err(&pdev->dev, "Cannot register net device\n");
 		goto err_flow_steering;
 	}
+
+	ena_config_debug_area(adapter);
 
 	ena_debugfs_init(netdev);
 
@@ -5718,7 +5726,6 @@ static int ena_probe(struct pci_dev *pdev, const struct pci_device_id *ent)
 err_flow_steering:
 	ena_com_flow_steering_destroy(ena_dev);
 err_rss:
-	ena_com_delete_debug_area(ena_dev);
 	ena_com_rss_destroy(ena_dev);
 err_terminate_sysfs:
 	ena_sysfs_terminate(&pdev->dev);
@@ -5817,6 +5824,8 @@ static void __ena_shutoff(struct pci_dev *pdev, bool shutdown)
 	ena_com_rss_destroy(ena_dev);
 
 	ena_com_flow_steering_destroy(ena_dev);
+
+	ena_reset_reason_info_free(adapter);
 
 	ena_com_delete_debug_area(ena_dev);
 

kernel/linux/common/ena_com/ena_admin_defs.h
@@ -1115,7 +1115,8 @@ struct ena_admin_host_info {
 	 * 6 : rx_page_reuse
 	 * 7 : tx_ipv6_csum_offload
 	 * 8 : phc
-	 * 31:9 : reserved
+	 * 9 : debug_area_ext
+	 * 31:10 : reserved
 	 */
 	u32 driver_supported_features;
 };
@@ -1535,6 +1536,8 @@ struct ena_admin_phc_resp {
 #define ENA_ADMIN_HOST_INFO_TX_IPV6_CSUM_OFFLOAD_MASK       BIT(7)
 #define ENA_ADMIN_HOST_INFO_PHC_SHIFT                       8
 #define ENA_ADMIN_HOST_INFO_PHC_MASK                        BIT(8)
+#define ENA_ADMIN_HOST_INFO_DEBUG_AREA_EXT_SHIFT            9
+#define ENA_ADMIN_HOST_INFO_DEBUG_AREA_EXT_MASK             BIT(9)
 
 /* feature_rss_ind_table */
 #define ENA_ADMIN_FEATURE_RSS_IND_TABLE_ONE_ENTRY_UPDATE_MASK BIT(0)

kernel/linux/common/ena_com/ena_com.c
@@ -1764,6 +1764,11 @@ void ena_com_set_admin_polling_mode(struct ena_com_dev *ena_dev, bool polling)
 	ena_dev->admin_queue.polling = polling;
 }
 
+u32 ena_com_get_admin_intr_mask(struct ena_com_dev *ena_dev)
+{
+	return ena_com_reg_bar_read32(ena_dev, ENA_REGS_INTR_MASK_OFF);
+}
+
 bool ena_com_get_admin_polling_mode(struct ena_com_dev *ena_dev)
 {
 	return ena_dev->admin_queue.polling;
@@ -3794,4 +3799,22 @@ int ena_com_set_frag_bypass(struct ena_com_dev *ena_dev, bool enable)
 		netdev_err(ena_dev->net_device, "Failed to enable frag bypass. error: %d\n", ret);
 
 	return ret;
+}
+
+void ena_com_write_debug_area_header(u32 version, u8 os, u8 *debug_area)
+{
+	struct ena_com_debug_area_header header = {};
+	u32 length;
+
+	/* Debug area header length val does not include name and length fields */
+	length = sizeof(struct ena_com_debug_area_header) -
+		ENA_DEBUG_AREA_HEADER_NAME_SIZE - sizeof(u32);
+
+	snprintf(header.name, ENA_DEBUG_AREA_HEADER_NAME_SIZE, "%s",
+		 ENA_DEBUG_AREA_HEADER_NAME);
+	header.length = length;
+	header.version = version;
+	header.os = os;
+
+	memcpy(debug_area, &header, sizeof(struct ena_com_debug_area_header));
 }

kernel/linux/common/ena_com/ena_com.h
@@ -54,6 +54,9 @@
 
 #define ENA_CUSTOMER_METRICS_BUFFER_SIZE 512
 
+#define ENA_DEBUG_AREA_HEADER_NAME		"debug-area-header"
+#define ENA_DEBUG_AREA_HEADER_NAME_SIZE		32
+
 /*****************************************************************************/
 /*****************************************************************************/
 /* ENA adaptive interrupt moderation settings */
@@ -370,6 +373,14 @@ struct ena_host_attribute {
 	dma_addr_t host_info_dma_addr;
 };
 
+struct ena_com_debug_area_header {
+	char name[ENA_DEBUG_AREA_HEADER_NAME_SIZE];
+	u32 length;
+	u32 version;
+	u8 os;
+	u8 reserved[3];
+};
+
 struct ena_com_flow_steering_rule_params {
 	struct ena_admin_flow_steering_rule_params flow_params;
 	u16 qid;
@@ -631,6 +642,16 @@ bool ena_com_get_admin_running_state(struct ena_com_dev *ena_dev);
  * Set the admin completion mode.
  */
 void ena_com_set_admin_polling_mode(struct ena_com_dev *ena_dev, bool polling);
+
+/* ena_com_get_admin_intr_state - Get admin interrupt mask from register bar
+ * @ena_dev: ENA communication layer struct
+ *
+ * Get the admin queue interrupt mask from ENA_REGS_INTR_MASK_OFF offset
+ * in the register bar
+ *
+ * @return - the content of admin interrupt mask off register
+ */
+u32 ena_com_get_admin_intr_mask(struct ena_com_dev *ena_dev);
 
 /* ena_com_get_admin_polling_mode - Get the admin completion queue polling mode
  * @ena_dev: ENA communication layer struct
@@ -1203,6 +1224,14 @@ int ena_com_flow_steering_restore_device_rules(struct ena_com_dev *ena_dev);
  * @return - 0 on success, negative value on failure.
  */
 int ena_com_set_frag_bypass(struct ena_com_dev *ena_dev, bool enable);
+
+/* ena_com_write_debug_area_header - write debug area header to debug_area
+ * buffer.
+ * @version: debug area version
+ * @os: OS type
+ * @debug_area: debug area buffer
+ */
+void ena_com_write_debug_area_header(u32 version, u8 os, u8 *debug_area);
 
 /* ena_com_io_sq_to_ena_dev - Extract ena_com_dev using contained field io_sq.
  * @io_sq: IO submit queue struct

kernel/linux/ena/Makefile
@@ -12,6 +12,7 @@ $(DRIVER_NAME)-srcs := ena_netdev.c \
 		dim.c \
 		net_dim.c \
 		ena_devlink.c \
+		ena_debug.c \
 		$(ENA_COM_PATH)/ena_com.c \
 		$(ENA_COM_PATH)/ena_eth_com.c
 

commit fcf0d63c20634b5335f645b148a2dd4c43fa4796

linux/ena: Fix use-after-free of adapter memory in __ena_shutoff
The adapter struct is allocated as part of the netdev via
alloc_etherdev_mq() (it is the netdev_priv area). In the removal path,
free_netdev() releases the netdev and therefore also frees the adapter
memory.

In __ena_shutoff(), free_netdev() was called inside the removal branch,
but ena_reset_reason_info_free() and ena_free_stats_buffers() were
called afterwards. Both functions dereference and write to the adapter
(adapter->debug_area_info.reset_info and adapter->stats_buffers), so on
the removal path they accessed already-freed memory, resulting in a
use-after-free.

The shutdown path is not affected since it does not call free_netdev().

Fix this by freeing the adapter-embedded buffers before free_netdev()
is called, while the adapter memory is still valid. The remaining
cleanup only relies on the separately allocated ena_dev, which stays
valid until vfree() at the end of the function.

Fixes: 713ac477502b ("linux/ena: Add extended debug area support")
Fixes: 9b012e69241f ("linux/ena: Driver stats memory allocation optimization")
Signed-off-by: David Arinzon <darinzon@amazon.com>

kernel/linux/ena/ena_netdev.c
@@ -5858,6 +5858,9 @@ static void __ena_shutoff(struct pci_dev *pdev, bool shutdown)
 	ena_devlink_unregister(adapter->devlink);
 	ena_devlink_free(adapter->devlink);
 
+	ena_reset_reason_info_free(adapter);
+	ena_free_stats_buffers(adapter);
+
 	if (shutdown) {
 		netif_device_detach(netdev);
 		dev_close(netdev);
@@ -5865,6 +5868,7 @@ static void __ena_shutoff(struct pci_dev *pdev, bool shutdown)
 	} else {
 		rtnl_unlock();
 		unregister_netdev(netdev);
+		/* No access to adapter and netdev after this point */
 		free_netdev(netdev);
 	}
 
@@ -5872,11 +5876,7 @@ static void __ena_shutoff(struct pci_dev *pdev, bool shutdown)
 
 	ena_com_flow_steering_destroy(ena_dev);
 
-	ena_reset_reason_info_free(adapter);
-
 	ena_com_delete_debug_area(ena_dev);
-
-	ena_free_stats_buffers(adapter);
 
 	ena_com_delete_host_info(ena_dev);
 